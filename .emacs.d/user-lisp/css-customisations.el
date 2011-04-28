4b845b63b8e8b82f97f0d1e46222078fcc6eb3b3
Wilfred Hughes <wilfred@potatolondon.com>
2011-04-28 12:09:11 +0100

    highlight colour codes as the colours they represent in CSS files

--- .emacs.d/user-lisp/css-customisations.el
+++ .emacs.d/user-lisp/css-customisations.el
@@ -6,4 +6,17 @@
                (setq css-indent-offset 8)
                (setq indent-tabs-mode t)))))
 
+; highlight colours as themselves
+(defvar hexcolour-keywords
+  '(("#[abcdef[:digit:]]\\{6\\}"
+     (0 (put-text-property
+         (match-beginning 0)
+         (match-end 0)
+         'face (list :background
+                     (match-string-no-properties 0)))))))
+(defun hexcolour-add-to-font-lock ()
+  (font-lock-add-keywords nil hexcolour-keywords))
+
+(add-hook 'css-mode-hook 'hexcolour-add-to-font-lock)
+
 (provide 'css-customisations)
