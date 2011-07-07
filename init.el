149b3f40a8bdb28718ab60fcd398ff80d18ddee8
Wilfred Hughes <me@wilfred.me.uk>
2011-07-07 23:34:03 +0100

    indentation fixes

--- init.el
+++ init.el
@@ -180,16 +180,16 @@ are interchanged."
   "Renames both current buffer and file it's visiting to NEW-NAME."
   (interactive (list (read-from-minibuffer "New name: " (buffer-name))))
   (let ((name (buffer-name))
-    (filename (buffer-file-name)))
+        (filename (buffer-file-name)))
     (if (not filename)
-    (message "Buffer '%s' is not visiting a file!" name)
+        (message "Buffer '%s' is not visiting a file!" name)
       (if (get-buffer new-name)
-      (message "A buffer named '%s' already exists!" new-name)
-    (progn
-      (rename-file name new-name 1)
-      (rename-buffer new-name)
-      (set-visited-file-name new-name)
-      (set-buffer-modified-p nil))))))
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file name new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
 
 ; something in the above has switched on debugging. Switch it off.
 ; TODO: find offending code
