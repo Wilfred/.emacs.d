e147d76ac69d70f7c4797325cacc8199ec79b643
Wilfred Hughes <wilfred@potatolondon.com>
2011-07-12 16:49:21 +0100

    display tabs as four characters in js-mode

--- user-lisp/javascript-customisations.el
+++ user-lisp/javascript-customisations.el
@@ -1,5 +1,6 @@
 ; indent with tab characters (treating them as eight spaces)
-(setq-default js-indent-level 8)
+(setq-default js-indent-level 4)
+(setq tab-width 4)
 (add-hook 'js-mode-hook
           '(lambda ()
              (setq indent-tabs-mode t)))
