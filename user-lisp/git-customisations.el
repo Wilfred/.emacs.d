d2ec65ac68135073c301bae0442fabe3a8c5aa8d
Wilfred Hughes <wilfred@potatolondon.com>
2011-08-26 17:22:52 +0100

    use standard diff colouring in magit

--- user-lisp/git-customisations.el
+++ user-lisp/git-customisations.el
@@ -3,6 +3,14 @@
 
 (global-set-key [(f2)] 'magit-status)
 
+;; change magit diff colors to the standard red and green
+(eval-after-load 'magit
+  '(progn
+     (set-face-foreground 'magit-diff-add "green3")
+     (set-face-foreground 'magit-diff-del "red3")
+     (when (not window-system)
+       (set-face-background 'magit-item-highlight "black"))))
+
 (require 'thingatpt) ; provides symbol-at-point
 
 (defun grep-git-project (search-term)
