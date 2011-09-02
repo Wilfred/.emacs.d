d1893089f40b0af6b59e4fbc2cdfedbe5ab90786
Wilfred Hughes <wilfred@potatolondon.com>
2011-09-02 15:03:48 +0100

    moving OS X tweaks out of init.el into os-x-fixes.el

---
+++ user-lisp/os-x-fixes.el
@@ -0,0 +1,17 @@
+(defun insert-hash ()
+  (interactive)
+  (insert "#"))
+
+; set Meta-3 to insert a # character
+(global-set-key "\263" 'insert-hash)
+
+; same PATH as bash from Terminal
+(setenv
+ "PATH"
+ "/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:/usr/local/bin/g4bin:/usr/local/git/bin:/usr/local/sbin:/usr/X11/bin:/Users/wilfredhughes/homebrew/bin:/Users/wilfredhughes/homebrew/Cellar/fish/1.23.1/bin:/usr/X11R6/bin")
+
+; and add these to exec-path too, so Emacs can find git and so on
+(setq exec-path (append exec-path
+                        (split-string (getenv "PATH") ":")))
+
+(provide 'os-x-fixes)
