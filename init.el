6c588b4c96f4d664e468518cddb98cedd7ce8125
Wilfred Hughes <wilfred@potatolondon.com>
2011-09-21 14:11:27 +0100

    moving structured text configuration to a separate file

--- init.el
+++ init.el
@@ -234,20 +234,8 @@ are interchanged."
 ;
 ; always spaces, never tabs
 (setq-default indent-tabs-mode nil)
-;
-; Text formatting
-; ---------------
-;
-; csv mode stuff, since it's used extensively in GBBO
-(require 'csv-mode)
-; yaml mode stuff, since google app engine uses it
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
-(add-hook 'yaml-mode-hook
-	  '(lambda ()
-	     (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
 
+(require 'structured-text-customisations)
 (require 'html-customisations)
 (require 'css-customisations)
 (require 'python-customisations)
