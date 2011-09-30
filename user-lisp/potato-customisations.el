39e9d96c91bb0cf59c947af93f4479ba23920f3c
Wilfred Hughes <wilfred@potatolondon.com>
2011-09-30 19:45:22 +0100

    further work on App Engine deployments from within Emacs

--- user-lisp/potato-customisations.el
+++ user-lisp/potato-customisations.el
@@ -1,27 +1,28 @@
+(defvar last-email-address "" "Email address previously used")
 
-(defun set-gxbo-market (str)
-  "Write the GxBO market into app.yaml."
-  (interactive "sMarket: ")
-  (goto-char 19)
-  (delete-region (point) (line-end-position))
-  (insert str)
-  (save-buffer))
-
-(define-key yaml-mode-map [(f12)] 'set-gxbo-market)
-
-(defun deploy-gae-backend (email password market)
-  "Set app.yaml market as appropriate, then deploy the business-progress backend."
-  (interactive "sGAE email: \nsGAE password: \nsMarket: ")
-  (let ((project-root (find-gae-project-root (buffer-file-name)))
-        (app-yaml-path (concat project-root "/app.yaml")))
-    
-    (find-file app-yaml-path)
-    (set-gxbo-market market))
+(defun deploy-gae-application (email password)
+  "Deploy the current GAE application."
+  (interactive "sGAE email: \nsGAE password: ")
   
-  (let ((project-root (find-gae-project-root (buffer-file-name)))
+  (let ((project-root (vc-git-root (buffer-file-name)))
+        (process (start-process "gae_deploy" "*GAE-deploy*"
+                                "~/bin/google_appengine/appcfg.py"
+                                "update" project-root
+                                "-e" email)))
+    (process-send-string process (concat conf "\n"))
+    (switch-to-buffer "*GAE-deploy*")))
+
+(require 'yaml-mode)
+(define-key yaml-mode-map [(f12)] 'deploy-gae-application)
+
+(defun deploy-gae-backend (email password)
+  "Deploy all the backends of the current application."
+  (interactive "sGAE email: \nsGAE password: ")
+  
+  (let ((project-root (vc-git-root (buffer-file-name)))
         (process (start-process "gae_deploy" "*GAE-deploy*"
                                 "/opt/google_appengine/appcfg.py"
-                                "backends" project-root "update" "business-progress")))
+                                "backends" project-root "update")))
     (process-send-string process (concat email "\n"))
     (process-send-string process (concat password "\n"))))
 
