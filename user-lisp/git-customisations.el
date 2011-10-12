734a713c06fe0cb0b23af21cde61407753e4c355
Wilfred Hughes <wilfred@potatolondon.com>
2011-10-12 11:17:43 +0100

    fixing grep-git-project so it doesn't die on buffers without files and doesn't create a global variable

--- user-lisp/git-customisations.el
+++ user-lisp/git-customisations.el
@@ -14,15 +14,18 @@
 (require 'thingatpt) ; provides symbol-at-point
 
 (defun grep-git-project (search-term)
-  "Search a git project using git grep. The default search term is the symbol at point."
-  (interactive (list (read-from-minibuffer "Search for: "
+  "Search a git project for SEARCH-TERM. The default search term is the symbol at point.
+Note that this will not search git submodules."
+ (interactive (list (read-from-minibuffer "Search for: "
                                            (if (symbol-at-point)
                                                (symbol-name (symbol-at-point))))))
-  (setq project-root (vc-git-root (buffer-file-name)))
-  (if project-root
-      (vc-git-grep search-term "*" project-root)
-    (message "Couldn't find project root.")))
+ (if (buffer-file-name)
+     (let ((project-root (expand-file-name (vc-git-root (buffer-file-name)))))
+       (if project-root
+           (vc-git-grep search-term "*" project-root)
+         (message "Couldn't find project root.")))
+   (message "You need to be in a file buffer.")))
 
-(global-set-key [(f5)] 'grep-git-project)
+(global-set-key (kbd "<f5>") 'grep-git-project)
 
 (provide 'git-customisations)
