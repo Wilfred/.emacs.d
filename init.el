62b35fa0c164049e8e379d28191d35485349b74c
Wilfred Hughes <wilfred@potatolondon.com>
2011-10-19 17:43:36 +0100

    adding package.el 0.9 along with the marmalade package repo

--- init.el
+++ init.el
@@ -184,3 +184,15 @@
   (interactive))
 (defun suspend-frame (&rest)
   (interactive))
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
+
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
