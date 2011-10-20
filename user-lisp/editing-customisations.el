229592cb4e868692f1136481ce2cd7fdfb505832
Wilfred Hughes <wilfred@potatolondon.com>
2011-10-20 16:14:33 +0100

    courtesy of #emacs on freenode, use the kbd macro correctly

--- user-lisp/editing-customisations.el
+++ user-lisp/editing-customisations.el
@@ -20,7 +20,7 @@ Ignores CHAR at point."
 		     (backward-char direction))
 		   (point)))))
 ; bind it to the usual zap-to-char shortcut
-(global-set-key (kbd "<M-z>") 'zap-up-to-char)
+(global-set-key (kbd "M-z") 'zap-up-to-char)
 
 
 ; kill-word is less useful than kill-symbol
@@ -31,7 +31,7 @@ With argument ARG, do this that many times."
   (interactive "p")
   (kill-region (point) (progn (forward-symbol arg) (point))))
 
-(global-set-key (kbd "<M-d>") 'kill-symbol)
+(global-set-key (kbd "M-d") 'kill-symbol)
 
 
 (defun backward-kill-symbol (arg)
@@ -40,10 +40,10 @@ With argument ARG, do this that many times."
   (interactive "p")
   (kill-symbol (- arg)))
 
-(global-set-key (kbd "<M-backspace>") 'backward-kill-symbol)
+(global-set-key (kbd "M-backspace") 'backward-kill-symbol)
 
 ; to be consistent with C-M-f as forward-sexp, bind C-M-backspace to backward-kill-sexp
-(global-set-key (kbd "<C-M-backspace>") 'backward-kill-sexp)
+(global-set-key (kbd "C-M-backspace") 'backward-kill-sexp)
 
 
 (defun toggle-case-next-char ()
@@ -60,7 +60,7 @@ The point is also moved one character forward."
 
 ; toggling on char is more useful than capitalising a whole word since
 ; it doesn't break camelcase
-(global-set-key "\M-c" 'toggle-case-next-char)
+(global-set-key (kbd "M-c") 'toggle-case-next-char)
 
 
 (defun transpose-symbols (arg)
@@ -73,7 +73,7 @@ are interchanged."
   (transpose-subr 'forward-symbol arg))
 
 ; bind it to the usual transpose-word key combination
-(global-set-key (kbd "<M-t>") 'transpose-symbols)
+(global-set-key (kbd "M-t") 'transpose-symbols)
 
 
 (provide 'editing-customisations)
