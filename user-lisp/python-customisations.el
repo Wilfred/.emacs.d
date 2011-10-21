79d3090cf3028f8034a4b02fdbc351638a3a8535
Wilfred Hughes <wilfred@potatolondon.com>
2011-10-21 12:22:52 +0100

    since PyPI has been updated, use Pyflakes from PyPI

--- user-lisp/python-customisations.el
+++ user-lisp/python-customisations.el
@@ -18,7 +18,7 @@
            (local-file (file-relative-name
                         temp-file
                         (file-name-directory buffer-file-name))))
-      (list "~/.emacs.d/user-python/run-pyflakes" (list local-file))))
+      (list "pyflakes" (list local-file))))
 
 (add-to-list 'flymake-allowed-file-name-masks
              '("\\.py\\'" flymake-pyflakes-init))
