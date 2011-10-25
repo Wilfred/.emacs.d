386c15379cd7ded7461834cc39b8722f9d5c4b47
Wilfred Hughes <wilfred@potatolondon.com>
2011-10-25 11:15:13 +0100

    utility to print XML path of point

--- user-lisp/structured-text-customisations.el
+++ user-lisp/structured-text-customisations.el
@@ -17,4 +17,28 @@
 (setq auto-mode-alist
    (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
 
+; XML position utility:
+(autoload 'nxml-ensure-scan-up-to-date "nxml-rap")
+(autoload 'nxml-backward-up-element "nxml-mode")
+(autoload 'xmltok-start-tag-qname "xmltok")
+
+; taken from http://osdir.com/ml/emacs.nxml.general/2006-05/msg00013.html
+(defun show-xml-path (&optional print-message)
+  "Return all the elements in the ancestor axis of the current
+    element.  If called interactively, show it in the echo area."
+  (interactive "p")
+  (nxml-ensure-scan-up-to-date)
+  (let ((path ""))
+    (save-excursion
+      (condition-case ()
+          (while t
+            (nxml-backward-up-element)
+            (setq path (concat
+                        " / " (xmltok-start-tag-qname) path)))
+        (error nil)))
+    (when print-message
+      (message "%s" path))
+    path))
+
+
 (provide 'structured-text-customisations)
