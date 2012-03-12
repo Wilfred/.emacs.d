ca2cfe39c0fea8bb63bc54007e16be51d4f7b049
Wilfred Hughes <me@wilfred.me.uk>
2012-03-12 14:15:53 +0000

    allow more application IDs to be cycled in app.yaml

--- user-lisp/potato-customisations.el
+++ user-lisp/potato-customisations.el
@@ -15,17 +15,21 @@
   "Path to directory containing all Potato projects."
   :group 'potato-customisations)
 
-(defun gap2-toggle-application ()
-  "Toggle between gap2-staging and gap2-production in app.yaml"
+(defun gap2-cycle-application ()
+  "Cycle through gap2-staging, gap2-production and gap2-proto in app.yaml"
   (interactive)
   (save-excursion
     (set-buffer (find-file (concat potato-path
                                    "/gap2/app.yaml")))
-    (if (buffer-contains-string-p "application: gap2-staging")
-        (replace-in-buffer "application: gap2-staging" "application: gap2-production")
-      (replace-in-buffer "application: gap2-production" "application: gap2-staging"))))
+    
+    (cond ((buffer-contains-string-p "application: gap2-staging")
+           (replace-in-buffer "application: gap2-staging" "application: gap2-production"))
+          ((buffer-contains-string-p "application: gap2-production")
+           (replace-in-buffer "application: gap2-production" "application: gap2-proto"))
+          (t
+           (replace-in-buffer "application: gap2-proto" "application: gap2-staging")))))
 
 
-(define-key yaml-mode-map (kbd "<f11>") 'gap2-toggle-application)
+(define-key yaml-mode-map (kbd "<f11>") 'gap2-cycle-application)
 
 (provide 'potato-customisations)
