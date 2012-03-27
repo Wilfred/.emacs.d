dd125e2f0b81b9be53a129583a8342e5e89255a9
Wilfred Hughes <me@wilfred.me.uk>
2012-03-27 15:11:32 +0100

    increment-number and decrement-number editing utilities to speed up number tweaks

--- user-lisp/editing-customisations.el
+++ user-lisp/editing-customisations.el
@@ -82,5 +82,29 @@ are interchanged."
 ; bind it to the usual transpose-word key combination
 (global-set-key (kbd "M-t") 'transpose-symbols)
 
+(defun increment-number-decimal (&optional arg)
+  "Increment the number forward from point by ARG."
+  (interactive "p*")
+  (save-excursion
+    (save-match-data
+      (let (inc-by field-width answer)
+        (setq inc-by (if arg arg 1))
+        (skip-chars-backward "0123456789")
+        (when (re-search-forward "[0-9]+" nil t)
+          (setq field-width (- (match-end 0) (match-beginning 0)))
+          (setq answer (+ (string-to-number (match-string 0) 10) inc-by))
+          (when (< answer 0)
+            (setq answer (+ (expt 10 field-width) answer)))
+          (replace-match (format (concat "%0" (int-to-string field-width) "d")
+                                 answer)))))))
+
+(defun decrement-number-decimal (&optional arg)
+  (interactive "p*")
+  (increment-number-decimal (if arg (- arg) -1)))
+
+;; bound to Ctrl with + on the keypad
+(global-set-key (kbd "<C-kp-add>") 'increment-number-decimal)
+(global-set-key (kbd "<C-kp-subtract>") 'decrement-number-decimal)
+
 
 (provide 'editing-customisations)
