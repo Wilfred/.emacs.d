9660433c6e3e0367340aae67c96eae762aad805d
Wilfred Hughes <me@wilfred.me.uk>
2012-03-28 16:43:51 +0100

    emacs 24: removing `vc-git-grep-highlight` now that `vc-git-grep` has had Emacs bug #9408 fixed

--- user-lisp/git-customisations.el
+++ user-lisp/git-customisations.el
@@ -11,62 +11,6 @@
      (when (not window-system)
        (set-face-background 'magit-item-highlight "black"))))
 
-; vc-git-grep, fixed so it doesn't use the local color settings (Emacs bug #9408)
-(defun vc-git-grep-highlight (regexp &optional files dir)
-  "Run git grep, searching for REGEXP in FILES in directory DIR.
-The search is limited to file names matching shell pattern FILES.
-FILES may use abbreviations defined in `grep-files-aliases', e.g.
-entering `ch' is equivalent to `*.[ch]'.
-
-With \\[universal-argument] prefix, you can edit the constructed shell command line
-before it is executed.
-With two \\[universal-argument] prefixes, directly edit and run `grep-command'.
-
-Collect output in a buffer.  While git grep runs asynchronously, you
-can use \\[next-error] (M-x next-error), or \\<grep-mode-map>\\[compile-goto-error] \
-in the grep output buffer,
-to go to the lines where grep found matches.
-
-This command shares argument histories with \\[rgrep] and \\[grep]."
-  (interactive
-   (progn
-     (grep-compute-defaults)
-     (cond
-      ((equal current-prefix-arg '(16))
-       (list (read-from-minibuffer "Run: " "git grep"
-				   nil nil 'grep-history)
-	     nil))
-      (t (let* ((regexp (grep-read-regexp))
-		(files (grep-read-files regexp))
-		(dir (read-directory-name "In directory: "
-					  nil default-directory t)))
-	   (list regexp files dir))))))
-  (require 'grep)
-  (when (and (stringp regexp) (> (length regexp) 0))
-    (let ((command regexp))
-      (if (null files)
-	  (if (string= command "git grep")
-	      (setq command nil))
-	(setq dir (file-name-as-directory (expand-file-name dir)))
-	(setq command
-              ; wilfred changed this line:
-	      (grep-expand-template "git grep --no-color -n -e <R> -- <F>" regexp files))
-	(when command
-	  (if (equal current-prefix-arg '(4))
-	      (setq command
-		    (read-from-minibuffer "Confirm: "
-					  command nil nil 'grep-history))
-	    (add-to-history 'grep-history command))))
-      (when command
-	(let ((default-directory dir)
-	      (compilation-environment '("PAGER=")))
-	  ;; Setting process-setup-function makes exit-message-function work
-	  ;; even when async processes aren't supported.
-	  (compilation-start command 'grep-mode))
-	(if (eq next-error-last-buffer (current-buffer))
-	    (setq default-directory dir))))))
-
-
 (autoload 'symbol-at-point "thingatpt" nil t)
 
 ; TODO: make more generic, falling back to ack if git isn't available
@@ -79,7 +23,7 @@ Note that this will not search git submodules."
  (if (buffer-file-name)
      (let ((project-root (expand-file-name (vc-git-root (buffer-file-name)))))
        (if project-root
-           (vc-git-grep-highlight search-term "*" project-root)
+           (vc-git-grep search-term "*" project-root)
          (message "Couldn't find project root.")))
    (message "You need to be in a file buffer.")))
 
