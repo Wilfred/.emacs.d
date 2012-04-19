5639379c338783642298ed4418ed9dc98cc3c29d
Wilfred Hughes <me@wilfred.me.uk>
2012-04-19 11:27:53 +0100

    use the version of pyflakes included in .emacs.d for portability

--- user-lisp/python-customisations.el
+++ user-lisp/python-customisations.el
@@ -61,7 +61,7 @@
            (local-file (file-relative-name
                         temp-file
                         (file-name-directory buffer-file-name))))
-      (list "pyflakes" (list local-file))))
+      (list "~/.emacs.d/user-python/run-pyflakes" (list local-file))))
 
 (add-to-list 'flymake-allowed-file-name-masks
              '("\\.py\\'" flymake-pyflakes-init))
