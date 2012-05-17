35a1e523de7061134aa79c78b43e01893d2078fa
Wilfred Hughes <wilfred@openmarket.com>
2012-05-17 13:19:35 +0100

    adding comments to java-customisations

--- user-lisp/java-customisations.el
+++ user-lisp/java-customisations.el
@@ -1,17 +1,26 @@
+; configure eclim, which lets use use eclispe as a server
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/third-party-lisp/emacs-eclim"))
 (require 'eclim)
+(require 'cc-mode)
 
 (setq eclim-executable (expand-file-name "~/.eclipse/org.eclipse.platform_3.7.0_155965261/eclim"))
 
 (global-eclim-mode)
 
+; show eclim errors in minibuffer
 (setq help-at-pt-display-when-idle t)
 (setq help-at-pt-timer-delay 0.1)
 (help-at-pt-set-timer)
 
+; eclim key bindings
+(define-key java-mode-map (kbd "<f6>") 'eclim-java-find-declaration)
+
+
+; treat camelCaseWords as different words with M-f and M-b
 (add-hook 'java-mode-hook
 	  '(lambda ()
-             (camelCase-mode)
-	     (define-key java-mode-map (kbd "<f6>") 'eclim-java-find-declaration)))
+             (camelCase-mode)))
+
+
 
 (provide 'java-customisations)
