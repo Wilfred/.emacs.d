9baeb256458899136064426ed2cc7fe5e0229568
Wilfred Hughes <wilfred@openmarket.com>
2012-05-21 16:06:24 +0100

    adding regexp-utils.el to try to make elisp regexps less painful

---
+++ user-lisp/regexp-utils.el
@@ -0,0 +1,66 @@
+;;; regexp-utils.el --- making string regexps easier in Emacs
+
+;; Author: Wilfred Hughes <me@wilfred.me.uk>
+;; Created: 21 May 2012
+;; Version: 1.0
+;; Keywords: regexp, regular expression
+
+;;; Comemntary:
+
+;; Provides a collection of regular expression utilities for dealing
+;; with strings. Based on the Perl/Python standard library. All
+;; functions are case sensitive.
+
+;; All functions are case sensitive. If you're also using Emacs'
+;; standard regular expressions functions, note that these functions
+;; change the match data. You can use 'save-match-data to fix this.
+
+;; Functions of the form re--* are considered private. They're
+;; probably not what you want to use.
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+(defun re-find-all (regexp string)
+  "Find all matches of REGEXP in STRING, return a list of the
+ matching substrings."
+  (re--find-all regexp string 0))
+
+(defun re--find-all (regexp string offset)
+  "Find all matches of REGEXP in STRING, return a list of the
+ matching substrings."
+  (let* ((case-fold-search nil)
+         (match-start-index (string-match regexp string offset)))
+    (if match-start-index 
+        (cons 
+         (substring string (match-beginning 0) (match-end 0))
+         (re--find-all regexp string (match-end 0)))
+      nil)))
+
+(defun re-split (regexp string)
+  "Split STRING at separators REGEXP."
+  (let ((case-fold-search nil))
+    (split-string string regexp)))
+
+(defun re-search-p (regexp string)
+  "Returns t if REGEXP matches anywhere inside STRING."
+  (let ((case-fold-search nil))
+    (if (string-match regexp string)
+        t nil)))
