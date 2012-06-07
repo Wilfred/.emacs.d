83806e4bedfcb3488f86b7db816c60c3afb7fbb4
Wilfred Hughes <me@wilfred.me.uk>
2012-06-07 17:20:45 +0100

    use project-find-root with ack-at-point as we handle maven repos better than ack-and-a-half does

--- user-lisp/search-tools.el
+++ user-lisp/search-tools.el
@@ -1,13 +1,15 @@
 (autoload 'symbol-at-point "thingatpt")
 (autoload 'ack-and-a-half "ack-and-a-half")
 
+(autoload 'project-find-root "file-customisations")
+
 (defun ack-at-point (search-term)
   "Run ack searching for string SEARCH-TERM, defaulting to the
 current symbol at point."
  (interactive (list (read-from-minibuffer "Search for: "
                                            (if (symbol-at-point)
                                                (symbol-name (symbol-at-point))))))
- (ack search-term nil))
+ (ack search-term nil (project-find-root)))
 
 (global-set-key (kbd "<f5>") 'ack-at-point)
 
