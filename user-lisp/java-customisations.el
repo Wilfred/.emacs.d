8e84c91240999e92618e5e2b677b9483066deb5d
Wilfred Hughes <me@wilfred.me.uk>
2012-06-12 18:24:15 +0100

    minor readability tweaks to java-customisations

--- user-lisp/java-customisations.el
+++ user-lisp/java-customisations.el
@@ -1,7 +1,6 @@
 ; configure eclim, which lets use use eclispe as a server
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/third-party-lisp/emacs-eclim"))
 (require 'eclim)
-(require 'cc-mode)
 
 (setq eclim-executable (expand-file-name "~/.eclipse/org.eclipse.platform_3.7.0_155965261/eclim"))
 
@@ -9,12 +8,12 @@
 
 ; show eclim errors in minibuffer
 (setq help-at-pt-display-when-idle t)
-(setq help-at-pt-timer-delay 0.1)
+(setq help-at-pt-timer-delay 0.3)
 (help-at-pt-set-timer)
 
 ; eclim key bindings
+(require 'cc-mode); java-mode-map
 (define-key java-mode-map (kbd "<f6>") 'eclim-java-find-declaration)
-
 
 ; treat camelCaseWords as different words with M-f and M-b
 (add-hook 'java-mode-hook 'camelCase-mode)
@@ -50,7 +49,7 @@
           (compilation-mode))
       (message (format "No failed tests in %s." test-results-directory)))))
 
-(defun java-run-maven-tests ()
+(defun java-maven-run-tests ()
   "Run the test goal for the current project with maven."
   (interactive)
   (let ((pom-path (find-path-parent-directory (path-for-current-buffer) "pom.xml")))
