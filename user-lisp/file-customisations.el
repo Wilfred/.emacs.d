310c046c3791f635c417241b352ba172f1311e88
Wilfred Hughes <me@wilfred.me.uk>
2012-06-12 15:43:26 +0100

    moving file utility functions to file-utils.el

--- user-lisp/file-customisations.el
+++ user-lisp/file-customisations.el
@@ -34,13 +34,7 @@ DIRECTORY is in a git repo."
     (let ((command (format "cd %s; git ls-files" directory)))
       (split-string (shell-command-to-string command) "\n" t)))
 
-(defun path-for-current-buffer ()
-  "Find a path associated with the current buffer, if
-possible. No trailing slash. Returns nil otherwise."
-  (let ((file-name (buffer-file-name)))
-    (if file-name (directory-file-name (file-name-directory file-name))
-      (expand-file-name "."))))
-
+(autoload 'path-for-current-buffer "file-utils")
 (autoload 'ido-completing-read "ido")
 
 (defun git-pick-file ()
@@ -58,43 +52,5 @@ possible. No trailing slash. Returns nil otherwise."
       (message "This buffer isn't related to a git repo."))))
 
 (global-set-key (kbd "C-x C-g") 'git-pick-file)
-
-(defun parent-directory (path)
-  (directory-file-name (file-name-directory path)))
-
-(defun file-path-join (directory-name file-name)
-  "Join the relative FILE-NAME to DIRECTORY-NAME, adding slashes where appropriate."
-  (concat (file-name-as-directory directory-name) file-name))
-
-(defun find-containing-parent-directory (path file-name)
-  "Search PATH and all parent directories for file FILE-NAME,
-returning the first containing diretory found or nil."
-  (let* ((canonical-path (expand-file-name path))
-         (absolute-file-path (file-path-join canonical-path file-name)))
-    (cond ((file-exists-p absolute-file-path)
-           ;; success -- we've found it!
-           canonical-path)
-          ((string= canonical-path "/")
-           ;; reached root, the file doesn't exist in any ancestor directory
-           nil)
-          (t
-           ;; recurse
-           (find-containing-parent-directory
-            (parent-directory canonical-path) file-name)))))
-
-(defun find-path-parent-directory (path file-name)
-  "Search PATH and all parent directories for file FILE-NAME,
-returning the path where FILE-NAME can be found."
-  (let ((directory-path (find-containing-parent-directory path file-name)))
-    (when directory-path
-        (file-path-join directory-path file-name))))
-
-(defun project-find-root ()
-  "Find the probable root of the project for the current buffer.
-TODO: svn"
-  (let ((current-directory (expand-file-name ".")))
-    (or
-     (find-containing-parent-directory current-directory ".git")
-     (find-containing-parent-directory current-directory "pom.xml"))))
 
 (provide 'file-customisations)
---
+++ user-lisp/file-utils.el
@@ -0,0 +1,49 @@
+;;;; files related to buffers
+(defun path-for-current-buffer ()
+  "Find a directory path associated with the current buffer, if
+possible. No trailing slash. Returns nil otherwise."
+  (let ((file-name (buffer-file-name)))
+    (if file-name (directory-file-name (file-name-directory file-name))
+      (expand-file-name "."))))
+
+;;;; path manipulation
+(defun file-path-join (directory-name file-name)
+  "Join the relative FILE-NAME to DIRECTORY-NAME, adding slashes where appropriate."
+  (concat (file-name-as-directory directory-name) file-name))
+
+(defun parent-directory (path)
+  (directory-file-name (file-name-directory path)))
+
+;;;; path searching
+(defun find-containing-parent-directory (path file-name)
+  "Search PATH and all parent directories for file FILE-NAME,
+returning the first containing diretory found or nil."
+  (let* ((canonical-path (expand-file-name path))
+         (absolute-file-path (file-path-join canonical-path file-name)))
+    (cond ((file-exists-p absolute-file-path)
+           ;; success -- we've found it!
+           canonical-path)
+          ((string= canonical-path "/")
+           ;; reached root, the file doesn't exist in any ancestor directory
+           nil)
+          (t
+           ;; recurse
+           (find-containing-parent-directory
+            (parent-directory canonical-path) file-name)))))
+
+(defun find-path-parent-directory (path file-name)
+  "Search PATH and all parent directories for file FILE-NAME,
+returning the path where FILE-NAME can be found."
+  (let ((directory-path (find-containing-parent-directory path file-name)))
+    (when directory-path
+        (file-path-join directory-path file-name))))
+
+(defun project-find-root ()
+  "Find the probable root of the project for the current buffer.
+TODO: svn"
+  (let ((current-directory (expand-file-name ".")))
+    (or
+     (find-containing-parent-directory current-directory ".git")
+     (find-containing-parent-directory current-directory "pom.xml"))))
+
+(provide 'file-utils)
