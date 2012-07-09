ff21be63d2845a2b6ab60f5f0b0c9fb46920f4d2
Wilfred Hughes <me@wilfred.me.uk>
2012-07-09 18:32:44 +0100

    refactoring relative line numbers -- consistent naming, removing duplicate file

--- third-party-lisp/relative-linum.el
+++ third-party-lisp/relative-linum.el
@@ -12,33 +12,34 @@
 ;; (linum-mode 1)
 
 
-(defvar my-linum-format-string "%3d")
+(defvar relative-linum-format-string "%3d")
 
-(add-hook 'linum-before-numbering-hook 'my-linum-get-format-string)
+(add-hook 'linum-before-numbering-hook 'relative-linum-get-format-string)
 
-(defun my-linum-get-format-string ()
+(defun relative-linum-get-format-string ()
   (let* ((width (1+ (length (number-to-string
                              (count-lines (point-min) (point-max))))))
          (format (concat "%" (number-to-string width) "d")))
-    (setq my-linum-format-string format)))
+    (setq relative-linum-format-string format)))
 
-(defvar my-linum-current-line-number 0)
+(defvar relative-linum-current-line-number 0)
 
-(setq linum-format 'my-linum-relative-line-numbers)
+(setq linum-format 'relative-linum-relative-line-numbers)
 
-(defun my-linum-relative-line-numbers (line-number)
-  (let ((offset (- line-number my-linum-current-line-number)))
-    (propertize (format my-linum-format-string offset) 'face 'linum)))
+(defun relative-linum-relative-line-numbers (line-number)
+  (let ((offset (- line-number relative-linum-current-line-number)))
+    (propertize (format relative-linum-format-string offset) 'face 'linum)))
 
-(defadvice linum-update (around my-linum-update)
-  (let ((my-linum-current-line-number (line-number-at-pos)))
+(defadvice linum-update (around relative-linum-update)
+  (let ((relative-linum-current-line-number (line-number-at-pos)))
     ad-do-it))
 (ad-activate 'linum-update)
 
-(defun jump-n-lines (offset)
+(defun relative-linum-jump (offset)
+  "Move OFFSET lines up or down from the current line."
   (interactive "nLine offset: ")
   (forward-line offset))
 
-(global-set-key (kbd "C-S-n") 'jump-n-lines)
+(global-set-key (kbd "C-S-n") 'relative-linum-jump)
 
 (provide 'relative-linum)
