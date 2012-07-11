345ddfecd9d5d3f4e06b434938670c709c0c7ef1
Wilfred Hughes <me@wilfred.me.uk>
2012-07-11 14:45:16 +0100

    don't assume ack is at ack-grep

--- init.el
+++ init.el
@@ -223,7 +223,6 @@ by (current-time)."
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(frame-background-mode (quote dark))
- '(ack-and-a-half-executable (executable-find "ack-grep")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
