970633c7f1bd1d85cf811f63449296d11bf1fe09
Wilfred Hughes <me@wilfred.me.uk>
2012-07-13 15:02:02 +0100

    move recent-files configuration to file-customisations

--- init.el
+++ init.el
@@ -136,25 +136,6 @@ by (current-time)."
 (setq reb-re-syntax 'string)
 
 ;
-; offer recent files
-(require 'recentf)
-;
-; get rid of `find-file-read-only' and replace it with something
-; more useful.
-(global-set-key (kbd "C-x C-r") 'ido-recentf-open)
-;
-; enable recent files mode.
-(recentf-mode t)
-;
-; 200 files ought to be enough.
-(setq recentf-max-saved-items 200)
-;
-(defun ido-recentf-open ()
-  "Use `ido-completing-read' to \\[find-file] a recent file"
-  (interactive)
-  (if (find-file (ido-completing-read "Find recent file: " recentf-list))
-      (message "Opening file...")
-    (message "Aborting")))
 ;
 ; automatically finish quotes, brackets etc according to mode
 (require 'autopair)
--- user-lisp/file-customisations.el
+++ user-lisp/file-customisations.el
@@ -1,4 +1,26 @@
-; better dired, with colouring
+; offer recent files
+(require 'recentf)
+
+; get rid of `find-file-read-only' and replace it with something
+; more useful.
+(global-set-key (kbd "C-x C-r") 'ido-recentf-open)
+
+; enable recent files mode.
+(recentf-mode t)
+
+; 200 files ought to be enough.
+(setq recentf-max-saved-items 200)
+
+(defun ido-recentf-open ()
+  "Use `ido-completing-read' to \\[find-file] a recent file"
+  (interactive)
+
+  (if (find-file (ido-completing-read "Find recent file: " recentf-list))
+      (message "Opening file...")
+    (message "Aborting")))
+
+
+;; better dired, with colouring
 (require 'dired+)
 
 ;; deleting files should go to recycle bin
