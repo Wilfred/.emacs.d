dbace778aea4c53f7dd1963a2cbd24b86ab6c4ae
Wilfred Hughes <me@wilfred.me.uk>
2012-07-14 13:55:14 +0100

    syntax fix

--- init.el
+++ init.el
@@ -203,7 +203,7 @@ by (current-time)."
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(frame-background-mode (quote dark))
+ '(frame-background-mode (quote dark)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
