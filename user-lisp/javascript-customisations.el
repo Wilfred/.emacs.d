5ff3318ee1d7b56c7523cc6df4e7c1952d1c8de4
Wilfred Hughes <me@wilfred.me.uk>
2012-07-25 18:40:52 +0100

    indent JS with four spaces

--- user-lisp/javascript-customisations.el
+++ user-lisp/javascript-customisations.el
@@ -4,16 +4,17 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 ; indent with tab characters (treating them as eight spaces)
-(setq-default js-indent-level 8)
-(setq tab-width 8)
+(setq-default js-indent-level 4)
+(setq tab-width 4)
 
+;; these are the default settings, but it's nice to be explicit for ease of customisation
 (add-hook 'js-mode-hook
           '(lambda ()
-             (setq indent-tabs-mode t)))
+             (setq indent-tabs-mode nil)))
 (add-hook 'js2-mode-hook
           '(lambda ()
-             (setq indent-tabs-mode t)
-	     (setq js2-basic-offset 8)))
+             (setq indent-tabs-mode nil)
+	     (setq js2-basic-offset 4)))
 
 ; fix js2-mode's function parameter colour, which is too dark for a dark theme
 (custom-set-faces
