20efc1d21684c7276af84e26a8fd66659b7ce34f
Wilfred Hughes <me@wilfred.me.uk>
2012-08-04 16:58:45 +0100

    set PATH and exec-path based on shell configuration

--- init.el
+++ init.el
@@ -1,5 +1,16 @@
 (setq *emacs-load-start* (current-time))
 
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+
+This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(set-exec-path-from-shell-PATH)
+
 (add-to-list 'load-path "~/.emacs.d/third-party-lisp/")
 (add-to-list 'load-path "~/.emacs.d/user-lisp/")
 
