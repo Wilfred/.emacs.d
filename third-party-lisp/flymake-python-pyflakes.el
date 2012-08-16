a7aa48a9d5ff6c5d16669bd9e3cbc9eb249ab297
Wilfred Hughes <me@wilfred.me.uk>
2012-08-16 11:24:17 +0100

    fixing pyflakes crash if default-directory has changed
    pull request to upstream: https://github.com/purcell/flymake-python-pyflakes/pull/1

--- third-party-lisp/flymake-python-pyflakes.el
+++ third-party-lisp/flymake-python-pyflakes.el
@@ -18,9 +18,9 @@
 
 (defun flymake-python-pyflakes-init ()
   (list flymake-python-pyflakes-executable
-        (list (file-relative-name
-               (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace)
-               (file-name-directory buffer-file-name)))))
+        (list
+         (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace)
+         (file-name-directory buffer-file-name))))
 
 ;;;###autoload
 (defun flymake-python-pyflakes-load ()
