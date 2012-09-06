e8db3d162480ca31dbf3aa6ae489d2e1b3c729dc
Wilfred Hughes <me@wilfred.me.uk>
2012-09-06 18:06:28 +0100

    always preserve undo history, even if the file is reverted

--- user-lisp/file-customisations.el
+++ user-lisp/file-customisations.el
@@ -29,6 +29,26 @@
    kept-old-versions 2
    version-control t)       ; use versioned backups
 
+;; emacs doesn't actually save undo history with revert-buffer
+;; see http://lists.gnu.org/archive/html/bug-gnu-emacs/2011-04/msg00151.html
+;; fix that.
+(defun revert-buffer-keep-history (&optional IGNORE-AUTO NOCONFIRM PRESERVE-MODES)
+  (interactive)
+
+  ;; tell Emacs the modtime is fine, so we can edit the buffer
+  (clear-visited-file-modtime)
+
+  ;; insert the current contents of the file on disk
+  (widen)
+  (delete-region (point-min) (point-max))
+  (insert-file-contents (buffer-file-name))
+
+  ;; mark the buffer as not modified
+  (not-modified)
+  (set-visited-file-modtime))
+
+(setq revert-buffer-function 'revert-buffer-keep-history)
+
 ;; note there is also set-visited-file-name but this is for name changes, not path changes
 (defun rename-file-and-buffer (new-name)
   "Renames both current buffer and file it's visiting to NEW-NAME."
