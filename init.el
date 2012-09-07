0247ff0d8c50a41dc00a6db2b538ae4e09dfdfa6
Wilfred Hughes <me@wilfred.me.uk>
2012-09-07 12:37:43 +0100

    fixing C-x C-f not working with viewing /

--- init.el
+++ init.el
@@ -97,19 +97,34 @@ by (current-time)."
 (setq ido-max-directory-size 100000)
 
 
-; when using ido for opening files, show last modified first:
+;; when using ido for opening files, show last modified first:
+;; this version from http://jqian.googlecode.com/svn-history/r145/trunk/emacsconf/config/30-elisp.el
 (add-hook 'ido-make-file-list-hook 'ido-sort-mtime)
 (add-hook 'ido-make-dir-list-hook 'ido-sort-mtime)
 (defun ido-sort-mtime ()
   (setq ido-temp-list
-        (sort ido-temp-list 
-              (lambda (a b)
-                (time-less-p
-                 (sixth (file-attributes (concat ido-current-directory b)))
-                 (sixth (file-attributes (concat ido-current-directory a)))))))
+        (sort ido-temp-list
+              (lambda (a b)           ; avoid tramp
+                (cond ((and (string-match "[:\\*]$" a) (not (string-match "[:\\*]$" b)))
+                       nil)
+                      ((and (string-match "[:\\*]$" b) (not (string-match "[:\\*]$" a)))
+                         t)
+                      ((and (string-match "[:\\*]$" a) (string-match "[:\\*]$" b))
+                       nil)
+                      (t
+                         (let ((ta (nth 5 (file-attributes
+                                           (concat ido-current-directory a))))
+                               (tb (nth 5 (file-attributes
+                                           (concat ido-current-directory b)))))
+                           (cond ((and (null ta) tb) nil) ; avoid temporary buffers
+                                 ((and ta (null tb)) t)
+                                 ((and (null ta) (null tb)) nil)
+                                 (t (if (= (nth 0 ta) (nth 0 tb))
+                                        (> (nth 1 ta) (nth 1 tb))
+                                      (> (nth 0 ta) (nth 0 tb)))))))))))
   (ido-to-end  ;; move . files to end (again)
    (delq nil (mapcar
-              (lambda (x) (and (char-equal (string-to-char x) ?.) x))
+              (lambda (x) (if (string-equal (substring x 0 1) ".") x))
               ido-temp-list))))
 
 (require 'smex)
