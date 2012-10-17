5815d790b833becd9ba32cff0ef7d6fdd82c17ac
Wilfred Hughes <me@wilfred.me.uk>
2012-10-17 16:43:35 +0100

    removing git-grep-to-definition, TAGS are a better solution

--- user-lisp/git-customisations.el
+++ user-lisp/git-customisations.el
@@ -20,29 +20,7 @@ Note that this will not search git submodules."
          (message "Couldn't find project root.")))
    (message "You need to be in a file buffer.")))
 
-(defun git-grep-to-first-result (search-term)
-  "Search a git project for SEARCH-TERM, jumping to the first result."
-  (git-grep-project search-term)
-  (switch-to-buffer "*grep*")
-  (sleep-for 0.3)
-  (next-error))
-
-; TODO: jump directly to first result
-(defun git-grep-to-definition (function-or-class-name)
-  "Search a git project for the definition of a Python function or class."
-  (interactive (list (read-from-minibuffer "Find definition for: "
-                                           (if (symbol-at-point)
-                                               (symbol-name (symbol-at-point))))))
-  (let* ((first-char (substring function-or-class-name 0 1))
-         (case-fold-search nil)
-         (is-function (string-match "[a-z]" first-char)))
-    (if is-function
-        (git-grep-project (format "def %s(" function-or-class-name))
-      ; we assume all Python classes are of the form "clas Foo(bar)" not "class Foo:"
-      (git-grep-project (format "class %s(" function-or-class-name)))))
-
 (global-set-key (kbd "<f5>") 'git-grep-project)
-(global-set-key (kbd "<C-f5>") 'git-grep-to-definition)
 
 (provide 'git-customisations)
 
