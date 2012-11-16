8c98101817fc2a0ad13af19a0d778ec09bcb8e61
Wilfred Hughes <me@wilfred.me.uk>
2012-11-16 16:09:28 +0000

    moving isearch configuration to isearch-customisations.el

--- init.el
+++ init.el
@@ -135,29 +135,6 @@ by (current-time)."
 (global-set-key (kbd "C-c M-x") 'execute-extended-command)
 
 
-;; I-search with initial contents
-(defvar isearch-initial-string nil)
-
-(defun isearch-set-initial-string ()
-  (remove-hook 'isearch-mode-hook 'isearch-set-initial-string)
-  (setq isearch-string isearch-initial-string)
-  (isearch-search-and-update))
-
-; TODO: make this case-sensitive
-(defun isearch-forward-at-point (&optional regexp-p no-recursive-edit)
-  "Interactive search forward for the symbol at point."
-  (interactive "P\np")
-  (if regexp-p (isearch-forward regexp-p no-recursive-edit)
-    (let* ((end (progn (skip-syntax-forward "w_") (point)))
-           (begin (progn (skip-syntax-backward "w_") (point))))
-      (if (eq begin end)
-          (isearch-forward regexp-p no-recursive-edit)
-        (setq isearch-initial-string (buffer-substring begin end))
-        (add-hook 'isearch-mode-hook 'isearch-set-initial-string)
-        (isearch-forward regexp-p no-recursive-edit)))))
-
-(global-set-key (kbd "<f12>") 'isearch-forward-at-point)
-
 ;; make re-builder use the same regexp format as regexp-replace (no double escaping)
 (setq reb-re-syntax 'string)
 
@@ -172,6 +149,7 @@ by (current-time)."
 
 (require 'completion-customisations)
 (require 'structured-text-customisations)
+(require 'isearch-customisations)
 (require 'html-customisations)
 (require 'css-customisations)
 (require 'python-customisations)
---
+++ user-lisp/isearch-customisations.el
@@ -0,0 +1,24 @@
+;; I-search with initial contents
+(defvar isearch-initial-string nil)
+
+(defun isearch-set-initial-string ()
+  (remove-hook 'isearch-mode-hook 'isearch-set-initial-string)
+  (setq isearch-string isearch-initial-string)
+  (isearch-search-and-update))
+
+; TODO: make this case-sensitive
+(defun isearch-forward-at-point (&optional regexp-p no-recursive-edit)
+  "Interactive search forward for the symbol at point."
+  (interactive "P\np")
+  (if regexp-p (isearch-forward regexp-p no-recursive-edit)
+    (let* ((end (progn (skip-syntax-forward "w_") (point)))
+           (begin (progn (skip-syntax-backward "w_") (point))))
+      (if (eq begin end)
+          (isearch-forward regexp-p no-recursive-edit)
+        (setq isearch-initial-string (buffer-substring begin end))
+        (add-hook 'isearch-mode-hook 'isearch-set-initial-string)
+        (isearch-forward regexp-p no-recursive-edit)))))
+
+(global-set-key (kbd "<f12>") 'isearch-forward-at-point)
+
+(provide 'isearch-customisations)
