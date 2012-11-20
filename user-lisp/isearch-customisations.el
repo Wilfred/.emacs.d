cc50de77d6c0aeabe2afb26061cd723dc5d432b0
Wilfred Hughes <me@wilfred.me.uk>
2012-11-20 16:23:30 +0000

    don't delay before highlighting identical symbols

--- user-lisp/isearch-customisations.el
+++ user-lisp/isearch-customisations.el
@@ -4,6 +4,8 @@
 (add-hook 'prog-mode-hook '(lambda () (highlight-symbol-mode 1)))
 
 (global-set-key (kbd "<f12>") 'highlight-symbol-next)
+;; no delay before highlighting
+(setq highlight-symbol-idle-delay 0)
 
 (eval-after-load "isearch" '(require 'isearch+))
 
