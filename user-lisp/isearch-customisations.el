8121e79ff63b8ac24a229b6c842c47f5a008ea5c
Wilfred Hughes <me@wilfred.me.uk>
2012-11-20 16:23:43 +0000

    tweaking keybindings for navigating between symbols

--- user-lisp/isearch-customisations.el
+++ user-lisp/isearch-customisations.el
@@ -3,9 +3,11 @@
 ;; use highlight-symbol-mode in all programming modes
 (add-hook 'prog-mode-hook '(lambda () (highlight-symbol-mode 1)))
 
-(global-set-key (kbd "<f12>") 'highlight-symbol-next)
 ;; no delay before highlighting
 (setq highlight-symbol-idle-delay 0)
+
+(global-set-key (kbd "M-n") 'highlight-symbol-next)
+(global-set-key (kbd "M-p") 'highlight-symbol-prev)
 
 (eval-after-load "isearch" '(require 'isearch+))
 
