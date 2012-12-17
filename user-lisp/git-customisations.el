c900c44dda0243c207466e30f430cec8073616a9
Wilfred Hughes <me@wilfred.me.uk>
2012-12-17 23:50:14 +0000

    when creating a local branch with git, use the same name as the remote

--- user-lisp/git-customisations.el
+++ user-lisp/git-customisations.el
@@ -4,6 +4,7 @@
 (global-set-key (kbd "<f2>") 'magit-status)
 
 (setq magit-completing-read-function 'magit-ido-completing-read)
+(setq magit-default-tracking-name-function 'magit-default-tracking-name-branch-only)
 
 (autoload 'symbol-at-point "thingatpt" nil t)
 
