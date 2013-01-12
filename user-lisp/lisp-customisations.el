b51b068b8f460d95e8a56f881f980740a5d15a1e
Wilfred Hughes <me@wilfred.me.uk>
2013-01-12 18:50:49 +0000

    adding rainbow-delimeters for Clojure and Emacs lisp

--- user-lisp/lisp-customisations.el
+++ user-lisp/lisp-customisations.el
@@ -7,6 +7,23 @@
 (add-hook 'emacs-lisp-mode-hook 'turn-on-paredit)
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
 
+(require 'rainbow-delimiters)
+(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
+
+;; brighter colours, snaffled from solarized mode
+(set-face-foreground 'rainbow-delimiters-depth-1-face "white")
+(set-face-foreground 'rainbow-delimiters-depth-2-face "cyan")
+(set-face-foreground 'rainbow-delimiters-depth-3-face "yellow")
+(set-face-foreground 'rainbow-delimiters-depth-4-face "green")
+(set-face-foreground 'rainbow-delimiters-depth-5-face "blue")
+(set-face-foreground 'rainbow-delimiters-depth-6-face "normal")
+(set-face-foreground 'rainbow-delimiters-depth-7-face "cyan")
+(set-face-foreground 'rainbow-delimiters-depth-8-face "yellow")
+(set-face-foreground 'rainbow-delimiters-depth-9-face "green")
+(set-face-foreground 'rainbow-delimiters-unmatched-face "red")
+
+
 (autoload 'clojure-mode "clojure-mode")
 (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
 
