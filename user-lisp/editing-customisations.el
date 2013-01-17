b5ea2aff53a8fef03f0f613930f8ae9f97e955d5
Wilfred Hughes <me@wilfred.me.uk>
2013-01-17 11:38:59 +0000

    moving move-text commands to move-text.el

--- user-lisp/editing-customisations.el
+++ user-lisp/editing-customisations.el
@@ -183,42 +183,7 @@ are interchanged."
 (require 'expand-region)
 (global-set-key (kbd "C-=") 'er/expand-region)
 
-;; move the current line or active region up/down, NetBeans style
-;; from http://groups.google.com/group/gnu.emacs.help/msg/a784fbb684a24e17
-;; FIXME: interacts badly with paredit-mode
-(defun move-text-internal (arg)
-   (cond
-    ((and mark-active transient-mark-mode)
-     (if (> (point) (mark))
-        (exchange-point-and-mark))
-     (let ((column (current-column))
-          (text (delete-and-extract-region (point) (mark))))
-       (forward-line arg)
-       (move-to-column column t)
-       (set-mark (point))
-       (insert text)
-       (exchange-point-and-mark)
-       (setq deactivate-mark nil)))
-    (t
-     (beginning-of-line)
-     (when (or (> arg 0) (not (bobp)))
-       (forward-line)
-       (when (or (< arg 0) (not (eobp)))
-        (transpose-lines arg))
-       (forward-line -1)))))
-
-(defun move-text-down (arg)
-   "Move region (transient-mark-mode active) or current line
-  arg lines down."
-   (interactive "*p")
-   (move-text-internal arg))
-
-(defun move-text-up (arg)
-   "Move region (transient-mark-mode active) or current line
-  arg lines up."
-   (interactive "*p")
-   (move-text-internal (- arg)))
-
+(require 'move-text)
 (global-set-key (kbd "<M-up>") 'move-text-up)
 (global-set-key (kbd "<M-down>") 'move-text-down)
 
