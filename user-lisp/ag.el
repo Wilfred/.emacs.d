2b5d534f33a11568b2038d0db40278e4ccdc254e
Wilfred Hughes <me@wilfred.me.uk>
2013-01-22 15:59:24 +0000

    bumping version of ag.el

--- user-lisp/ag.el
+++ user-lisp/ag.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Wilfred Hughes <me@wilfred.me.uk>
 ;; Created: 11 January 2012
-;; Version: 0.7
+;; Version: 0.10
 
 ;;; Commentary
 
@@ -20,11 +20,6 @@
 ;; I like to bind ag-project-at-point to F5:
 
 ;; (global-set-key (kbd "<f5>") 'ag-project-at-point)
-
-;;; Todo:
-
-;; 1. Add ag-regexp
-;; 2. Add highlighting to *Ag* buffer
 
 ;;; License:
 
@@ -47,14 +42,20 @@
 ;; Boston, MA 02110-1301, USA.
 
 (defcustom ag-arguments
-  (list "--nocolor" "--literal" "--smart-case" "--nogroup" "--column")
+  (list "--color" "--color-match" "'30;43'" "--smart-case" "--nogroup" "--column")
   "Default arguments passed to ag."
   :type '(repeat (string)))
 
 (require 'compile)
 
+(defvar ag-match-face 'match
+  "Face name to use for grep matches.")
+
+
 (define-compilation-mode ag-mode "Ag"
-  "Ag results compilation mode")
+  "Ag results compilation mode"
+  (setq ag-last-buffer (current-buffer))
+  (add-hook 'compilation-filter-hook 'ag-filter nil t))
 
 (defun ag/s-join (separator strings)
   "Join all the strings in STRINGS with SEPARATOR in between."
@@ -68,12 +69,18 @@
   "Wrap in single quotes, and quote existing single quotes to make shell safe."
   (concat "'" (ag/s-replace "'" "'\\''" string) "'"))
 
-(defun ag/search (string directory)
-  "Run ag searching for the literal STRING given in DIRECTORY."
-  (let ((default-directory (file-name-as-directory directory)))
+(defun ag/search (string directory &optional regexp)
+  "Run ag searching for the STRING given in DIRECTORY. If REGEXP
+is non-nil, treat STRING as a regular expression."
+  (let ((default-directory (file-name-as-directory directory))
+        (arguments (if regexp
+                       ag-arguments
+                     (cons "--literal" ag-arguments))))
+    (unless (file-exists-p default-directory)
+      (error "No such directory %s" default-directory))
     (compilation-start
      (ag/s-join " "
-                (append '("ag") ag-arguments (list (ag/shell-quote string))))
+                (append '("ag") arguments (list (ag/shell-quote string))))
      'ag-mode)))
 
 (autoload 'vc-git-root "vc-git")
@@ -90,6 +97,11 @@
   (interactive "sSearch string: \nDDirectory: ")
   (ag/search string directory))
 
+(defun ag-regexp (string directory)
+  "Search using ag in a given directory for a given string."
+  (interactive "sSearch regexp: \nDDirectory: ")
+  (ag/search string directory t))
+
 (defun ag-project (string)
   "Guess the root of the current project and search it with ag
 for the given string."
@@ -105,5 +117,29 @@ to the symbol under point."
                                            (if (symbol-at-point)
                                                (symbol-name (symbol-at-point))))))
    (ag/search string (ag/project-root default-directory)))
+
+;; Taken from grep-filter, just changed the color regex.
+(defun ag-filter ()
+  "Handle match highlighting escape sequences inserted by the grep process.
+This function is called from `compilation-filter-hook'."
+  (save-excursion
+    (forward-line 0)
+    (let ((end (point)) beg)
+      (goto-char compilation-filter-start)
+      (forward-line 0)
+      (setq beg (point))
+      ;; Only operate on whole lines so we don't get caught with part of an
+      ;; escape sequence in one chunk and the rest in another.
+      (when (< (point) end)
+        (setq end (copy-marker end))
+        ;; Highlight grep matches and delete marking sequences.
+        (while (re-search-forward "\033\\[30;43m\\(.*?\\)\033\\[[0-9]*m" end 1)
+          (replace-match (propertize (match-string 1)
+                                     'face nil 'font-lock-face ag-match-face)
+                         t t))
+        ;; Delete all remaining escape sequences
+        (goto-char beg)
+        (while (re-search-forward "\033\\[[0-9;]*[mK]" end 1)
+          (replace-match "" t t))))))
 
 (provide 'ag)
