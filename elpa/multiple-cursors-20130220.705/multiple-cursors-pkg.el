d9cee3771c34399d0403a79944ea41b42a2546c6
Wilfred Hughes <me@wilfred.me.uk>
2013-02-25 14:13:53 +0000

    moving multiple-cursors to use the package available

---
+++ elpa/multiple-cursors-20130220.705/multiple-cursors-pkg.el
@@ -0,0 +1,2 @@
+(define-package "multiple-cursors" "20130220.705" "Multiple cursors for Emacs. [source: github]" 'nil)
+
--- third-party-lisp/multiple-cursors/multiple-cursors-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "multiple-cursors" "1.1.5"
-                "Multiple cursors for Emacs.")
