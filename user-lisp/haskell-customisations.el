8eb1fe956aa7799a8f1e131e80d34019498c85d5
Wilfred Hughes <me@wilfred.me.uk>
2013-02-25 22:22:03 +0000

    calling load on haskell-mode's haskell-site-file is unnecessary after installing with MELPA

--- user-lisp/haskell-customisations.el
+++ user-lisp/haskell-customisations.el
@@ -1,5 +1,3 @@
-(load "~/.emacs.d/third-party-lisp/haskell-mode/haskell-site-file")
-
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
