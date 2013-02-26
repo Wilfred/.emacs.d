d2ffeb1db583787a70de2796ec976521d17561e9
Wilfred Hughes <me@wilfred.me.uk>
2013-02-26 16:57:07 +0000

    bumping less-css version

--- elpa/less-css-mode-20121108.1718/less-css-mode-autoloads.el
+++ elpa/less-css-mode-20121108.1718/less-css-mode-autoloads.el
@@ -3,9 +3,14 @@
 ;;; Code:
 
 
-;;;### (autoloads (less-css-mode) "less-css-mode" "less-css-mode.el"
-;;;;;;  (20133 38174))
+;;;### (autoloads (flymake-less-css-init less-css-mode less-css-compile)
+;;;;;;  "less-css-mode" "less-css-mode.el" (20779 37883))
 ;;; Generated autoloads from less-css-mode.el
+
+(autoload 'less-css-compile "less-css-mode" "\
+Compiles the current buffer to css using `less-css-lessc-command'.
+
+\(fn)" t nil)
 
 (autoload 'less-css-mode "less-css-mode" "\
 Major mode for editing LESS files, http://lesscss.org/
@@ -16,9 +21,14 @@ Special commands:
 
 (add-to-list 'auto-mode-alist '("\\.less" . less-css-mode))
 
+(autoload 'flymake-less-css-init "less-css-mode" "\
+Flymake support for LESS files
+
+\(fn)" nil nil)
+
 ;;;***
 
-;;;### (autoloads nil nil ("less-css-mode-pkg.el") (20133 38174 92957))
+;;;### (autoloads nil nil ("less-css-mode-pkg.el") (20779 37883 928177))
 
 ;;;***
 
