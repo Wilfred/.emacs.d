2db1653a6c6a5046575e5fbd183a7971659cd28c
Wilfred Hughes <me@wilfred.me.uk>
2013-02-27 14:49:31 +0000

    adding ht.el from melpa

---
+++ elpa/ht-20130220.1423/ht-autoloads.el
@@ -0,0 +1,18 @@
+;;; ht-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("ht-pkg.el" "ht-tests.el" "ht.el") (20772
+;;;;;;  56496 323852))
+
+;;;***
+
+(provide 'ht-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ht-autoloads.el ends here
---
+++ elpa/ht-20130220.1423/ht-pkg.el
@@ -0,0 +1,2 @@
+(define-package "ht" "20130220.1423" "The missing hash table library for Emacs [source: github]" 'nil)
+
