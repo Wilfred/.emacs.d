9f5aa7def2f883b85884930edfea25173afc4dbd
Wilfred Hughes <me@wilfred.me.uk>
2013-03-12 14:32:31 +0000

    bump emacs-eclim version

--- elpa/emacs-eclim-20130302.1031/emacs-eclim-pkg.el
+++
@@ -1,3 +0,0 @@
-(define-package "emacs-eclim" "20130302.1031" "An interface to the Eclipse IDE. [source: github]"
-  '((json "1.2")))
-
---
+++ elpa/emacs-eclim-20130310.1237/emacs-eclim-pkg.el
@@ -0,0 +1,3 @@
+(define-package "emacs-eclim" "20130310.1237" "An interface to the Eclipse IDE. [source: github]"
+  '((json "1.2")))
+
