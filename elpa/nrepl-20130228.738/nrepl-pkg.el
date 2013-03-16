2d3074a379293f877d750bf34066ec10d3b0e785
Wilfred Hughes <me@wilfred.me.uk>
2013-03-16 19:12:38 +0000

    bumping nrepl version

--- elpa/nrepl-20130228.738/nrepl-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "nrepl" "20130228.738" "Client for Clojure nREPL [source: github]" (quote ((clojure-mode "2.0.0"))))
--- elpa/nrepl-20130313.2127/nrepl-autoloads.el
+++ elpa/nrepl-20130313.2127/nrepl-autoloads.el
@@ -5,7 +5,7 @@
 
 ;;;### (autoloads (nrepl nrepl-jack-in nrepl-disable-on-existing-clojure-buffers
 ;;;;;;  nrepl-enable-on-existing-clojure-buffers nrepl-interaction-mode)
-;;;;;;  "nrepl" "nrepl.el" (20794 700))
+;;;;;;  "nrepl" "nrepl.el" (20804 50186))
 ;;; Generated autoloads from nrepl.el
 
 (autoload 'nrepl-interaction-mode "nrepl" "\
@@ -45,7 +45,7 @@ Connect nrepl to HOST and PORT.
 
 ;;;***
 
-;;;### (autoloads nil nil ("nrepl-pkg.el") (20794 700 916093))
+;;;### (autoloads nil nil ("nrepl-pkg.el") (20804 50186 855093))
 
 ;;;***
 
