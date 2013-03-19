650e3e3c6ac1afa1c1103264c8c064025e33f579
Wilfred Hughes <me@wilfred.me.uk>
2013-03-19 12:02:39 +0000

    virtualenv-workon should check that the current virtualenv root exists before crashing

--- user-lisp/python-utils.el
+++ user-lisp/python-utils.el
@@ -7,6 +7,8 @@
 (defun virtualenv-workon ()
   "Convenience for setting `python-shell-virtualenv-path'."
   (interactive)
+  (unless (file-exists-p virtualenvs-root)
+    (error "`virtualenvs-root' is set to %s, which doesn't exist" virtualenvs-root))
   (let* ((virtualenv-names
           (no-dot-directories (directory-files virtualenvs-root)))
          (virtualenv-name (ido-completing-read "Virtualenv: " virtualenv-names))
