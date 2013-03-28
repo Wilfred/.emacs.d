e1da09ab383dd73a24333858c8233514f678710d
Wilfred Hughes <me@wilfred.me.uk>
2013-03-28 16:20:17 +0000

    bumping auto-complete version

--- elpa/auto-complete-20130324.1306/auto-complete-autoloads.el
+++ elpa/auto-complete-20130324.1306/auto-complete-autoloads.el
@@ -4,7 +4,7 @@
 
 
 ;;;### (autoloads (auto-complete) "auto-complete" "auto-complete.el"
-;;;;;;  (20800 34173))
+;;;;;;  (20820 19832 990038 370000))
 ;;; Generated autoloads from auto-complete.el
 
 (autoload 'auto-complete "auto-complete" "\
@@ -15,7 +15,7 @@ Start auto-completion at current point.
 ;;;***
 
 ;;;### (autoloads nil nil ("auto-complete-config.el" "auto-complete-pkg.el")
-;;;;;;  (20800 34174 482796))
+;;;;;;  (20820 19833 834828 7000))
 
 ;;;***
 
