33a77d45e7022cf798ac98dbb16a475e1322b061
Wilfred Hughes <me@wilfred.me.uk>
2013-04-24 15:39:16 +0100

    modifying virtualenv-search to respect the current region, and set the initial search term to be 'def foo' or 'class Foo'

--- user-lisp/python-utils.el
+++ user-lisp/python-utils.el
@@ -1,5 +1,7 @@
 (require 'file-utils)
 (require 'ag)
+(require 'dash)
+(require 's)
 
 (defcustom virtualenvs-root (expand-file-name "~/.envs")
   "Absolute path to the directory that contains all the virtualenvs.")
@@ -15,6 +17,17 @@
          (virtualenv-path (file-path-join virtualenvs-root virtualenv-name)))
     (setq python-shell-virtualenv-path virtualenv-path)))
 
+(defun virtualenv-search--dwim-at-point ()
+  "If there's an active selection, return that.
+Otherwise, get the symbol at point and return a search term for its definition."
+  (if (use-region-p)
+      (buffer-substring-no-properties (region-beginning) (region-end))
+    (-when-let* ((symbol (symbol-at-point))
+                 (symbol-name (symbol-name symbol)))
+      (let* ((case-fold-search nil)
+             (is-class-name (s-matches-p "^[A-Z]" symbol-name)))
+        (format "%s %s" (if is-class-name "class" "def") symbol-name)))))
+
 (defun virtualenv-search ()
   "Search the source code in the current virtualenv for
 a specific search string."
@@ -22,8 +35,7 @@ a specific search string."
   (unless python-shell-virtualenv-path
     (error "Need to set `python-shell-virtualenv-path', see `virtualenv-workon'"))
   (let ((search-term (read-from-minibuffer "Search virtualenv for: "
-                                           (if (symbol-at-point)
-                                               (symbol-name (symbol-at-point)))))
+                                           (virtualenv-search--dwim-at-point)))
         (libraries-path
          (file-path-join python-shell-virtualenv-path "lib/python2.7/site-packages")))
     (ag/search search-term libraries-path)))
