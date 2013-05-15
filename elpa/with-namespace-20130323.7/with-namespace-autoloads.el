fcfda39e2603718a76519eb314a6d34cf9b37227
Wilfred Hughes <me@wilfred.me.uk>
2013-05-15 14:42:20 +0100

    removing with-namespace

--- elpa/with-namespace-20130323.7/with-namespace-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; with-namespace-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("with-namespace-pkg.el" "with-namespace.el")
-;;;;;;  (20820 19780 323610 598000))
-
-;;;***
-
-(provide 'with-namespace-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; with-namespace-autoloads.el ends here
--- elpa/with-namespace-20130323.7/with-namespace-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "with-namespace" "20130323.7" "interoperable elisp namespaces [source: github]" (quote ((dash "1.1.0") (loop "1.1"))))
