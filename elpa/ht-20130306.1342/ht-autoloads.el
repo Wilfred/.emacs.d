f9e1a0c1dd2a910c848fa8951409eea4a64d9ec9
Wilfred Hughes <me@wilfred.me.uk>
2013-05-16 19:09:07 +0100

    removing old version of ht

--- elpa/ht-20130306.1342/ht-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; ht-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("ht-pkg.el" "ht.el") (20794 714 452956))
-
-;;;***
-
-(provide 'ht-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; ht-autoloads.el ends here
--- elpa/ht-20130306.1342/ht-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ht" "20130306.1342" "The missing hash table library for Emacs [source: github]" (quote nil))
