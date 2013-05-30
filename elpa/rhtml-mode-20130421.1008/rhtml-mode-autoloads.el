9ddc85d959ea11006013beca6939475da9b1b695
Wilfred Hughes <me@wilfred.me.uk>
2013-05-30 11:29:16 +0100

    installing rhtml for editing erb templates

---
+++ elpa/rhtml-mode-20130421.1008/rhtml-mode-autoloads.el
@@ -0,0 +1,32 @@
+;;; rhtml-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (rhtml-mode) "rhtml-mode" "rhtml-mode.el" (20891
+;;;;;;  29764 293769 344000))
+;;; Generated autoloads from rhtml-mode.el
+
+(autoload 'rhtml-mode "rhtml-mode" "\
+Embedded Ruby Mode (RHTML)
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.html\\.erb$" . rhtml-mode))
+
+;;;***
+
+;;;### (autoloads nil nil ("rhtml-erb.el" "rhtml-fonts.el" "rhtml-mode-pkg.el"
+;;;;;;  "rhtml-navigation.el" "rhtml-ruby-hook.el" "rhtml-sgml-hacks.el")
+;;;;;;  (20891 29764 441607 612000))
+
+;;;***
+
+(provide 'rhtml-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; rhtml-mode-autoloads.el ends here
