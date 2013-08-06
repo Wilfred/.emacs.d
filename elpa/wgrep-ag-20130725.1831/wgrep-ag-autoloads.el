aba1f896a404a46a91de1a7b4d2cdc48446e4772
Wilfred Hughes <me@wilfred.me.uk>
2013-08-06 17:15:18 +0100

    installing wgrep-ag.el

---
+++ elpa/wgrep-ag-20130725.1831/wgrep-ag-autoloads.el
@@ -0,0 +1,30 @@
+;;; wgrep-ag-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (wgrep-ag-setup) "wgrep-ag" "wgrep-ag.el" (20983
+;;;;;;  48024 75348 676000))
+;;; Generated autoloads from wgrep-ag.el
+
+(autoload 'wgrep-ag-setup "wgrep-ag" "\
+
+
+\(fn)" nil nil)
+(add-hook 'ag-mode-hook 'wgrep-ag-setup)
+
+;;;***
+
+;;;### (autoloads nil nil ("wgrep-ag-pkg.el") (20983 48024 165907
+;;;;;;  618000))
+
+;;;***
+
+(provide 'wgrep-ag-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; wgrep-ag-autoloads.el ends here
