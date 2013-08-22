120e258b864bb1cce2f6042430d5a513d200a284
Wilfred Hughes <me@wilfred.me.uk>
2013-08-22 15:40:13 +0100

    moving to rainbow-mode for CSS colouring, since it supports all colour formats

--- user-lisp/css-customisations.el
+++ user-lisp/css-customisations.el
@@ -8,17 +8,7 @@
                (setq css-indent-offset 4)
                (setq indent-tabs-mode nil)))))
 
-; highlight colours as themselves
-(defvar hexcolour-keywords
-  '(("#[abcdef[:digit:]]\\{6\\}"
-     (0 (put-text-property
-         (match-beginning 0)
-         (match-end 0)
-         'face (list :background
-                     (match-string-no-properties 0)))))))
-(defun hexcolour-add-to-font-lock ()
-  (font-lock-add-keywords nil hexcolour-keywords))
-
-(add-hook 'css-mode-hook 'hexcolour-add-to-font-lock)
+;; highlight colour values to show the colour they represent
+(add-hook 'css-mode-hook 'rainbow-mode)
 
 (provide 'css-customisations)
