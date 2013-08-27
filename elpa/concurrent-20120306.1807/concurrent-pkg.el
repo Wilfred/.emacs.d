7c33b3b7b400655f9da7dbd6601be24c938a5b70
Wilfred Hughes <me@wilfred.me.uk>
2013-08-27 12:07:25 +0100

    removing concurrent, deferred and ctable, since they were only used by jedi

--- elpa/concurrent-20120306.1807/concurrent-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "concurrent" "20120306.1807" "Concurrent utility functions for emacs lisp [source: github]" (quote ((deferred "0.3.1"))))
--- elpa/ctable-20130313.1743/ctable-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ctable" "20130313.1743" "Table component for Emacs Lisp [source: github]" (quote nil))
