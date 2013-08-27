d63bf5b479a6aaca6d553d71c6f3ac3a6f05918e
Wilfred Hughes <me@wilfred.me.uk>
2013-08-27 12:00:46 +0100

    version bump haskell-mode

--- elpa/haskell-mode-20130505.255/haskell-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "haskell-mode" "20130505.255" "A Haskell editing mode [github]" 'nil)
-
---
+++ elpa/haskell-mode-20130728.1338/haskell-mode-pkg.el
@@ -0,0 +1,2 @@
+(define-package "haskell-mode" "20130728.1338" "A Haskell editing mode [github]" 'nil)
+
