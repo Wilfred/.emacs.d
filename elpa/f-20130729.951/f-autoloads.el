4e4fd971ce8592ddb068c82dc52cf9cab4a7ff26
Wilfred Hughes <me@wilfred.me.uk>
2013-09-11 18:04:37 +0100

    Removing an old (duplicate) version of f.el

--- elpa/f-20130729.951/f-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; f-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("f-pkg.el" "f.el") (20982 16994 787639
-;;;;;;  157000))
-
-;;;***
-
-(provide 'f-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; f-autoloads.el ends here
