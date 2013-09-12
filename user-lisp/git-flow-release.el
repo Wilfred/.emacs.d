b10df2bf9de1629244dd657801ef3a952e688b77
Wilfred Hughes <me@wilfred.me.uk>
2013-09-12 16:25:09 +0100

    Adding the usual module header/footer to git-flow-release.el

--- user-lisp/git-flow-release.el
+++ user-lisp/git-flow-release.el
@@ -1,3 +1,33 @@
+;;; git-flow-release.el --- Automating git-flow release
+
+;; Copyright (C) 2013 Wilfred Hughes <me@wilfred.me.uk>
+;;
+;; Author: Wilfred Hughes <me@wilfred.me.uk>
+;; Created: 12 September 2013
+;; Version: 1.0
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
 (require 's)
 (require 'vc-git)
 (require 'execute-commands)
@@ -46,3 +76,4 @@
                       "git checkout develop")))
 
 (provide 'git-flow-release)
+;;; git-flow-release.el ends here
