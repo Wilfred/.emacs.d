421957fbdf15c77a0144e69745040f32c137858d
Wilfred Hughes <me@wilfred.me.uk>
2013-09-18 15:28:20 +0100

    Removing some unused functions (better to use f.el anyway)

--- user-lisp/file-utils.el
+++ user-lisp/file-utils.el
@@ -8,17 +8,6 @@ possible. No trailing slash. Returns nil otherwise."
     (if file-name (directory-file-name (file-name-directory file-name))
       default-directory)))
 
-;;;; path manipulation
-(defun parent-directory (path)
-  (directory-file-name (file-name-directory path)))
-
-;;;; file creation
-(defun file-create-file (path content)
-  "Create a file at PATH with CONTENT as its contents."
-  (with-temp-buffer
-    (insert content)
-    (write-region (point-min) (point-max) path)))
-
 ;;;; path searching
 
 (defun find-path-parent-directory (path file-name)
@@ -30,14 +19,6 @@ returning the path where FILE-NAME can be found."
 
 (autoload 'vc-git-root "vc-git")
 (autoload 'vc-svn-root "vc-svn")
-
-(defun file-find-project-root (path)
-  "Find the probable root of the project for the current buffer."
-  (or
-   (vc-git-root path)
-   (vc-svn-root path)
-   (locate-dominating-file path "pom.xml")
-   (error "%s doesn't seem to be part of a project" path)))
 
 (autoload '--remove "dash" nil t)
 
