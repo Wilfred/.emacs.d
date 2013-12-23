1b7b84363d5fe5c92f04d9ac18bb99b2947dd583
Wilfred Hughes <me@wilfred.me.uk>
2013-12-23 20:46:23 +0000

    don't modify the cursor in god-mode

--- user-lisp/god-mode-customisations.el
+++ user-lisp/god-mode-customisations.el
@@ -2,11 +2,4 @@
 
 (global-set-key (kbd "<escape>") 'god-mode-all)
 
-
-(defun my-update-cursor ()
-  (setq cursor-type (if (or god-local-mode god-global-mode)
-                        'bar
-                      'box)))
-
-(add-hook 'god-mode-enabled-hook 'my-update-cursor)
-(add-hook 'god-mode-disabled-hook 'my-update-cursor)
+(setq god-exempt-major-modes '())
