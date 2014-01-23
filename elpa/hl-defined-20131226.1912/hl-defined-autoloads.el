18603a2f210c93ad4a25a9a77252e73f371443c9
Wilfred Hughes <me@wilfred.me.uk>
2014-01-23 23:36:12 +0000

    Installing hl-defined.

---
+++ elpa/hl-defined-20131226.1912/hl-defined-autoloads.el
@@ -0,0 +1,18 @@
+;;; hl-defined-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("hl-defined-pkg.el" "hl-defined.el") (21217
+;;;;;;  41765 780796 720000))
+
+;;;***
+
+(provide 'hl-defined-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; hl-defined-autoloads.el ends here
---
+++ elpa/hl-defined-20131226.1912/hl-defined-pkg.el
@@ -0,0 +1,1 @@
+(define-package "hl-defined" "20131226.1912" "Highlight defined or undefined symbols in Emacs-Lisp." (quote nil))
