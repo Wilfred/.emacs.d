16ff32525751d0cda8ceb74753050e34e0f87f46
Wilfred Hughes <me@wilfred.me.uk>
2014-02-11 23:51:49 +0000

    Installing gist.

---
+++ .cask/24.3.1/bootstrap/f-20131225.721/f-autoloads.el
@@ -0,0 +1,18 @@
+;;; f-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("f-pkg.el" "f.el") (21216 21133 690782
+;;;;;;  643000))
+
+;;;***
+
+(provide 'f-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; f-autoloads.el ends here
---
+++ elpa/logito-20120225.1255/logito-pkg.el
@@ -0,0 +1,1 @@
+(define-package "logito" "20120225.1255" "logging library for Emacs" (quote ((eieio "1.3"))))
