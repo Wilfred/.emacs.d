39bc254de7db18f982dea9d2fb20363f11f26838
Wilfred Hughes <me@wilfred.me.uk>
2014-02-15 16:05:43 +0000

    Disabling autoquoting inside strings in smartparens.

--- init.el
+++ init.el
@@ -173,6 +173,8 @@ If a prefix argument is given, don't change the kill-ring."
 (require 'smartparens-config)
 (add-hook 'prog-mode-hook 'smartparens-mode)
 
+(setq sp-autoescape-string-quote nil)
+
 (require 'recentf)
 
 ;; offer recently accessed files from the menu
