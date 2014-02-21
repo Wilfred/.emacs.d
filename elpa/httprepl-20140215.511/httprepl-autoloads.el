0e48a3408c476816d0b60af88d11ae42fec5f8fb
Wilfred Hughes <me@wilfred.me.uk>
2014-02-21 17:39:10 +0000

    Installing HTTP repl for HTTP API testing.

---
+++ elpa/httprepl-20140215.511/httprepl-autoloads.el
@@ -0,0 +1,32 @@
+;;; httprepl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (httprepl) "httprepl" "httprepl.el" (21255 32635
+;;;;;;  340105 778000))
+;;; Generated autoloads from httprepl.el
+
+(autoload 'httprepl "httprepl" "\
+Interactively evaluate HTTP requests at a REPL-like
+interface. Switches to the buffer specified by
+`httprepl-buffer-name', or creates it if it does not exist. See
+`httprepl-mode' for a reference on constructing requests.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("httprepl-pkg.el") (21255 32635 620062
+;;;;;;  23000))
+
+;;;***
+
+(provide 'httprepl-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; httprepl-autoloads.el ends here
