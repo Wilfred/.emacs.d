915b12d73c44d7630e75b77265465cf6ff272948
Wilfred Hughes <me@wilfred.me.uk>
2014-03-15 16:41:03 +0000

    Installing swoop.

---
+++ elpa/async-20140311.747/async-pkg.el
@@ -0,0 +1,1 @@
+(define-package "async" "20140311.747" "Asynchronous processing in Emacs" 'nil)
---
+++ elpa/swoop-20140315.744/swoop-autoloads.el
@@ -0,0 +1,67 @@
+;;; swoop-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (swoop-from-evil-search swoop-function swoop-from-isearch
+;;;;;;  swoop-line-length-over80 swoop-migemo swoop-pcre-regexp swoop-multi
+;;;;;;  swoop) "swoop" "swoop.el" (21284 32992 317169 142000))
+;;; Generated autoloads from swoop.el
+
+(autoload 'swoop "swoop" "\
+Search through words within the current buffer.
+
+\(fn &optional $QUERY)" t nil)
+
+(autoload 'swoop-multi "swoop" "\
+Search words across currently opened multiple buffers.
+Ignore non file buffer.
+
+\(fn &optional $QUERY)" t nil)
+
+(autoload 'swoop-pcre-regexp "swoop" "\
+Use PCRE like regexp to swoop.
+
+\(fn &optional $QUERY)" t nil)
+
+(autoload 'swoop-migemo "swoop" "\
+Japanese words matching with the alphabet.
+
+\(fn &optional $QUERY)" t nil)
+
+(autoload 'swoop-line-length-over80 "swoop" "\
+Get over 80 colomn number linees.
+
+\(fn)" t nil)
+
+(autoload 'swoop-from-isearch "swoop" "\
+During isearch, switch over to swoop.
+
+\(fn)" t nil)
+
+(autoload 'swoop-function "swoop" "\
+Show function list in buffer judging from major-mode and regexp.
+Currently c-mode only.
+
+\(fn &optional $QUERY)" t nil)
+
+(autoload 'swoop-from-evil-search "swoop" "\
+During evil-search, switch over to swoop.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("swoop-edit.el" "swoop-lib.el" "swoop-pkg.el")
+;;;;;;  (21284 32992 374116 421000))
+
+;;;***
+
+(provide 'swoop-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; swoop-autoloads.el ends here
