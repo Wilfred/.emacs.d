cd92fd45f26e153c3b1d541b733e247664237f72
Wilfred Hughes <me@wilfred.me.uk>
2014-03-18 18:36:01 +0000

    Convenience command for finding backup files.

---
+++ user-lisp/backup-customisations.el
@@ -0,0 +1,7 @@
+(require 'f)
+
+(defun backup-each-save-show ()
+  "Show the directory (using dired) containing backups of the current buffer."
+  (interactive)
+  (dired
+   (f-dirname (f-expand (backup-each-save-compute-location (buffer-file-name))))))
