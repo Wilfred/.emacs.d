c7865901f1911b63441a0637c9fb219618e9fedd
Wilfred Hughes <me@wilfred.me.uk>
2014-03-23 10:51:15 +0000

    When indenting Trifle code, consider all the parens on the current line.

--- user-lisp/trifle-mode.el
+++ user-lisp/trifle-mode.el
@@ -62,6 +62,7 @@ For every level of parentheses, indent by two spaces."
     ;; Calculate sexp depth by calling sp-up-sexp until we cannot go
     ;; up any further.
     (save-excursion
+      (end-of-line)
       (while (sp-up-sexp)
         (incf sexp-depth)))
 
