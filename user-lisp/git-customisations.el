3426c450b86de1dc577be1b191c66e90ed13bff0
Wilfred Hughes <me@wilfred.me.uk>
2014-04-02 18:40:20 +0100

    Don't switch on auto-revert-mode for files in git repos.

--- user-lisp/git-customisations.el
+++ user-lisp/git-customisations.el
@@ -1,9 +1,11 @@
-(autoload 'magit-status "magit")
+(require 'magit)
 
 (global-set-key (kbd "<f2>") 'magit-status)
 
 (setq magit-completing-read-function 'magit-ido-completing-read)
 (setq magit-default-tracking-name-function 'magit-default-tracking-name-branch-only)
+
+(setq magit-turn-on-auto-revert-mode nil)
 
 (provide 'git-customisations)
 
