e4ecce43f924b963f82cece368e5353d9559bfc3
Wilfred Hughes <me@wilfred.me.uk>
2014-04-04 23:54:38 +0100

    Version bump ac-dabbrev

--- elpa/ac-dabbrev-20130906.518/ac-dabbrev.el
+++ elpa/ac-dabbrev-20130906.518/ac-dabbrev.el
@@ -17,20 +17,27 @@
 ;; along with this program; if not, write to the Free Software
 ;; Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
-;; Version: 0.0.8
+;; Version: 20130906.518
+;; X-Original-Version: 0.0.9
 ;; Author: k1LoW (Kenichirou Oyama), <k1lowxb [at] gmail [dot] com> <k1low [at] 101000lab [dot] org>
-;; URL: http://code.101000lab.org, http://trac.codecheck.in
 
 ;;; Install
 ;; Put this file into load-path'ed directory, and byte compile it if
 ;; desired.  And put the following expression into your ~/.emacs.
 ;;
 ;; (require 'ac-dabbrev)
-;; (setq ac-sources
-;;      (list ac-source-dabbrev
-;;            ))
+;; (add-to-list 'ac-sources 'ac-source-dabbrev)
+;;
+;; Or you could replace dabbrev-expand with auto-complete using this source:
+;;
+;; (defun ac-dabbrev-expand ()
+;;   (interactive)
+;;   (auto-complete '(ac-source-dabbrev)))
+;;
+;; (global-set-key "\M-/" 'ac-dabbrev-expand)
 
 ;;; Change Log
+;; 0.0.9: fix for Emacs 24, add autoloads, fix a limit bug (by Akinori MUSHA <knu@idaemons.org>)
 ;; 0.0.8: fix parameter for auto-complete.el 1.0a
 ;; 0.0.7: fix parameter for auto-complete.el 0.3.0
 ;; 0.0.6: fix face for auto-complete.el 0.2.0
@@ -64,7 +71,7 @@
 (defun ac-dabbrev-find-limit-expansions (abbrev limit ignore-case)
   "Return a list of limit expansions of ABBREV.
 If IGNORE-CASE is non-nil, accept matches which differ in case."
-  (let ((all-expansions nil) (i 0)
+  (let ((all-expansions nil) (i 0) (limit (or limit 100))
         expansion)
     (save-excursion
       (goto-char (point-min))
@@ -84,7 +91,7 @@ If IGNORE-CASE is non-nil, accept matches which differ in case."
 
 (lexical-let ((count 1))
   (defun ac-dabbrev-set-count ()
-    (setq command-char last-command-char)
+    (setq command-char last-command-event)
     (if (memq last-command ac-dabbrev-trigger-commands)
         (incf count)
       (setq count 1))
@@ -92,15 +99,17 @@ If IGNORE-CASE is non-nil, accept matches which differ in case."
              (not ac-candidates))
         (setq count 1))
     (loop for char in ac-dabbrev-reset-char
-          do (if (char-equal last-command-char (string-to-char char))
+          do (if (char-equal last-command-event (string-to-char char))
                  (setq count 1)))
     count))
 
+;;;###autoload
 (defun ac-dabbrev-get-candidates (abbrev)
   (if (>= (ac-dabbrev-set-count) ac-dabbrev-all-min-count)
       (ac-dabbrev-get-limit-candidates abbrev t)
     (ac-dabbrev-get-limit-candidates abbrev nil)))
 
+;;;###autoload
 (defvar ac-source-dabbrev
  '((candidates
      . (lambda () (all-completions ac-target (ac-dabbrev-get-candidates ac-target))))
