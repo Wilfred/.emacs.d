01b7b3de8499cc558a3ac2e02af8fd405f2e0105
Wilfred Hughes <me@wilfred.me.uk>
2014-04-10 00:18:41 +0100

    Removing kill-ring-choose in favour of the browse-kill-ring package.

---
+++ elpa/browse-kill-ring-20131116.2113/browse-kill-ring-pkg.el
@@ -0,0 +1,1 @@
+(define-package "browse-kill-ring" "20131116.2113" "interactively insert items from kill-ring" (quote nil))
--- user-lisp/kill-ring-customisations.el
+++ user-lisp/kill-ring-customisations.el
@@ -1,28 +1,4 @@
 ;; top of kill ring should also be in X clipboard
 (setq x-select-enable-clipboard t)
 
-(defun remove-all-text-properties (string)
-  "Return a copy of STRING without any text properties."
-  (let ((cleaned-string (copy-sequence string)))
-    (set-text-properties 0 (length string) nil cleaned-string)
-    cleaned-string))
-
-(require 's)
-(require 'ht)
-(eval-when-compile '(require 'cl))
-
-(defun kill-ring-choose ()
-  "Pick any previously killed item (using ido) to insert into the buffer."
-  (interactive)
-  (let ((abbrevs (ht)))
-    (dolist (item kill-ring)
-      (setq item (remove-all-text-properties item))
-      (ht-set abbrevs
-              (s-truncate 40 (s-replace "\n" " " item))
-              item))
-    (insert
-     (ht-get
-      abbrevs
-      (ido-completing-read "Previously killed: " (ht-keys abbrevs))))))
-
 (provide 'kill-ring-customisations)
