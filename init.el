23947956b38118973c6e67f3272fc89941210860
Wilfred Hughes <me@wilfred.me.uk>
2014-05-04 19:08:38 +0100

    Configuring paradox.

--- init.el
+++ init.el
@@ -572,7 +572,8 @@ If a prefix argument is given, don't change the kill-ring."
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(frame-background-mode (quote dark)))
+ '(frame-background-mode (quote dark))
+ '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
