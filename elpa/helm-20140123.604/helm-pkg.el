69bff7a132532571b9dd0d38e170d8d1e3f7e60b
Wilfred Hughes <me@wilfred.me.uk>
2014-05-13 22:23:46 +0100

    Version bump helm.

--- elpa/helm-20140123.604/helm-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm" "20140123.604" "Helm is an Emacs incremental and narrowing framework" 'nil)
---
+++ elpa/helm-20140512.2106/helm-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm" "20140512.2106" "Helm is an Emacs incremental and narrowing framework" 'nil)
