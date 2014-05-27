42d33958e94e693c22235de6fcfe28c5f9594697
Wilfred Hughes <me@wilfred.me.uk>
2014-05-27 23:13:08 +0100

    Installing visual-regexp.

---
+++ elpa/visual-regexp-20140311.724/visual-regexp-autoloads.el
@@ -0,0 +1,39 @@
+;;; visual-regexp-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (vr/query-replace vr/replace vr/mc-mark) "visual-regexp"
+;;;;;;  "visual-regexp.el" (21377 52420 600717 797000))
+;;; Generated autoloads from visual-regexp.el
+
+(autoload 'vr/mc-mark "visual-regexp" "\
+Convert regexp selection to multiple cursors.
+
+\(fn REGEXP START END)" t nil)
+
+(autoload 'vr/replace "visual-regexp" "\
+Regexp-replace with live visual feedback.
+
+\(fn REGEXP REPLACE START END)" t nil)
+
+(autoload 'vr/query-replace "visual-regexp" "\
+Use vr/query-replace like you would use query-replace-regexp.
+
+\(fn REGEXP REPLACE START END)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("visual-regexp-pkg.el") (21377 52420 716726
+;;;;;;  355000))
+
+;;;***
+
+(provide 'visual-regexp-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; visual-regexp-autoloads.el ends here
