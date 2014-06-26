cfabdb9be61e18209b730fe170b25c46392154ee
Wilfred Hughes <me@wilfred.me.uk>
2014-06-26 21:53:20 +0100

    Installing markdown-toc.

---
+++ elpa/markdown-toc-20140613.1051/markdown-toc-autoloads.el
@@ -0,0 +1,35 @@
+;;; markdown-toc-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (markdown-toc/generate-toc markdown-toc/version)
+;;;;;;  "markdown-toc" "markdown-toc.el" (21420 34870 528740 56000))
+;;; Generated autoloads from markdown-toc.el
+
+(autoload 'markdown-toc/version "markdown-toc" "\
+Markdown-toc version.
+
+\(fn)" t nil)
+
+(autoload 'markdown-toc/generate-toc "markdown-toc" "\
+Generate a TOC for markdown file at current position.
+Afterward, if a TOC is already present in the buffer, it will update it.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("markdown-toc-pkg.el") (21420 34870 613146
+;;;;;;  991000))
+
+;;;***
+
+(provide 'markdown-toc-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; markdown-toc-autoloads.el ends here
