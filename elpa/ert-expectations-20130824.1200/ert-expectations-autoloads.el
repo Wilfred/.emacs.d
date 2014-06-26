6477395baf8e77789c689c2177e462b77d8b85f4
Wilfred Hughes <me@wilfred.me.uk>
2014-06-26 22:29:40 +0100

    Installing el-mock and ert-expectations for contributing to markdown-toc.

---
+++ elpa/ert-expectations-20130824.1200/ert-expectations-autoloads.el
@@ -0,0 +1,18 @@
+;;; ert-expectations-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("ert-expectations-pkg.el" "ert-expectations.el")
+;;;;;;  (21420 35501 789388 237000))
+
+;;;***
+
+(provide 'ert-expectations-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ert-expectations-autoloads.el ends here
---
+++ elpa/ert-expectations-20130824.1200/ert-expectations-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ert-expectations" "20130824.1200" "The simplest unit test framework in the world" (quote nil))
