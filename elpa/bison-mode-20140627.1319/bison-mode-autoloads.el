846791b93017100f251ff8de9a47c2a5c223aeab
Wilfred Hughes <me@wilfred.me.uk>
2014-06-28 13:07:23 +0100

    Installing bison-mode.

---
+++ elpa/bison-mode-20140627.1319/bison-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; bison-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (bison-mode) "bison-mode" "bison-mode.el" (21422
+;;;;;;  44994 430754 168000))
+;;; Generated autoloads from bison-mode.el
+
+(autoload 'bison-mode "bison-mode" "\
+Major mode for editing bison/yacc files.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("bison-mode-pkg.el") (21422 44994 511507
+;;;;;;  715000))
+
+;;;***
+
+(provide 'bison-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; bison-mode-autoloads.el ends here
---
+++ elpa/bison-mode-20140627.1319/bison-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "bison-mode" "20140627.1319" "Major mode for editing bison, yacc and lex files." (quote nil))
