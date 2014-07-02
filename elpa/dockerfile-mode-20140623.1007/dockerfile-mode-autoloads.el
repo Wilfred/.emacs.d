33c04aaab998c205547ced2903f2ffb6736947c4
Wilfred Hughes <me@wilfred.me.uk>
2014-07-02 23:15:59 +0100

    Installing Dockerfile mode.

---
+++ elpa/dockerfile-mode-20140623.1007/dockerfile-mode-autoloads.el
@@ -0,0 +1,32 @@
+;;; dockerfile-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (dockerfile-mode) "dockerfile-mode" "dockerfile-mode.el"
+;;;;;;  (21428 33935 881670 967000))
+;;; Generated autoloads from dockerfile-mode.el
+
+(autoload 'dockerfile-mode "dockerfile-mode" "\
+A major mode to edit Dockerfiles.
+\\{dockerfile-mode-map}
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+
+;;;***
+
+;;;### (autoloads nil nil ("dockerfile-mode-pkg.el") (21428 33936
+;;;;;;  11099 944000))
+
+;;;***
+
+(provide 'dockerfile-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; dockerfile-mode-autoloads.el ends here
---
+++ elpa/dockerfile-mode-20140623.1007/dockerfile-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "dockerfile-mode" "20140623.1007" "Major mode for editing Docker's Dockerfiles" (quote nil))
