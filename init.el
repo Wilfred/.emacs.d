9c45e2d0a75e1228b3e93a1c7de23886b40fe857
Wilfred Hughes <me@wilfred.me.uk>
2014-07-06 22:07:34 +0100

    Adding user42 to package archives.

--- init.el
+++ init.el
@@ -13,6 +13,7 @@
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("user42" . "http://download.tuxfamily.org/user42/elpa/packages/") t)
 
 (package-initialize)
 
