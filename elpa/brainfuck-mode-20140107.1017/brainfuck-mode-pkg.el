ff8d8163cdaa3f411991f84841e6e313123598b5
Wilfred Hughes <me@wilfred.me.uk>
2014-07-23 19:55:17 +0100

    Version bump BF mode.

--- elpa/brainfuck-mode-20140107.1017/brainfuck-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "brainfuck-mode" "20140107.1017" "Brainfuck mode for Emacs" (quote ((langdoc "20130601.1450"))))
---
+++ elpa/brainfuck-mode-20140220.1855/brainfuck-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "brainfuck-mode" "20140220.1855" "Brainfuck mode for Emacs" (quote ((langdoc "20130601.1450"))))
