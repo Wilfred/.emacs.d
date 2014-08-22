5ec77ff3d3ee594d9891b2856ab146df0a886913
Wilfred Hughes <me@wilfred.me.uk>
2014-08-22 18:40:39 +0100

    Fresh eshell buffers should have more helpful names than just appending a number.

--- user-lisp/eshell-customisations.el
+++ user-lisp/eshell-customisations.el
@@ -3,4 +3,15 @@
 ;; Auto-complete in eshell should stop at the first ambiguity.
 (setq eshell-cmpl-cycle-completions nil)
 
+(defadvice eshell (around eshell-append-cwd (&optional arg) activate)
+  "New eshell buffers should be named according to their working directory."
+  (interactive "P")
+  (if (and arg (not (numberp arg)))
+      ;; Non-numeric prefix arg given, change the eshell buffer name.
+      (let* ((current-dir (f-parent (buffer-file-name)))
+             (eshell-buffer-name (format "*eshell*<%s>" current-dir)))
+        ad-do-it)
+    ;; Otherwise, continue as normal
+    ad-do-it))
+
 (provide 'eshell-customisations)
