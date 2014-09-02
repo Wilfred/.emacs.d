b265710bdd221f7a58f785ded5cebbcd827207ae
Wilfred Hughes <me@wilfred.me.uk>
2014-09-03 00:33:29 +0100

    Installing highlight-quoted.

---
+++ elpa/highlight-quoted-20140820.1649/highlight-quoted-autoloads.el
@@ -0,0 +1,35 @@
+;;; highlight-quoted-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (highlight-quoted-mode) "highlight-quoted" "highlight-quoted.el"
+;;;;;;  (21510 21204 89328 397000))
+;;; Generated autoloads from highlight-quoted.el
+
+(autoload 'highlight-quoted-mode "highlight-quoted" "\
+Highlight Lisp quotes and quoted symbols.
+
+Toggle Highlight-Quoted mode on or off.
+With a prefix argument ARG, enable Highlight-Quoted mode if ARG is positive, and
+disable it otherwise.  If called from Lisp, enable the mode if ARG is omitted or
+nil, and toggle it if ARG is `toggle'.
+\\{highlight-quoted-mode-map}
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("highlight-quoted-pkg.el") (21510 21204
+;;;;;;  197791 271000))
+
+;;;***
+
+(provide 'highlight-quoted-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; highlight-quoted-autoloads.el ends here
---
+++ elpa/highlight-quoted-20140820.1649/highlight-quoted-pkg.el
@@ -0,0 +1,1 @@
+(define-package "highlight-quoted" "20140820.1649" "Highlight Lisp quotes and quoted symbols" (quote ((emacs "24"))))
