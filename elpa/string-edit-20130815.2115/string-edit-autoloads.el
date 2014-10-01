f52e4bd45de7090749e2d68563b973650e639a13
Wilfred Hughes <me@wilfred.me.uk>
2014-10-01 22:51:59 +0100

    Installing string-edit, turns out it's very handy.

---
+++ elpa/string-edit-20130815.2115/string-edit-autoloads.el
@@ -0,0 +1,29 @@
+;;; string-edit-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (string-edit-at-point) "string-edit" "string-edit.el"
+;;;;;;  (21539 12487 552518 775000))
+;;; Generated autoloads from string-edit.el
+
+(autoload 'string-edit-at-point "string-edit" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("string-edit-pkg.el") (21539 12487 624771
+;;;;;;  478000))
+
+;;;***
+
+(provide 'string-edit-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; string-edit-autoloads.el ends here
---
+++ elpa/string-edit-20130815.2115/string-edit-pkg.el
@@ -0,0 +1,1 @@
+(define-package "string-edit" "20130815.2115" "Avoid escape nightmares by editing string in separate buffer" (quote ((dash "1.2.0"))))
