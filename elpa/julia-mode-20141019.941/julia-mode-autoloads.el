472332a0809205e3b3618c252029011a416bf202
Wilfred Hughes <me@wilfred.me.uk>
2014-10-22 22:13:45 +0100

    Version bump julia-mode.

--- elpa/julia-mode-20141019.941/julia-mode-autoloads.el
+++ elpa/julia-mode-20141019.941/julia-mode-autoloads.el
@@ -1,10 +1,10 @@
 ;;; julia-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads (julia-mode) "julia-mode" "julia-mode.el" (21547
-;;;;;;  2231 963480 215000))
+;;;### (autoloads nil "julia-mode" "julia-mode.el" (21576 7664 3488
+;;;;;;  307000))
 ;;; Generated autoloads from julia-mode.el
 
 (add-to-list 'auto-mode-alist '("\\.jl\\'" . julia-mode))
@@ -16,16 +16,9 @@ Major mode for editing julia code.
 
 ;;;***
 
-;;;### (autoloads nil nil ("julia-mode-pkg.el") (21547 2232 49792
-;;;;;;  327000))
-
-;;;***
-
-(provide 'julia-mode-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; julia-mode-autoloads.el ends here
---
+++ elpa/julia-mode-20141019.941/julia-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "julia-mode" "20141019.941" "Major mode for editing Julia source code" 'nil :url "https://github.com/JuliaLang/julia" :keywords '("languages"))
