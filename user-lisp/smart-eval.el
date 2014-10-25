4fd3eea2e79f4d0114a38fa5a649503907fd6dd3
Wilfred Hughes <me@wilfred.me.uk>
2014-10-26 00:23:22 +0100

    Adding a full header and footer to smart-eval.

--- user-lisp/smart-eval.el
+++ user-lisp/smart-eval.el
@@ -1,9 +1,15 @@
-;;; smart-eval.el --- A do-what-I-mean elisp eval command
+;;; smart-eval.el --- helpful eval, re-eval and uneval for elisp
+
+;; Copyright (C) 2014 Wilfred Hughes <me@wilfred.me.uk>
+;;
+;; Author: Wilfred Hughes <me@wilfred.me.uk>
+;; Created: 18 October 2014
+;; Version: 0.1
 
 ;;; Commentary:
 ;; When evaluating an elisp buffer, we want the following features:
 ;;
-;; 1. Evaluate the current conntent. This could either be the form
+;; 1. Evaluate the current content.  This could either be the form
 ;; before point, the top-level form enclosing point, a region, or a
 ;; buffer.
 ;; 
@@ -15,12 +21,39 @@
 ;;
 ;; 4. Always open a debugger if the evaluation throws an error.
 ;;
+;; 5. The ability to completely uneval (i.e. unload or forget)
+;; our bindings.
+;;
 ;; Note that eval-last-sexp sets debug-on-error regardless of what it
-;; was set to before. Customise eval-expression-debug-on-error to
-;; change it. We need to think about what's best for smart-eval
-;; (always debug I think).
+;; was set to before.  Customise eval-expression-debug-on-error to
+;; change it.  We need to think about what's best for smart-eval
+;; (always debug, I think).
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
 (require 'shut-up)
 
+;;; Code:
+
+;;;###autoload
 (defun smart-eval-sexp ()
   (interactive)
   )
@@ -46,3 +79,6 @@
       (forward-sexp)
       (shut-up
         (smart-eval-outer-sexp nil)))))
+
+(provide 'smart-eval)
+;;; smart-eval.el ends here
