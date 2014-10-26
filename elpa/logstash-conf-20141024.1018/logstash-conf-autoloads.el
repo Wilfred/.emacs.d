7832be7f1a46a44d4abe55b56c551fe0279b9c81
Wilfred Hughes <me@wilfred.me.uk>
2014-10-26 19:19:38 +0000

    Installing logstash-conf.

---
+++ elpa/logstash-conf-20141024.1018/logstash-conf-autoloads.el
@@ -0,0 +1,22 @@
+;;; logstash-conf-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "logstash-conf" "logstash-conf.el" (21581 18745
+;;;;;;  233097 115000))
+;;; Generated autoloads from logstash-conf.el
+
+(autoload 'logstash-conf-mode "logstash-conf" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; logstash-conf-autoloads.el ends here
---
+++ elpa/logstash-conf-20141024.1018/logstash-conf-pkg.el
@@ -0,0 +1,1 @@
+(define-package "logstash-conf" "20141024.1018" "basic mode for editing logstash configuration" 'nil)
