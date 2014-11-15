63f5c33bde585b54e1ac042da560182555e4af46
Wilfred Hughes <me@wilfred.me.uk>
2014-11-15 00:28:45 +0000

    Project idea: insert logging statements automatically.

---
+++ user-lisp/print-points.el
@@ -0,0 +1,13 @@
+;;; print-points --- insert a useful debugging statement.
+
+;;; Commentary.
+;; I often find myself writing:
+;;
+;; print "1. Inside do_it"
+;; ... later ...
+;; print "2. Inside do_it"
+;; ... later ...
+;; print "3. Inside do_other_thing"
+;;
+;; This package is to help insert those debugging lines with one
+;; keystroke.
