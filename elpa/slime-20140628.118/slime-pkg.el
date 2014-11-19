f8ea9101725325ac96d386843cfd1c778de72efe
Wilfred Hughes <me@wilfred.me.uk>
2014-11-19 23:48:34 +0000

    Version bump slime.

--- elpa/slime-20140628.118/slime-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "slime" "20140628.118" "Superior Lisp Interaction Mode for Emacs"
-  '((cl-lib "0.5"))
-  :url "https://github.com/slime/slime" :keywords
-  '("languages" "lisp" "slime"))
