6b1b00c91df46b04fa70a4a79441bf411c1d1ee3
Wilfred Hughes <me@wilfred.me.uk>
2014-11-26 22:49:50 +0000

    Version bump paradox.

--- elpa/paradox-20140515.553/paradox-autoloads.el
+++
@@ -1,36 +0,0 @@
-;;; paradox-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (paradox-list-packages paradox--refresh-star-count)
-;;;;;;  "paradox" "paradox.el" (21423 59434 946750 382000))
-;;; Generated autoloads from paradox.el
-
-(autoload 'paradox--refresh-star-count "paradox" "\
-Download the star-count file and populate the respective variable.
-
-\(fn)" t nil)
-
-(autoload 'paradox-list-packages "paradox" "\
-Improved version of `package-list-packages'.
-Shows star count for packages, and extra information in the
-mode-line.
-
-\(fn NO-FETCH)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("paradox-compat.el" "paradox-pkg.el")
-;;;;;;  (21423 59434 978492 94000))
-
-;;;***
-
-(provide 'paradox-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; paradox-autoloads.el ends here
