492b521401be812088a791ceefd44390278add8f
Wilfred Hughes <me@wilfred.me.uk>
2014-12-17 00:17:08 +0000

    Removing lush.

--- elpa/lush-theme-20141022.127/lush-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "lush-theme" "20141022.127" "A dark theme with strong colors" '((emacs "24")) :url "https://github.com/andre-richter/emacs-lush-theme" :keywords '("theme" "dark" "strong colors"))
