000ab47f96f1e11d9cb304e25921b0c1551a9da2
Wilfred Hughes <me@wilfred.me.uk>
2015-01-10 16:42:48 +0000

    Version bump el-x.

--- elpa/el-x-20140111.1401/el-x-autoloads.el
+++ elpa/el-x-20140111.1401/el-x-autoloads.el
@@ -1,25 +1,29 @@
 ;;; el-x-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "dflet" "dflet.el" (20872 55312 968509 804000))
+;;;### (autoloads nil "dflet" "dflet.el" (21613 8205 47551 783000))
 ;;; Generated autoloads from dflet.el
 
 (autoload 'dflet "dflet")
 
-;;;***
-
-;;;### (autoloads nil nil ("el-x-pkg.el" "el-x.el" "subr-compat.el")
-;;;;;;  (20872 55313 111820 449000))
+(autoload 'adflet "dflet" "\
+Anaphoric version of `dflet'. Binds `this-fn' to the original
+definition of the function.
+
+\(fn BINDINGS &rest BODY)" nil t)
 
 ;;;***
 
-(provide 'el-x-autoloads)
+;;;### (autoloads nil nil ("el-x-pkg.el" "el-x.el" "subr-compat.el")
+;;;;;;  (21613 8205 193581 778000))
+
+;;;***
+
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; el-x-autoloads.el ends here
---
+++ elpa/el-x-20140111.1401/el-x-pkg.el
@@ -0,0 +1,5 @@
+(define-package "el-x" "20140111.1401" "main entry point for el-x package" 'nil :keywords
+  '("lisp"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
