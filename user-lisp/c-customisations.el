4d8272cf4917e0194e28163debfa7552bce0b7c8
Wilfred Hughes <me@wilfred.me.uk>
2015-02-02 23:43:25 +0000

    Assume C++11 for C++ code with flycheck.

--- user-lisp/c-customisations.el
+++ user-lisp/c-customisations.el
@@ -1,3 +1,5 @@
+(require 'flycheck)
+
 ;; todo: show eldoc in modeline, since the minibuffer is used by
 ;; flycheck.
 (add-hook 'c-mode-hook 'c-turn-on-eldoc-mode)
@@ -5,5 +7,12 @@
 (add-hook 'c-mode-common-hook #'flycheck-mode)
 
 (add-hook 'c-mode-hook #'which-function-mode)
+
+;; C++
+
+(add-hook
+ 'c++-mode-hook
+ (lambda ()
+   (setq flycheck-clang-language-standard "c++11")))
 
 (provide 'c-customisations)
