28c5ea34915ba6cae54e1d13ea98a756c3e130c5
Wilfred Hughes <me@wilfred.me.uk>
2015-02-05 20:16:28 +0000

    Version bump esup.

--- elpa/esup-20130607.1350/esup-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; esup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (esup) "esup" "esup.el" (21423 59517 218868 931000))
-;;; Generated autoloads from esup.el
-
-(autoload 'esup "esup" "\
-Profile the startup time of Emacs in the background.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("esup-pkg.el") (21423 59517 274957 754000))
-
-;;;***
-
-(provide 'esup-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; esup-autoloads.el ends here
--- elpa/esup-20130607.1350/esup-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "esup" "20130607.1350" "the Emacs StartUp Profiler (ESUP)" (quote nil))
