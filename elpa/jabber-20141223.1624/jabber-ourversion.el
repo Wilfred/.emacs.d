24e49c7062d62fcf14310fd8e96455dc9763db40
Wilfred Hughes <me@wilfred.me.uk>
2015-02-14 23:14:26 +0000

    Removing jabber, I no longer use it.

--- elpa/jabber-20141223.1624/jabber-ourversion.el
+++
@@ -1,8 +0,0 @@
-;; jabber-ourversion.el.  Holds the version number in a format that
-;; configure.ac can read.
-
-;; On the following line, only change the part between double quotes:
-(defconst jabber-version "0.8.92"
-  "version returned to those who query us")
-
-(provide 'jabber-ourversion)
--- elpa/jabber-20141223.1624/jabber-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "jabber" "20141223.1624" "A Jabber client for Emacs." 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
