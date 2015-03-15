4aa543d722ec1fb06d6fedd4a2b5765136b4c007
Wilfred Hughes <me@wilfred.me.uk>
2015-03-15 23:34:14 +0000

    Sharp-quote functions.

--- user-lisp/c-customisations.el
+++ user-lisp/c-customisations.el
@@ -2,7 +2,7 @@
 
 ;; todo: show eldoc in modeline, since the minibuffer is used by
 ;; flycheck.
-(add-hook 'c-mode-hook 'c-turn-on-eldoc-mode)
+(add-hook 'c-mode-hook #'c-turn-on-eldoc-mode)
 
 (add-hook 'c-mode-hook #'which-function-mode)
 
