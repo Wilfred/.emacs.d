37a96a5f699318b90f0106fa690331233b906a62
Wilfred Hughes <me@wilfred.me.uk>
2015-05-14 23:41:12 +0100

    Use github-flavored markdown for .md files.
    I edit more GFM files than vanilla markdown these days.

--- user-lisp/structured-text-customisations.el
+++ user-lisp/structured-text-customisations.el
@@ -15,7 +15,7 @@
 ; markdown mode
 (autoload 'markdown-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md$" . gfm-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
 
 ; XML position utility:
