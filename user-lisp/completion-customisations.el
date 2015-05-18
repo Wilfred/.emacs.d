4a18879db7b97e901dd6c51d3d10519af13df187
Wilfred Hughes <whughes@ahl.com>
2015-05-18 11:42:00 +0100

    Spelling fix.

--- user-lisp/completion-customisations.el
+++ user-lisp/completion-customisations.el
@@ -95,7 +95,7 @@
 (add-to-list 'company-backends 'company-c-headers)
 (add-hook 'prog-mode-hook #'company-mode)
 
-;; Offer completion for as littile as one character.
+;; Offer completion for as little as one character.
 (setq company-minimum-prefix-length 1)
 
 (company-quickhelp-mode 1)
