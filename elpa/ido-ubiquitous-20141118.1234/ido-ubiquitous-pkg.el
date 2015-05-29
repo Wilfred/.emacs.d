006f7d65250e4e425e0ea21bfae4611282b2cbf0
Wilfred Hughes <me@wilfred.me.uk>
2015-05-29 22:24:57 +0100

    Removing the now-unused ido-ubiquitous.

--- elpa/ido-ubiquitous-20141118.1234/ido-ubiquitous-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ido-ubiquitous" "20141118.1234" "Use ido (nearly) everywhere." '((emacs "24.1")) :url "https://github.com/DarwinAwardWinner/ido-ubiquitous" :keywords '("convenience" "completion" "ido"))
