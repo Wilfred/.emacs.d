4daefb32a8317ecbf669f771d7da319722ecfcca
Wilfred Hughes <me@wilfred.me.uk>
2015-06-04 17:31:28 +0100

    Version bump erlang.

--- elpa/erlang-20150319.456/erlang-autoloads.el
+++ elpa/erlang-20150319.456/erlang-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "erlang" "erlang.el" (21660 35615 919428 728000))
+;;;### (autoloads nil "erlang" "erlang.el" (21777 59966 192104 869000))
 ;;; Generated autoloads from erlang.el
 
 (autoload 'erlang-mode "erlang" "\
@@ -118,8 +118,8 @@ editing control characters:
 
 ;;;***
 
-;;;### (autoloads nil "erlang-start" "erlang-start.el" (21660 35615
-;;;;;;  799433 264000))
+;;;### (autoloads nil "erlang-start" "erlang-start.el" (21777 59966
+;;;;;;  52108 569000))
 ;;; Generated autoloads from erlang-start.el
 
 (let ((a '("\\.erl\\'" . erlang-mode)) (b '("\\.hrl\\'" . erlang-mode))) (or (assoc (car a) auto-mode-alist) (setq auto-mode-alist (cons a auto-mode-alist))) (or (assoc (car b) auto-mode-alist) (setq auto-mode-alist (cons b auto-mode-alist))))
@@ -132,7 +132,7 @@ editing control characters:
 
 ;;;### (autoloads nil nil ("erlang-eunit.el" "erlang-flymake.el"
 ;;;;;;  "erlang-pkg.el" "erlang-skels-old.el" "erlang-skels.el" "erlang_appwiz.el")
-;;;;;;  (21660 35616 145396 903000))
+;;;;;;  (21777 59966 401707 731000))
 
 ;;;***
 
---
+++ elpa/erlang-20150319.456/erlang-pkg.el
@@ -0,0 +1,4 @@
+(define-package "erlang" "20150319.456" "Erlang major mode" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
