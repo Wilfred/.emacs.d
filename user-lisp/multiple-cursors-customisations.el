bfbb7489ca75971841cbf74992e51188d6fd5476
Wilfred Hughes <whughes@ahl.com>
2015-06-15 16:37:58 +0100

    Remove unused file.

--- user-lisp/multiple-cursors-customisations.el
+++
@@ -1,6 +0,0 @@
-(require 'multiple-cursors)
-
-(global-set-key (kbd "M-'") 'mc/mark-all-symbols-like-this)
-(global-set-key (kbd "C-c M-'") 'mc/mark-all-symbols-like-this-in-defun)
-
-(global-set-key (kbd "M-@") 'mc/mark-next-symbol-like-this)
