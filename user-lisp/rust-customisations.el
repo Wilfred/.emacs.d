8ddf2bf6eed051de1ba627c3bbf4dc754360333b
Wilfred Hughes <me@wilfred.me.uk>
2015-06-29 22:02:21 +0100

    Configuring racer.

--- user-lisp/rust-customisations.el
+++ user-lisp/rust-customisations.el
@@ -3,4 +3,12 @@
 
 (add-hook 'rust-mode-hook #'flycheck-mode)
 
+(require 'racer)
+(setq racer-rust-src-path "/home/wilfred/src/rust/src")
+(setq racer-cmd "/home/wilfred/src/racer/target/release/racer")
+
+(require 'rust-mode)
+(add-hook 'rust-mode-hook #'racer-activate)
+(define-key rust-mode-map (kbd "M-.") #'racer-find-definition)
+
 (provide 'rust-customisations)
