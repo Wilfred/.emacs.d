f0736c28cc7d7de8d915214b5a226342a07d6c9f
Wilfred Hughes <whughes@ahl.com>
2015-07-03 15:04:32 +0100

    Correcting python shebang.

--- user-lisp/editing-customisations.el
+++ user-lisp/editing-customisations.el
@@ -243,7 +243,7 @@ Handy when editing markdown."
   
   (set-file-modes (buffer-file-name)
                   (file-modes-symbolic-to-number "u+rwx" (file-modes (buffer-file-name))))
-  (insert (ido-completing-read "Interpreter: " (list "#!/bin/bash" "#!/bin/env python")))
+  (insert (ido-completing-read "Interpreter: " (list "#!/bin/bash" "#!/usr/bin/env python")))
   (insert "\n\n"))
 
 (defun wh/apply-on-region (beg end func)
