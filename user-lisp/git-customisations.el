e83bf7a4d7cdf30ff7b2bc66d56dc35824fa4ed1
Wilfred Hughes <me@wilfred.me.uk>
2015-07-06 22:41:16 +0100

    Permit longer lines in commit messages.

--- user-lisp/git-customisations.el
+++ user-lisp/git-customisations.el
@@ -1,6 +1,10 @@
 (require 'magit)
 
+;; Use F2 to open magit.
 (global-set-key (kbd "<f2>") 'magit-status)
+
+;; Don't prompt when first line of commit is over 50 chars.
+(setq git-commit-finish-query-functions '())
 
 (provide 'git-customisations)
 
