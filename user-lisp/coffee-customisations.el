442a9b913947dbc8816485305327d95b3dc0308c
Wilfred Hughes <whughes@ahl.com>
2015-07-10 10:09:57 +0100

    Only use dabbrev for company in coffee-mode.

--- user-lisp/coffee-customisations.el
+++ user-lisp/coffee-customisations.el
@@ -2,6 +2,11 @@
 
 (setq coffee-tab-width 2)
 
+(defun wh/company-in-coffee-mode ()
+  (setq company-backends (list #'company-dabbrev-code)))
+
+(add-hook 'coffee-mode-hook #'wh/company-in-coffee-mode)
+
 (require 'flycheck)
 (add-hook 'coffee-mode-hook #'flycheck-mode)
 
