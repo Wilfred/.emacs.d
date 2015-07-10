690fbb17678d90d20af1aff6a76658042597ba3b
Wilfred Hughes <me@wilfred.me.uk>
2015-07-10 23:46:38 +0100

    Version bump jade-mode.

--- elpa/jade-mode-20131018.1010/jade-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "jade-mode" "20131018.1010" "Major mode for editing .jade files" (quote ((sws-mode "0"))))
--- elpa/jade-mode-20150324.624/jade-mode-autoloads.el
+++ elpa/jade-mode-20150324.624/jade-mode-autoloads.el
@@ -1,10 +1,10 @@
 ;;; jade-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads (jade-mode) "jade-mode" "jade-mode.el" (21361 13262
-;;;;;;  370000 835000))
+;;;### (autoloads nil "jade-mode" "jade-mode.el" (21777 59933 102983
+;;;;;;  132000))
 ;;; Generated autoloads from jade-mode.el
 
 (autoload 'jade-mode "jade-mode" "\
@@ -12,20 +12,13 @@ Major mode for editing jade node.js templates
 
 \(fn)" t nil)
 
-(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+(add-to-list 'auto-mode-alist '("\\.jade\\'" . jade-mode))
 
 ;;;***
 
-;;;### (autoloads nil nil ("jade-mode-pkg.el") (21361 13262 432476
-;;;;;;  935000))
-
-;;;***
-
-(provide 'jade-mode-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; jade-mode-autoloads.el ends here
