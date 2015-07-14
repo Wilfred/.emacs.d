a1dd5d80ff1b2636ec5d350024449f2832d2750e
Wilfred Hughes <me@wilfred.me.uk>
2015-07-14 23:00:50 +0100

    Version bump rust-mode.

--- elpa/rust-mode-20150708.1641/rust-mode-autoloads.el
+++ elpa/rust-mode-20150708.1641/rust-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "rust-mode" "rust-mode.el" (21892 27430 512961
-;;;;;;  533000))
+;;;### (autoloads nil "rust-mode" "rust-mode.el" (21925 34378 807548
+;;;;;;  684000))
 ;;; Generated autoloads from rust-mode.el
 
 (autoload 'rust-mode "rust-mode" "\
