0a16cdc2df9fcefbe1f86f7d0633f74f2255a5aa
Wilfred Hughes <me@wilfred.me.uk>
2015-07-14 22:42:07 +0100

    Adding a shortcut for switching buffers in the same mode.

---
+++ elpa/cbm-20150713.816/cbm-autoloads.el
@@ -0,0 +1,21 @@
+;;; cbm-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "cbm" "cbm.el" (21925 27651 508877 173000))
+;;; Generated autoloads from cbm.el
+
+(autoload 'cbm-cycle "cbm" "\
+Cycles through buffers with same `major-mode'.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; cbm-autoloads.el ends here
