2920b505b2944f151643dc3c11aec8e1ff79569f
Wilfred Hughes <whughes@ahl.com>
2015-07-15 15:08:59 +0100

    A quick script for opening company at just the right time.

---
+++ user-lisp/company-jit.el
@@ -0,0 +1,25 @@
+(require 'company)
+(require 'ht)
+(require 'dash)
+
+(defvar company-jit-prefixes
+  (ht
+   (#'python-mode '("import " "from ")))
+  )
+
+(defun company-jit-match-p (prefix)
+  "Does the text before point match PREFIX?"
+  (save-excursion
+    (save-match-data
+      (backward-char (length prefix))
+      (looking-at prefix))))
+
+(defun company-jit-post-self-insert ()
+  "Open company in situations where the user probably wants completions."
+  (when (let ((prefixes (ht-get company-jit-prefixes major-mode '())))
+          (--any-p (company-jit-match-p it) prefixes))
+    (company-complete)))
+
+(add-hook 'post-self-insert-hook #'company-jit-post-self-insert)
+
+(provide 'company-jit)
