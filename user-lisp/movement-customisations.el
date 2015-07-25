75a9fd4525d7b3423d54174fb78accd636297695
Wilfred Hughes <me@wilfred.me.uk>
2015-07-25 11:29:05 +0100

    Experimenting with hydra for movement.

--- user-lisp/movement-customisations.el
+++ user-lisp/movement-customisations.el
@@ -1,5 +1,3 @@
-
-
 ;; avy -- quickly jump to an arbitrary word or line
 (require 'avy)
 (setq avy-case-fold-search nil)
@@ -7,5 +5,24 @@
       (append (number-sequence ?a ?z) (number-sequence ?A ?Z)))
 (define-key global-map (kbd "<f11>") #'avy-goto-word-or-subword-1)
 (define-key global-map (kbd "C-c <f11>") #'avy-goto-line)
+
+;; I use next/previous-line and forward/backward-char an awful lot
+;; according to `keyfreq-show'. Allow these commands to be repeated,
+;; so 'C-n n' is equivalent to 'C-n C-n' and so on.
+(global-set-key
+ (kbd "C-n")
+ (defhydra hydra-move
+   (:body-pre (next-line))
+   "move"
+   ("n" next-line)
+   ("p" previous-line)
+   ("f" forward-char)
+   ("b" backward-char)
+   ("a" beginning-of-line-dwim)
+   ("e" move-end-of-line)
+   ("v" scroll-up-command)
+   ;; Converting M-v to V here by analogy.
+   ("V" scroll-down-command)))
+
 
 (provide 'movement-customisations)
