ffef3b57806f1ea79cf955347e99c80b1329e527
Wilfred Hughes <whughes@ahl.com>
2015-07-27 13:54:08 +0100

    Don't track branches by default with magit.
    See https://github.com/magit/magit/issues/2009#issuecomment-122024689

--- user-lisp/git-customisations.el
+++ user-lisp/git-customisations.el
@@ -6,5 +6,7 @@
 ;; Don't prompt when first line of commit is over 50 chars.
 (setq git-commit-finish-query-functions '())
 
+(setq magit-branch-arguments (remove "--track" magit-branch-arguments))
+
 (provide 'git-customisations)
 
