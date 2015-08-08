f97cd496b136996f09f3d0fbc507f25fc1cec785
Wilfred Hughes <me@wilfred.me.uk>
2015-08-08 14:58:05 +0100

    Ensure our prefixes are treated as strings.

--- user-lisp/company-jit.el
+++ user-lisp/company-jit.el
@@ -7,14 +7,14 @@
    (#'python-mode '("import " "from "))
    (#'rust-mode '("::" "."))
    (#'css-mode '(": ")))
-  )
+  "Prefix strings that trigger company completion.")
 
 (defun company-jit-match-p (prefix)
   "Does the text before point match PREFIX?"
   (save-excursion
     (save-match-data
       (backward-char (length prefix))
-      (looking-at prefix))))
+      (looking-at (regexp-quote prefix)))))
 
 (defun company-jit-should-complete-p ()
   "Return nil if point is in a comment or string."
