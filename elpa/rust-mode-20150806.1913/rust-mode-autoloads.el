db7aaa0cb27ad93cd072042e3ad0532fcb6a234a
Wilfred Hughes <me@wilfred.me.uk>
2015-08-10 21:34:41 +0100

    Version bump rust-mode.
    Previous version had a performance issue:
    https://github.com/rust-lang/rust-mode/issues/86

--- elpa/rust-mode-20150806.1913/rust-mode-autoloads.el
+++ elpa/rust-mode-20150806.1913/rust-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "rust-mode" "rust-mode.el" (21925 34378 807548
-;;;;;;  684000))
+;;;### (autoloads nil "rust-mode" "rust-mode.el" (21961 2770 438639
+;;;;;;  682000))
 ;;; Generated autoloads from rust-mode.el
 
 (autoload 'rust-mode "rust-mode" "\
---
+++ elpa/rust-mode-20150806.1913/rust-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "rust-mode" "20150806.1913" "A major emacs mode for editing Rust source code" 'nil :url "https://github.com/rust-lang/rust-mode" :keywords '("languages"))
