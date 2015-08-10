17f00a82e23fb2c1769239665445f4cf566dc57a
Wilfred Hughes <me@wilfred.me.uk>
2015-08-10 23:37:57 +0100

    Installing diff-hl.

---
+++ elpa/diff-hl-20150808.1329/diff-hl-pkg.el
@@ -0,0 +1,7 @@
+(define-package "diff-hl" "20150808.1329" "Highlight uncommitted changes"
+  '((cl-lib "0.2"))
+  :url "https://github.com/dgutov/diff-hl" :keywords
+  '("vc" "diff"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
