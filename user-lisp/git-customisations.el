728fb569053711bdedd03206efb746a7402a6c80
Wilfred Hughes <whughes@ahl.com>
2015-08-12 09:45:19 +0100

    Configuring diff-hl.

--- user-lisp/git-customisations.el
+++ user-lisp/git-customisations.el
@@ -8,5 +8,18 @@
 
 (setq magit-branch-arguments (remove "--track" magit-branch-arguments))
 
+;; Highlight new/removed/changed lines relative to the last commit in VCS.
+(global-diff-hl-mode)
+
+;; Default colours are too subtle, make them obvious.
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(diff-hl-change ((t (:background "blue3" :foreground "blue3"))))
+ '(diff-hl-delete ((t (:inherit diff-removed :background "red3" :foreground "red3"))))
+ '(diff-hl-insert ((t (:inherit diff-added :background "green4" :foreground "green4")))))
+
 (provide 'git-customisations)
 
