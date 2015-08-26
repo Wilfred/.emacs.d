1470bd1857bcb568b07c35d6795f342c3fcd61f6
Wilfred Hughes <whughes@ahl.com>
2015-08-26 11:56:10 +0100

    Use company-whole-line for Python.

--- user-lisp/python-customisations.el
+++ user-lisp/python-customisations.el
@@ -88,7 +88,8 @@
 (require 'company)
 (defun wh/company-in-python-mode ()
   (setq-local company-idle-delay 0.1)
-  (set (make-local-variable 'company-backends) (list #'company-anaconda #'company-dabbrev-code)))
+  (set (make-local-variable 'company-backends)
+       (list #'company-whole-line #'company-anaconda #'company-dabbrev-code)))
 
 (add-hook 'python-mode-hook #'wh/company-in-python-mode)
 
