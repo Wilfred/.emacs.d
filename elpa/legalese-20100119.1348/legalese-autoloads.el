f4b42bfca8d314d40770c89e0a29b6e1ff6cc3d6
Wilfred Hughes <me@wilfred.me.uk>
2015-08-30 21:11:51 +0100

    Installing legalese.

---
+++ elpa/legalese-20100119.1348/legalese-autoloads.el
@@ -0,0 +1,15 @@
+;;; legalese-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("legalese.el") (21987 19337 45700 941000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; legalese-autoloads.el ends here
---
+++ elpa/legalese-20100119.1348/legalese-pkg.el
@@ -0,0 +1,1 @@
+(define-package "legalese" "20100119.1348" "Add legalese to your program files" 'nil :stars '("") :keywords '("convenience"))
