19650f02aeef8bd122bcb911c1595fc2229140af
Wilfred Hughes <whughes@ahl.com>
2015-09-01 11:38:45 +0100

    Installing company-try-hard.

---
+++ elpa/company-try-hard-20150830.1330/company-try-hard-autoloads.el
@@ -0,0 +1,23 @@
+;;; company-try-hard-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "company-try-hard" "company-try-hard.el" (21989
+;;;;;;  32773 928602 864000))
+;;; Generated autoloads from company-try-hard.el
+
+(autoload 'company-try-hard "company-try-hard" "\
+Offer completions from the first backend in `company-backends' that
+offers candidates. If called again, use the next backend, and so on.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; company-try-hard-autoloads.el ends here
