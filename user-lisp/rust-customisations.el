a4b8a8c6734ed4713c2c77fcb31582507f287ed1
Wilfred Hughes <me@wilfred.me.uk>
2015-09-02 23:03:15 +0100

    Simply add racer to company backends for rust.
    This means we can still use company-dabbrev-code if racer can't help us.

--- user-lisp/rust-customisations.el
+++ user-lisp/rust-customisations.el
@@ -7,8 +7,9 @@
 (setq racer-rust-src-path "/home/wilfred/src/rust/src")
 (setq racer-cmd "/home/wilfred/src/racer/target/release/racer")
 
+(add-to-list 'company-backends #'racer-company-complete)
+
 (require 'rust-mode)
-(add-hook 'rust-mode-hook #'racer-activate)
 (add-hook 'rust-mode-hook #'racer-turn-on-eldoc)
 (define-key rust-mode-map (kbd "M-.") #'racer-find-definition)
 
