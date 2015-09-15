7315773420ea79a15688cbd7d0c42ac84f01262c
Wilfred Hughes <whughes@ahl.com>
2015-09-15 15:01:26 +0100

    Installing pytest.el.

---
+++ elpa/pytest-20150824.900/pytest-autoloads.el
@@ -0,0 +1,15 @@
+;;; pytest-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("pytest.el") (22007 61184 134835 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; pytest-autoloads.el ends here
---
+++ elpa/pytest-20150824.900/pytest-pkg.el
@@ -0,0 +1,1 @@
+(define-package "pytest" "20150824.900" "Easy Python test running in Emacs" 'nil :url "http://bitbucket.org/elarson/pytest.el" :keywords '("pytest" "python" "testing"))
