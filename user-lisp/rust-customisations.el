d2b448f97538d057a14e820fef7faa125c7cbd84
Wilfred Hughes <me@wilfred.me.uk>
2015-09-18 20:18:16 +0100

    Allow toggling a function's visibility in Rust.

--- user-lisp/rust-customisations.el
+++ user-lisp/rust-customisations.el
@@ -13,4 +13,19 @@
 (add-hook 'rust-mode-hook #'racer-turn-on-eldoc)
 (define-key rust-mode-map (kbd "M-.") #'racer-find-definition)
 
+(defun wh/rust-toggle-visibility ()
+  "Toggle the public visibility of the function at point."
+  (interactive)
+  (save-excursion
+    ;; If we're already at the beginning of the function definition,
+    ;; `beginning-of-defun' moves to the previous function, so move elsewhere.
+    (end-of-line)
+
+    (beginning-of-defun)
+    (if (looking-at "pub ")
+        (delete-char 4)
+      (insert "pub "))))
+
+(define-key rust-mode-map (kbd "C-c v") #'wh/rust-toggle-visibility)
+
 (provide 'rust-customisations)
