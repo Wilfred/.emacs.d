064867a7bebfe095de3a1cd90fe259f53a2669f0
Wilfred Hughes <me@wilfred.me.uk>
2015-09-25 20:41:34 +0100

    Remove realgud.
    I don't use gdb much, so I've never really used realgud. Furthermore, it
    doesn't byte-compile, which is annoying.

--- elpa/realgud-20150719.1657/realgud-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; realgud-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("realgud-pkg.el" "realgud.el") (21967
-;;;;;;  8667 914006 680000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; realgud-autoloads.el ends here
