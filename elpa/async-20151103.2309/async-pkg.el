3c118acb4768a87ea5416df64b11a87f249ce4f0
Wilfred Hughes <whughes@ahl.com>
2015-11-04 14:46:11 +0000

    Version bump magit for latest release.

---
+++ elpa/async-20151103.2309/async-pkg.el
@@ -0,0 +1,7 @@
+(define-package "async" "20151103.2309" "Asynchronous processing in Emacs"
+  '((emacs "24")
+    (cl-lib "0.5"))
+  :url "https://github.com/jwiegley/emacs-async")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/dash-20151021.113/dash-pkg.el
@@ -0,0 +1,1 @@
+(define-package "dash" "20151021.113" "A modern list library for Emacs" 'nil :keywords '("lists"))
