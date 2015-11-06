e6d7496b238f3a53016bb380c5b9f889e2e0b15a
Wilfred Hughes <whughes@ahl.com>
2015-11-06 16:21:34 +0000

    Only use avy-goto-line in the current window.

--- user-lisp/movement-customisations.el
+++ user-lisp/movement-customisations.el
@@ -1,6 +1,9 @@
 ;; avy -- quickly jump to an arbitrary word or line
 (require 'avy)
 (setq avy-case-fold-search nil)
+;; Only consider the current window (i.e. the current buffer we're
+;; focused in).
+(setq avy-all-windows nil)
 (setq avy-keys
       (append (number-sequence ?a ?z) (number-sequence ?A ?Z)))
 (define-key global-map (kbd "<f11>") #'avy-goto-word-or-subword-1)
