47e7df287c78f0577a4464dea4cc74f67fd5dc3a
Wilfred Hughes <me@wilfred.me.uk>
2015-12-03 11:01:51 +0000

    Installing rustfmt.

---
+++ elpa/rustfmt-20151124.1111/rustfmt-autoloads.el
@@ -0,0 +1,26 @@
+;;; rustfmt-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "rustfmt" "rustfmt.el" (22112 5294 323768 116000))
+;;; Generated autoloads from rustfmt.el
+
+(autoload 'rustfmt-format-buffer "rustfmt" "\
+Format the current buffer using rustfmt.
+
+\(fn)" t nil)
+
+(autoload 'rustfmt-enable-on-save "rustfmt" "\
+Run rustfmt when saving buffer.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; rustfmt-autoloads.el ends here
---
+++ elpa/rustfmt-20151124.1111/rustfmt-pkg.el
@@ -0,0 +1,1 @@
+(define-package "rustfmt" "20151124.1111" "Format rust code using rustfmt" '((emacs "24")) :url "https://github.com/fbergroth/emacs-rustfmt" :keywords '("convenience"))
