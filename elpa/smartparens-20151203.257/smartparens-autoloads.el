55a3a1a0cd280c1ee06e20c8dd63b9914a0d093c
Wilfred Hughes <me@wilfred.me.uk>
2015-12-05 10:53:29 +0000

    Version bump smartparens.

--- elpa/smartparens-20151203.257/smartparens-autoloads.el
+++ elpa/smartparens-20151203.257/smartparens-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "smartparens" "smartparens.el" (22112 5654
-;;;;;;  681258 386000))
+;;;### (autoloads nil "smartparens" "smartparens.el" (22114 49273
+;;;;;;  298554 629000))
 ;;; Generated autoloads from smartparens.el
 
 (autoload 'sp-cheat-sheet "smartparens" "\
@@ -182,7 +182,7 @@ Turn off `show-smartparens-mode'.
 ;;;;;;  "smartparens-html.el" "smartparens-latex.el" "smartparens-lua.el"
 ;;;;;;  "smartparens-pkg.el" "smartparens-python.el" "smartparens-racket.el"
 ;;;;;;  "smartparens-ruby.el" "smartparens-rust.el" "smartparens-scala.el")
-;;;;;;  (22112 5655 59969 892000))
+;;;;;;  (22114 49273 860132 154000))
 
 ;;;***
 
