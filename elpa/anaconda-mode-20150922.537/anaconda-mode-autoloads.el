bdaf029398374e67fc80c3d2b649e43d3eab3253
Wilfred Hughes <me@wilfred.me.uk>
2015-12-11 12:19:08 +0000

    Version bump anaconda-mode.

--- elpa/anaconda-mode-20150922.537/anaconda-mode-autoloads.el
+++ elpa/anaconda-mode-20150922.537/anaconda-mode-autoloads.el
@@ -3,19 +3,9 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "anaconda-mode" "anaconda-mode.el" (21613 8223
-;;;;;;  377316 50000))
+;;;### (autoloads nil "anaconda-mode" "anaconda-mode.el" (22122 45920
+;;;;;;  665266 446000))
 ;;; Generated autoloads from anaconda-mode.el
-
-(autoload 'anaconda-mode-remote "anaconda-mode" "\
-Suggest anaconda_mode.py running on HOST at PORT.
-
-\(fn HOST PORT)" t nil)
-
-(autoload 'anaconda-mode-local "anaconda-mode" "\
-Suggest anaconda_mode.py running on localhost.
-
-\(fn)" t nil)
 
 (autoload 'anaconda-mode "anaconda-mode" "\
 Code navigation, documentation lookup and completion for Python.
@@ -23,11 +13,6 @@ Code navigation, documentation lookup and completion for Python.
 \\{anaconda-mode-map}
 
 \(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("anaconda-mode-pkg.el") (21613 8227 899288
-;;;;;;  641000))
 
 ;;;***
 
