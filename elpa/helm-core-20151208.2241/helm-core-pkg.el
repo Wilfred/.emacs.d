1695b0ed96c12bfb90730f8dda16a8aa12df96d4
Wilfred Hughes <me@wilfred.me.uk>
2015-12-11 12:28:40 +0000

    Version bump helm.

---
+++ elpa/helm-core-20151208.2241/helm-core-pkg.el
@@ -0,0 +1,8 @@
+(define-package "helm-core" "20151208.2241" "Development files for Helm"
+  '((emacs "24")
+    (cl-lib "0.5")
+    (async "1.6"))
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
