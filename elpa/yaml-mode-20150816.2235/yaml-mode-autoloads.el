d5b76ff3854208038c466c72207c1859a666dc38
Wilfred Hughes <me@wilfred.me.uk>
2015-12-13 19:38:48 +0000

    Version bump yaml-mode

--- elpa/yaml-mode-20150816.2235/yaml-mode-autoloads.el
+++ elpa/yaml-mode-20150816.2235/yaml-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "yaml-mode" "yaml-mode.el" (21620 58501 758101
-;;;;;;  160000))
+;;;### (autoloads nil "yaml-mode" "yaml-mode.el" (22122 45726 272582
+;;;;;;  43000))
 ;;; Generated autoloads from yaml-mode.el
 
 (let ((loads (get 'yaml 'custom-loads))) (if (member '"yaml-mode" loads) nil (put 'yaml 'custom-loads (cons '"yaml-mode" loads))))
@@ -16,7 +16,7 @@ Simple mode to edit YAML.
 
 \(fn)" t nil)
 
-(add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.e?ya?ml$" . yaml-mode))
 
 ;;;***
 
