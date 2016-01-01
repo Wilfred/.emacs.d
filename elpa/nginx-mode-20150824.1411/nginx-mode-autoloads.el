2de4d3e9ba2fab4ac7c38389eaabe7922dea7a2c
Wilfred Hughes <me@wilfred.me.uk>
2016-01-01 12:34:03 +0000

    Version bump nginx-mode.

--- elpa/nginx-mode-20150824.1411/nginx-mode-autoloads.el
+++ elpa/nginx-mode-20150824.1411/nginx-mode-autoloads.el
@@ -1,10 +1,10 @@
 ;;; nginx-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads (nginx-mode) "nginx-mode" "nginx-mode.el" (21423
-;;;;;;  59464 156316 906000))
+;;;### (autoloads nil "nginx-mode" "nginx-mode.el" (22122 45805 359636
+;;;;;;  102000))
 ;;; Generated autoloads from nginx-mode.el
 
 (autoload 'nginx-mode "nginx-mode" "\
@@ -15,20 +15,15 @@ The variable nginx-indent-level controls the amount of indentation.
 
 \(fn)" t nil)
 
-(add-to-list 'auto-mode-alist '("nginx.conf$" . nginx-mode) '("/etc/nginx/.*" . nginx-mode))
+(add-to-list 'auto-mode-alist '("nginx\\.conf\\'" . nginx-mode))
+
+(add-to-list 'auto-mode-alist '("/nginx/.+\\.conf\\'" . nginx-mode))
 
 ;;;***
 
-;;;### (autoloads nil nil ("nginx-mode-pkg.el") (21423 59464 211876
-;;;;;;  724000))
-
-;;;***
-
-(provide 'nginx-mode-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; nginx-mode-autoloads.el ends here
---
+++ elpa/nginx-mode-20150824.1411/nginx-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "nginx-mode" "20150824.1411" "major mode for editing nginx config files" 'nil :keywords '("nginx"))
