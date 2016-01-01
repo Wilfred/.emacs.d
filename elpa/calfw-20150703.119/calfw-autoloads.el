704a8863aee36b4904b0fe45565859256eab50c6
Wilfred Hughes <me@wilfred.me.uk>
2016-01-01 12:36:55 +0000

    Unused package.

--- elpa/calfw-20150703.119/calfw-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; calfw-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("calfw-cal.el" "calfw-howm.el" "calfw-ical.el"
-;;;;;;  "calfw-org.el" "calfw-pkg.el" "calfw.el") (21967 8762 499351
-;;;;;;  706000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; calfw-autoloads.el ends here
