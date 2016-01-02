143cc4c9772d53a213f6e801a2a0f51608081189
Wilfred Hughes <me@wilfred.me.uk>
2016-01-02 20:19:08 +0000

    Version bump popup.

--- elpa/popup-20151125.542/popup-autoloads.el
+++ elpa/popup-20151125.542/popup-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("popup.el") (21967 8671 709849 524000))
+;;;### (autoloads nil nil ("popup.el") (22122 45765 396325 924000))
 
 ;;;***
 
