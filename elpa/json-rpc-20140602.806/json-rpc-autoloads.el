cc3f8e9aa03b06a72fda83250508c4dbabe78311
Wilfred Hughes <me@wilfred.me.uk>
2016-01-02 20:18:40 +0000

    Remove unused package.

--- elpa/json-rpc-20140602.806/json-rpc-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; json-rpc-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("json-rpc.el") (21590 43732 818525 90000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; json-rpc-autoloads.el ends here
