0526e692ca036c80a1285eab4651b2f14180dd05
Wilfred Hughes <me@wilfred.me.uk>
2016-01-08 14:49:38 +0000

    Quick and dirty highlighting for ES log files.

---
+++ user-lisp/es-log-mode.el
@@ -0,0 +1,20 @@
+;;; es-log-mode --- Basic highlighting for elasticsearch log files
+
+(defvar es-log-mode-font-lock-keywords
+  `(
+    (,(rx line-start
+          (group "[" (*? not-newline) "]")
+          "[" (group (*? not-newline)) "]"
+          "[" (group (*? not-newline)) "]"
+          )
+     (1 font-lock-comment-face)
+     (2 font-lock-type-face)
+     (3 font-lock-variable-name-face)
+     )
+    ))
+
+(define-derived-mode es-log-mode text-mode "ES log"
+  "Major mode for view elasticsearch log files."
+  (setq-local font-lock-defaults '(es-log-mode-font-lock-keywords)))
+
+(provide 'es-log-mode)
