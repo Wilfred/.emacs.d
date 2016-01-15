01ced5f82a3f76afd058c551a919770cf59e4a20
Wilfred Hughes <me@wilfred.me.uk>
2016-01-15 14:35:31 +0000

    Allow sh and python code snippets in org-mode.

--- user-lisp/org-customisations.el
+++ user-lisp/org-customisations.el
@@ -5,4 +5,8 @@
 ;; Syntax highlight org code snippets.
 (setq org-src-fontify-natively t)
 
+;; Allow running code in sh and python
+(require 'ob-sh)
+(require 'ob-python)
+
 (provide 'org-customisations)
