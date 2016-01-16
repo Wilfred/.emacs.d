8d8d34e313228b6742bd9c68fae5926ae45273b0
Wilfred Hughes <me@wilfred.me.uk>
2016-01-16 16:48:36 +0000

    Install html-check-frag.

---
+++ elpa/html-check-frag-20160116.14/html-check-frag-autoloads.el
@@ -0,0 +1,16 @@
+;;; html-check-frag-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("html-check-frag.el") (22170 29695 856079
+;;;;;;  523000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; html-check-frag-autoloads.el ends here
---
+++ elpa/html-check-frag-20160116.14/html-check-frag-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "html-check-frag" "20160116.14" "Check html-fragments" '((emacs "24.3")) :keywords '("html"))
