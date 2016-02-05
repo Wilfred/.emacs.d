3bfde9f6360358f1ae984c9aa067227f3f62c1b1
Wilfred Hughes <me@wilfred.me.uk>
2016-02-05 22:35:27 +0000

    Removing help-fns+.
    I don't use it and I don't like the
    > For more information check the manuals.
    that it adds to every help buffer.

--- elpa/help-fns+-20151215.637/help-fns+-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "help-fns+" "20151215.637" "Extensions to `help-fns.el'." 'nil :url "http://www.emacswiki.org/help-fns+.el" :keywords '("help" "faces" "characters" "packages" "description"))
