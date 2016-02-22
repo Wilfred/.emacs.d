b3d57767ff4eecc72678a66a5b4c7e26e103819e
Wilfred Hughes <me@wilfred.me.uk>
2016-02-22 22:20:01 +0000

    Installing sotlisp

---
+++ elpa/sotlisp-20160222.813/sotlisp-pkg.el
@@ -0,0 +1,1 @@
+(define-package "sotlisp" "20160222.813" "Write lisp at the speed of thought." '((emacs "24.1")) :url "https://github.com/Malabarba/speed-of-thought-lisp" :keywords '("convenience" "lisp"))
