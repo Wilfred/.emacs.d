7afb39aff961bfc1fc1e8a6722de84188582865f
Wilfred Hughes <me@wilfred.me.uk>
2016-02-29 16:34:07 +0000

    More contrast in section highlighting in magit.

--- user-lisp/git-customisations.el
+++ user-lisp/git-customisations.el
@@ -13,6 +13,12 @@
 ;; track origin/A.
 (setq magit-branch-arguments (remove "--track" magit-branch-arguments))
 
+;; The default magit section highlighting is almost invisible on tangotango, so
+;; use a darker grey (we're using the same as hl-line here).
+;; TODO: send a patch to tangotango.
+(custom-set-faces
+ '(magit-section-highlight ((t (:background "grey14")))))
+
 ;; Highlight new/removed/changed lines relative to the last commit in
 ;; VCS.
 (global-diff-hl-mode)
@@ -28,10 +34,6 @@
 
 ;; Default colours are too subtle, make them obvious.
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(diff-hl-change ((t (:background "blue3" :foreground "blue3"))))
  '(diff-hl-delete ((t (:inherit diff-removed :background "red3" :foreground "red3"))))
  '(diff-hl-insert ((t (:inherit diff-added :background "green4" :foreground "green4")))))
