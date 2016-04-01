fdfaadca97a868f06bb6b0c4c87fcb732259d187
Wilfred Hughes <me@wilfred.me.uk>
2016-04-02 00:08:31 +0100

    Moving my pkg-config convenience tool to a separate package.

--- user-lisp/c-customisations.el
+++ user-lisp/c-customisations.el
@@ -17,45 +17,5 @@
          flycheck-clang-definitions '("__STDC_LIMIT_MACROS"
                                       "__STDC_CONSTANT_MACROS"))))
 
-(require 's)
-(require 'dash)
-
-(defvar wh/pkgconfig-libs nil)
-
-(defun wh/s-ignore-case-less-p (s1 s2)
-  (string< (downcase s1) (downcase s2)))
-
-(defun wh/flycheck-add-c-includes ()
-  "Configure flycheck to use additional includes
-when checking the current buffer."
-  (interactive)
-  ;; Find out all the libraries installed on this system.
-  (unless wh/pkgconfig-libs
-    (let* ((all-libs-with-names
-            (shell-command-to-string "pkg-config --list-all"))
-           (lines (s-split "\n" (s-trim all-libs-with-names)))
-           (libs (--map (-first-item (s-split " " it)) lines)))
-      (setq wh/pkgconfig-libs (-sort #'wh/s-ignore-case-less-p libs))))
-
-  (let* (;; Prompt for a library name.
-         (lib-name (completing-read "Library name: " wh/pkgconfig-libs))
-         ;; Find the include flags, e.g. "-I/usr/lib/foo"
-         (pkgconfig-cmd (format "pkg-config --cflags %s" lib-name))
-         (cc-args (s-trim (shell-command-to-string pkgconfig-cmd))))
-    (if (s-starts-with? "-I" cc-args)
-        ;; pkg-config has found a library with this name.
-        (let* ((include-args (s-split " " cc-args))
-               (lib-paths (--map (s-chop-prefix "-I" it) include-args)))
-          ;; Only set in this buffer.
-          (make-local-variable 'flycheck-clang-include-path)
-          ;; Add include paths to `flycheck-clang-include-path' unless
-          ;; already present.
-          (setq flycheck-clang-include-path
-                (-union flycheck-clang-include-path
-                        lib-paths))
-          (message "flycheck-clang-include-path: %s"
-                   flycheck-clang-include-path))
-      ;; Otherwise, no such library with this name.
-      (user-error cc-args))))
 
 (provide 'c-customisations)
