a69dd6d64a13e137152dd0a7608c5efbc1f759a4
Wilfred Hughes <me@wilfred.me.uk>
2016-04-03 13:38:25 +0100

    Install assess.
    It's not strictly necessary, but it's handy to interactively run ert
    tests.

---
+++ elpa/assess-20160226.429/assess-autoloads.el
@@ -0,0 +1,16 @@
+;;; assess-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("assess-discover.el" "assess-pkg.el" "assess.el")
+;;;;;;  (22271 43440 530887 35000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; assess-autoloads.el ends here
