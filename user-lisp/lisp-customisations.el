49179e2b04e5d967ed8168f006cfd0b71b42db18
Wilfred Hughes <me@wilfred.me.uk>
2016-04-04 21:02:33 -0400

    cider-style overlays in Emacs lisp

--- user-lisp/lisp-customisations.el
+++ user-lisp/lisp-customisations.el
@@ -96,4 +96,32 @@
 (add-hook 'eval-expression-minibuffer-setup-hook #'paredit-mode)
 (add-hook 'eval-expression-minibuffer-setup-hook #'eldoc-mode)
 
+;; from http://endlessparentheses.com/eval-result-overlays-in-emacs-lisp.html
+(autoload 'cider--make-result-overlay "cider-overlays")
+
+(defun endless/eval-overlay (value point)
+  (cider--make-result-overlay (format "%S" value)
+    :where point
+    :duration 'command)
+  ;; Preserve the return value.
+  value)
+
+(advice-add 'eval-region :around
+            (lambda (f beg end &rest r)
+              (endless/eval-overlay
+               (apply f beg end r)
+               end)))
+
+(advice-add 'eval-last-sexp :filter-return
+            (lambda (r)
+              (endless/eval-overlay r (point))))
+
+(advice-add 'eval-defun :filter-return
+            (lambda (r)
+              (endless/eval-overlay
+               r
+               (save-excursion
+                 (end-of-defun)
+                 (point)))))
+
 (provide 'lisp-customisations)
