421edaf612c07fc45fa4a0ec0f68bd57b924e0d7
Wilfred Hughes <whughes@ahl.com>
2016-04-19 18:00:55 +0100

    Don't push duplicates to the kill-ring.

--- user-lisp/kill-ring-customisations.el
+++ user-lisp/kill-ring-customisations.el
@@ -1,6 +1,10 @@
 ;; top of kill ring should also be in X clipboard
 (setq x-select-enable-clipboard t)
 
+;; If the current value is the same as the head of the kill ring,
+;; don't push the duplicate.
+(setq kill-do-not-save-duplicates t)
+
 (setq kill-ring-max 500)
 
 (provide 'kill-ring-customisations)
