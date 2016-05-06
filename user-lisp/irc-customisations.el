c97945f0d86cf485d82483e1b2c43d66f0969401
Wilfred Hughes <me@wilfred.me.uk>
2016-05-06 22:11:27 +0100

    Just join #emacs by default

--- user-lisp/irc-customisations.el
+++ user-lisp/irc-customisations.el
@@ -3,4 +3,5 @@
 (use-package rcirc
   :config
   (setq rcirc-default-nick "wilfredh")
-  (add-to-list 'rcirc-server-alist '("irc.mozilla.org" :channels ("#rust"))))
+  (setq rcirc-server-alist
+        '(("irc.freenode.net" :channels ("#emacs")))))
