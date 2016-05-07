4a33c58da4ea5832b0e9c2849ca076f7e413a0d8
Wilfred Hughes <me@wilfred.me.uk>
2016-05-07 23:58:17 +0100

    More config in use-package

--- user-lisp/org-customisations.el
+++ user-lisp/org-customisations.el
@@ -2,16 +2,18 @@
 
 (setq org-return-follows-link t)
 
-;; Syntax highlight org code snippets.
-(setq org-src-fontify-natively t)
-
 ;; Allow running code in sh and python
 (require 'ob-sh)
 (require 'ob-python)
 
 (use-package org
   :config
-  (setq org-clock-continuously t))
+  ;; When clocking in, just use the time from the last clocked out
+  ;; item.
+  (setq org-clock-continuously t)
+
+  ;; Syntax highlight org code snippets.
+  (setq org-src-fontify-natively t))
 
 (custom-set-faces
  '(org-date ((((class color)) (:underline nil))) t))
