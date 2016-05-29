6f720fc9ec4f8c5d545f601d2e0bb863d90c03dc
Wilfred Hughes <me@wilfred.me.uk>
2016-05-29 11:35:51 +0100

    Use use-package with etags-select

--- user-lisp/tags-customisations.el
+++ user-lisp/tags-customisations.el
@@ -3,31 +3,30 @@
 ;; Author: Wilfred Hughes <me@wilfred.me.uk>
 ;; Created: 1 October 2012
 
-(require 'etags-select)
-
-(global-set-key (kbd "<f6>") 'etags-select-find-tag-at-point)
-(global-set-key (kbd "M-.") 'etags-select-find-tag)
-(global-set-key (kbd "M-,") 'pop-tag-mark)
-
-(defadvice etags-select-find-tag (around case-sensitive-matching activate)
-  (let ((ido-case-fold nil))
-    ad-do-it))
-
-(defun etags-select-find-tag-other-window ()
-  "Equivalent to `etags-select-find-tag-at-point' but
+(use-package etags-select
+  :bind (("<f6>" . etags-select-find-tag-at-point)
+         ("M-." . etags-select-find-tag)
+         ("M-," . pop-tag-mark)
+         ("C-c M-." . etags-select-find-tag-other-window))
+  :config
+  (defadvice etags-select-find-tag (around case-sensitive-matching activate)
+    (let ((ido-case-fold nil))
+      ad-do-it))
+  
+  (defun etags-select-find-tag-other-window ()
+    "Equivalent to `etags-select-find-tag-at-point' but
 opening another window so the call site is still visible."
-  (interactive)
-  (delete-other-windows)
-  (split-window-right)
-  (other-window 1)
-  (etags-select-find-tag-at-point))
+    (interactive)
+    (delete-other-windows)
+    (split-window-right)
+    (other-window 1)
+    (etags-select-find-tag-at-point))
 
-(global-set-key (kbd "C-c M-.") 'etags-select-find-tag-other-window)
+  ;; finding tags should be case sensitive
+  (setq tags-case-fold-search nil)
 
-;; finding tags should be case sensitive
-(setq tags-case-fold-search nil)
+  ;; Always append tags to the tags table without prompting.
+  (setq tags-add-tables t))
 
-;; Always append tags to the tags table without prompting.
-(setq tags-add-tables t)
 
 (provide 'tags-customisations)
