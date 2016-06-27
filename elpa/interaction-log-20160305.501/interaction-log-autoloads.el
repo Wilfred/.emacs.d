4902712931ba5088a8d52b8d74372d346aaac181
Wilfred Hughes <me@wilfred.me.uk>
2016-06-27 08:29:10 +0100

    Install interaction log

---
+++ elpa/interaction-log-20160305.501/interaction-log-autoloads.el
@@ -0,0 +1,16 @@
+;;; interaction-log-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("interaction-log.el") (22383 42301 701034
+;;;;;;  849000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; interaction-log-autoloads.el ends here
---
+++ elpa/interaction-log-20160305.501/interaction-log-pkg.el
@@ -0,0 +1,1 @@
+(define-package "interaction-log" "20160305.501" "exhaustive log of interactions with Emacs" '((cl-lib "0")) :url "https://github.com/michael-heerdegen/interaction-log.el" :keywords '("convenience"))
