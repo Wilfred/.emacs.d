d36500029113adf376bcb7e1176b36257c71df94
Wilfred Hughes <me@wilfred.me.uk>
2016-06-29 10:37:30 +0100

    Refactor: don't depend on cl-loop, cl-defun, use save-excursion

--- user-lisp/python-customisations.el
+++ user-lisp/python-customisations.el
@@ -22,7 +22,7 @@
 
 (require 'python)
 (require 'python-utils)
-(eval-when-compile (require 'cl)) ;; first, second, loop
+(eval-when-compile (require 'cl)) ;; first, second
 
 ;; TODO: properly highlight differently named self arguments (often seen in nested classes):
 
@@ -33,45 +33,38 @@
 (require 'which-func)
 
 ;; FIXME: breaks for class methods, since it assumes `self'
-(defun* python-insert-super-function (&aux start-pos)
+(defun python-insert-super-function ()
   "Insert a call to super for the current class and function."
   (interactive)
+  (let* (class-name method-name args-start args-end)
+    ;; for some reason, we need a non-empty block for which-function to find the name of the method
+    (insert "pass")
 
-  (setq start-pos (point))
+    (let ((exact-position (split-string (which-function) (rx ".")))) ;; e.g. ("FooBar" "method")
+      (setq class-name (first exact-position))
+      (setq method-name (second exact-position)))
 
-  ;; for some reason, we need a non-empty block for which-function to find the name of the method
-  (insert "pass")
-
-  (let* ((exact-position (split-string (which-function) (rx "."))) ; e.g. ("FooBar" "method")
-         (class-name (first exact-position))
-         (method-name (second exact-position))
-         args-start
-         args-end)
     ;; remove 'pass'
     (backward-delete-char 4)
 
-    ;; go to the function definition and find the arguments
-    (beginning-of-defun)
+    (save-excursion
+      ;; go to the function definition and find the arguments
+      (beginning-of-defun)
 
-    (loop until (looking-at "(")
-          do (forward-char))
-    (setq args-start (1+ (point)))
-
-    (forward-sexp)
-    (setq args-end (1- (point)))
-    
-    ;; reset point to where we started
-    (goto-char start-pos)
+      (while (not (looking-at "("))
+        (forward-char))
+      (setq args-start (1+ (point)))
+      (forward-sexp)
+      (setq args-end (1- (point))))
 
     (let ((args (s-trim (buffer-substring args-start args-end))))
-      
       ;; remove 'self'
       (setq args (s-trim (s-chop-prefixes '("self," "self") args)))
 
-      (insert (format "super(%s, self).%s(%s)" class-name method-name args)))
+      (insert (format "super(%s, self).%s(%s)" class-name method-name args))))
 
-    ;; backward one char so the user can enter the argument for the superclass's function
-    (backward-char)))
+  ;; backward one char so the user can enter the argument for the superclass's function
+  (backward-char))
 
 (define-key python-mode-map (kbd "C-M-n") 'python-nav-forward-statement)
 (define-key python-mode-map (kbd "C-M-p") 'python-nav-backward-statement)
