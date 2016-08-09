8fcf08ad9af3644352be5862800b3e165215959a
Wilfred Hughes <me@wilfred.me.uk>
2016-08-09 15:59:45 +0100

    Correct hook name

--- user-lisp/javascript-customisations.el
+++ user-lisp/javascript-customisations.el
@@ -25,7 +25,7 @@
 (setq js2-strict-trailing-comma-warning nil)
 
 (add-hook 'js2-jsx-mode-hook #'flycheck-mode)
-(add-hook 'js2-mode #'flycheck-mode)
+(add-hook 'js2-mode-hook #'flycheck-mode)
 
 (require 'company)
 (add-to-list 'company-backends 'company-tern)
