5d55d94e9932bac5f2b04a63b30abd6537e96d37
Wilfred Hughes <me@wilfred.me.uk>
2016-08-23 15:33:47 +0100

    yaml-mode already sets auto-mode-alist

--- user-lisp/structured-text-customisations.el
+++ user-lisp/structured-text-customisations.el
@@ -3,8 +3,6 @@
 
 ; yaml mode stuff, since google app engine uses it
 (require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
 (add-hook 'yaml-mode-hook
 	  '(lambda ()
 	     (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
