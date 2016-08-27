53b6df39c39a8a6a816a3f8b5d9de51f5e8ddabc
Wilfred Hughes <me@wilfred.me.uk>
2016-08-27 12:54:36 -0400

    More multiple-cursors commands

--- .mc-lists.el
+++ .mc-lists.el
@@ -21,6 +21,12 @@
         jump-char-process-char
         kill-or-delete-region
         kill-visual-line
+        lispy-braces
+        lispy-brackets
+        lispy-delete
+        lispy-kill
+        lispy-parens
+        lispy-quotes
         markdown-exdent-or-delete
         open-newline-and-indent
         org-delete-char
@@ -36,6 +42,76 @@
         sp-forward-sexp
         sp-remove-active-pair-overlay
         sp-slurp-hybrid-sexp
+        special-digit-argument
+        special-lh-knight/body
+        special-lispy-ace-char
+        special-lispy-ace-paren
+        special-lispy-ace-subword
+        special-lispy-ace-symbol
+        special-lispy-ace-symbol-replace
+        special-lispy-alt-multiline
+        special-lispy-back
+        special-lispy-barf
+        special-lispy-beginning-of-defun
+        special-lispy-clone
+        special-lispy-convolute
+        special-lispy-convolute-left
+        special-lispy-different
+        special-lispy-down
+        special-lispy-down-slurp
+        special-lispy-edebug-stop
+        special-lispy-ediff-regions
+        special-lispy-eval
+        special-lispy-eval-and-insert
+        special-lispy-eval-other-window
+        special-lispy-flow
+        special-lispy-follow
+        special-lispy-goto
+        special-lispy-goto-def-ace
+        special-lispy-goto-def-down
+        special-lispy-goto-elisp-commands
+        special-lispy-goto-local
+        special-lispy-goto-mode
+        special-lispy-goto-projectile
+        special-lispy-goto-recursive
+        special-lispy-join
+        special-lispy-left
+        special-lispy-mark-list
+        special-lispy-move-down
+        special-lispy-move-left
+        special-lispy-move-right
+        special-lispy-move-up
+        special-lispy-narrow
+        special-lispy-new-copy
+        special-lispy-occur
+        special-lispy-oneline
+        special-lispy-other-mode
+        special-lispy-other-space
+        special-lispy-outline-goto-child
+        special-lispy-outline-next
+        special-lispy-outline-prev
+        special-lispy-paste
+        special-lispy-quit
+        special-lispy-raise
+        special-lispy-raise-some
+        special-lispy-repeat
+        special-lispy-right
+        special-lispy-shifttab
+        special-lispy-slurp
+        special-lispy-splice
+        special-lispy-stringify
+        special-lispy-tab
+        special-lispy-teleport
+        special-lispy-tilde
+        special-lispy-underscore
+        special-lispy-undo
+        special-lispy-up
+        special-lispy-up-slurp
+        special-lispy-view
+        special-lispy-visit
+        special-lispy-widen
+        special-lispy-x
+        special-pop-tag-mark
         wh/decrement-number-decimal
         wh/increment-number-decimal
         wh/kill-symbol
@@ -53,6 +129,8 @@
         flycheck-previous-error
         helm-M-x
         iedit-mode
+        ivy-alt-done
+        lispy-cursor-down
         mouse-set-region
         smex
         sp-rewrap-sexp
