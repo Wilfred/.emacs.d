0dc2813207e9fca4afced85b358da327da7e24a6
Wilfred Hughes <me@wilfred.me.uk>
2016-08-31 23:20:13 -0400

    Connect to #rust too

--- user-lisp/irc-customisations.el
+++ user-lisp/irc-customisations.el
@@ -4,6 +4,7 @@
   :config
   (setq rcirc-default-nick "wilfredh")
   (setq rcirc-server-alist
-        '(("irc.freenode.net" :channels ("#emacs")))))
+        '(("irc.freenode.net" :channels ("#emacs"))
+          ("irc.mozilla.org" :channels ("#rust")))))
 
 (provide 'irc-customisations)
