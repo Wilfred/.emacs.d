d4f186fe26762bc1a76277928749afc4ff6fffb6
Wilfred Hughes <me@wilfred.me.uk>
2016-09-07 21:15:13 -0400

    Join #guile

--- user-lisp/irc-customisations.el
+++ user-lisp/irc-customisations.el
@@ -4,7 +4,7 @@
   :config
   (setq rcirc-default-nick "wilfredh")
   (setq rcirc-server-alist
-        '(("irc.freenode.net" :channels ("#emacs"))
+        '(("irc.freenode.net" :channels ("#emacs" "#guile"))
           ("irc.mozilla.org" :channels ("#rust")))))
 
 (provide 'irc-customisations)
