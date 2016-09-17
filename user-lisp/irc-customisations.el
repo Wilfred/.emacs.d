946b8a116ff7aa30f360ab55044a598426657efa
Wilfred Hughes <me@wilfred.me.uk>
2016-09-17 19:42:34 -0400

    Keep IRC history

--- user-lisp/irc-customisations.el
+++ user-lisp/irc-customisations.el
@@ -5,6 +5,8 @@
   (setq rcirc-default-nick "wilfredh")
   (setq rcirc-server-alist
         '(("irc.freenode.net" :channels ("#emacs" "#guile"))
-          ("irc.mozilla.org" :channels ("#rust")))))
+          ("irc.mozilla.org" :channels ("#rust"))))
+  (setq rcirc-log-flag t)
+  (setq rcirc-log-directory "~/irc_logs"))
 
 (provide 'irc-customisations)
