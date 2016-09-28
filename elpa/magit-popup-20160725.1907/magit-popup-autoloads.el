dcb2095539e574bc81a7f24dfdee56457543e6e6
Wilfred Hughes <me@wilfred.me.uk>
2016-09-28 17:27:42 -0400

    Update magit and its dependencies

--- elpa/magit-popup-20160725.1907/magit-popup-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; magit-popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
-;;;;;;  (22426 55044 401825 274000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; magit-popup-autoloads.el ends here
