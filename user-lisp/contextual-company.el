901e2ca96575e7e019769f992a1f3264cf01b389
Wilfred Hughes <me@wilfred.me.uk>
2016-10-17 23:36:15 -0400

    Ideas for contextual completion in elisp

---
+++ user-lisp/contextual-company.el
@@ -0,0 +1,23 @@
+;; Ideas:
+
+;; (defcustom racer-complete-in-comments
+;;   nil
+;;   "foo"
+;;   :type 'boolean
+;;   :group 'racer)
+;;
+;; Could complete group value, simple type values, and keys to
+;; defcustom.
+
+;; Completing functions and variables based on context.
+;;
+;; Auto insert space when completing (foo ) if foo takes multiple
+;; args.
+;;
+;; Completion inside emacs style `backticks'.
+;;
+;; Even when we can't offer an exact set of options, offer the current
+;; package as a prefix.
+;;
+;; Consider only offering autoloaded/required functions in the current
+;; file.
