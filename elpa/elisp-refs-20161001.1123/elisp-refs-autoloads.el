8c09fe0f94968a177277fa7254da792f38b86e88
Wilfred Hughes <me@wilfred.me.uk>
2016-10-17 21:41:19 -0400

    Install elisp-refs

---
+++ elpa/elisp-refs-20161001.1123/elisp-refs-autoloads.el
@@ -0,0 +1,46 @@
+;;; elisp-refs-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "elisp-refs" "elisp-refs.el" (22533 32010 661299
+;;;;;;  931000))
+;;; Generated autoloads from elisp-refs.el
+
+(autoload 'elisp-refs-function "elisp-refs" "\
+Display all the references to function SYMBOL, in all loaded
+elisp files.
+
+\(fn SYMBOL)" t nil)
+
+(autoload 'elisp-refs-macro "elisp-refs" "\
+Display all the references to macro SYMBOL, in all loaded
+elisp files.
+
+\(fn SYMBOL)" t nil)
+
+(autoload 'elisp-refs-special "elisp-refs" "\
+Display all the references to special form SYMBOL, in all loaded
+elisp files.
+
+\(fn SYMBOL)" t nil)
+
+(autoload 'elisp-refs-variable "elisp-refs" "\
+Display all the references to variable SYMBOL, in all loaded
+elisp files.
+
+\(fn SYMBOL)" t nil)
+
+(autoload 'elisp-refs-symbol "elisp-refs" "\
+Display all the references to SYMBOL in all loaded elisp files.
+
+\(fn SYMBOL)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; elisp-refs-autoloads.el ends here
