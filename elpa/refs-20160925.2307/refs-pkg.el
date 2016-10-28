d373180fbf71900a22e1d73eed79213842337210
Wilfred Hughes <me@wilfred.me.uk>
2016-10-27 23:30:30 -0400

    Remove old version of refs.
    We already have elisp-refs installed anyway.

--- elpa/refs-20160925.2307/refs-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "refs" "20160925.2307" "find callers of elisp functions or macros" '((dash "2.12.0") (f "0.18.2") (list-utils "0.4.4") (loop "2.1") (shut-up "0.3.2")) :authors '(("Wilfred Hughes" . "me@wilfred.me.uk")) :maintainer '("Wilfred Hughes" . "me@wilfred.me.uk"))
