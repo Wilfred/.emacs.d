7bb5d685dee1c1f299f97dcbbd4503890b0f95d3
Wilfred Hughes <me@wilfred.me.uk>
2016-12-26 18:20:00 +0000

    Move customized data out of init.org to customized.el

--- init.el
+++ init.el
@@ -27,6 +27,9 @@
 
 (add-to-list 'load-path "~/.emacs.d/user-lisp/")
 
+(setq custom-file "~/.emacs.d/user-lisp/customized.el")
+(load custom-file)
+
 
 
 ;; Code that I haven't written lives in third-party-lisp. This directory
@@ -1144,42 +1147,6 @@ Visit the file after creation."
   (interactive))
 (defun suspend-frame (&rest)
   (interactive))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(frame-background-mode (quote dark))
- '(safe-local-variable-values
-   (quote
-    ((eval font-lock-add-keywords nil
-           (\`
-            (((\,
-               (concat "("
-                       (regexp-opt
-                        (quote
-                         ("sp-do-move-op" "sp-do-move-cl" "sp-do-put-op" "sp-do-put-cl" "sp-do-del-op" "sp-do-del-cl"))
-                        t)
-                       "\\_>"))
-              1
-              (quote font-lock-variable-name-face)))))))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ahs-plugin-defalt-face ((t nil)))
- '(ethan-wspace-face ((t (:background "#2e3434"))))
- '(flymake-errline ((((class color)) (:underline "Red"))))
- '(flymake-warnline ((((class color)) (:underline "Orange"))))
- '(highlight-symbol-face ((t (:underline t))))
- '(hl-line ((t (:background "gray14"))))
- '(hl-sexp-face ((t (:background "gray14"))))
- '(js2-function-param-face ((((class color)) (:foreground "Green"))))
- '(org-level-1 ((t (:foreground "dodger blue" :weight bold :height 1.0))))
- '(org-level-2 ((t (:foreground "#edd400" :weight bold :height 1.0))))
- '(smerge-refined-change ((t (:background "black")))))
 
 (put 'narrow-to-region 'disabled nil)
 
---
+++ user-lisp/customized.el
@@ -0,0 +1,37 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(frame-background-mode (quote dark))
+ '(safe-local-variable-values
+   (quote
+    ((eval font-lock-add-keywords nil
+           (\`
+            (((\,
+               (concat "("
+                       (regexp-opt
+                        (quote
+                         ("sp-do-move-op" "sp-do-move-cl" "sp-do-put-op" "sp-do-put-cl" "sp-do-del-op" "sp-do-del-cl"))
+                        t)
+                       "\\_>"))
+              1
+              (quote font-lock-variable-name-face)))))))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ahs-plugin-defalt-face ((t nil)))
+ '(ethan-wspace-face ((t (:background "#2e3434"))))
+ '(flymake-errline ((((class color)) (:underline "Red"))))
+ '(flymake-warnline ((((class color)) (:underline "Orange"))))
+ '(highlight-symbol-face ((t (:underline t))))
+ '(hl-line ((t (:background "gray14"))))
+ '(hl-sexp-face ((t (:background "gray14"))))
+ '(js2-function-param-face ((((class color)) (:foreground "Green"))))
+ '(org-level-1 ((t (:foreground "dodger blue" :weight bold :height 1.0))))
+ '(org-level-2 ((t (:foreground "#edd400" :weight bold :height 1.0))))
+ '(smerge-refined-change ((t (:background "black")))))
+
