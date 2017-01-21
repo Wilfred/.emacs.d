902485e091b55d22836f76a897b0a2f0fbdefbf4
Wilfred Hughes <me@wilfred.me.uk>
2017-01-21 19:13:10 +0000

    Install unfill

---
+++ elpa/unfill-20160816.2300/unfill-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "unfill" "20160816.2300" "Unfill paragraphs or regions, and toggle between filled & unfilled" 'nil :url "https://github.com/purcell/unfill" :keywords '("utilities"))
---
+++ elpa/unfill-20160816.2300/unfill.el
@@ -0,0 +1,58 @@
+;;; unfill.el --- Unfill paragraphs or regions, and toggle between filled & unfilled
+
+;; Copyright (C) 2012-2016 Steve Purcell.
+
+;; Author: Steve Purcell <steve@sanityinc.com>
+;; X-URL: https://github.com/purcell/unfill
+;; Version: DEV
+;; Package-Version: 20160816.2300
+;; Keywords: utilities
+
+;;; Commentary:
+
+;; Provides commands for explicitly unfilling (ie. unwrapping)
+;; paragraphs and regions, and also a command that will toggle between
+;; filling and unfilling the current paragraph or region.
+
+;; Based initially on Xah Lee's examples, and later rewritten based on an article by Artur Malabarba.
+;;   http://endlessparentheses.com/fill-and-unfill-paragraphs-with-a-single-key.html
+;;   http://xahlee.org/emacs/emacs_unfill-paragraph.html
+;;   http://xahlee.org/emacs/modernization_fill-paragraph.html
+
+;; This file is NOT part of GNU Emacs.
+
+;;; Code:
+
+;;;###autoload
+(defun unfill-paragraph ()
+  "Replace newline chars in current paragraph by single spaces.
+This command does the inverse of `fill-paragraph'."
+  (interactive)
+  (let ((fill-column most-positive-fixnum))
+    (call-interactively 'fill-paragraph)))
+
+;;;###autoload
+(defun unfill-region (start end)
+  "Replace newline chars in region from START to END by single spaces.
+This command does the inverse of `fill-region'."
+  (interactive "r")
+  (let ((fill-column most-positive-fixnum))
+    (fill-region start end)))
+
+;;;###autoload
+(defun unfill-toggle ()
+  "Toggle filling/unfilling of the current region, or current paragraph if no region active."
+  (interactive)
+  (let (deactivate-mark
+        (fill-column
+         (if (eq last-command this-command)
+             (progn (setq this-command nil)
+                    most-positive-fixnum)
+           fill-column)))
+    (call-interactively 'fill-paragraph)))
+
+;;;###autoload
+(define-obsolete-function-alias 'toggle-fill-unfill 'unfill-toggle)
+
+(provide 'unfill)
+;;; unfill.el ends here
