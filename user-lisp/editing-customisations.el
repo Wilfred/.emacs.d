e58fead9119f79673353e9ee762e15b30fd5f842
Wilfred Hughes <me@wilfred.me.uk>
2017-02-03 10:29:59 +0000

    Use a keybinding next to expand-region, as the docs suggest

--- user-lisp/editing-customisations.el
+++ user-lisp/editing-customisations.el
@@ -198,7 +198,7 @@ are interchanged."
 (require 'multiple-cursors)
 (require 'mc-mark-more)
 
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-]") #'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
