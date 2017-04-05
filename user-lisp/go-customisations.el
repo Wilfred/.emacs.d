ead06228504820d10f83660a70c0a942c5b08dff
Wilfred Hughes <whughes@ahl.com>
2017-04-05 10:41:01 +0100

    Configure go formatting

--- user-lisp/go-customisations.el
+++ user-lisp/go-customisations.el
@@ -3,6 +3,16 @@
 ;; Go is indented with tabs, so set the tab size in those buffers.
 (defun wh/set-go-tab-width ()
   (setq tab-width 4))
+
 (add-hook 'go-mode-hook #'wh/set-go-tab-width)
 
+(require 'go-mode)
+
+(setq gofmt-command "~/go/bin/goimports")
+;; Using -s with goimports is not supported with upstream goimports.
+;; See https://github.com/golang/go/issues/8759 . Instead, use
+;; $ go get github.com/jzelinskie/tools/cmd/goimports
+(setq gofmt-args (list "-s"))
+
+(setq godef-command "~/go/bin/godef")
 (provide 'go-customisations)
