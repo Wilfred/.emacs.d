c4368833d6984a031551771a868e227970dff56e
Wilfred Hughes <whughes@ahl.com>
2017-04-20 14:26:59 +0100

    Enable projectile caching

--- user-lisp/projectile-customisations.el
+++ user-lisp/projectile-customisations.el
@@ -1,6 +1,9 @@
 (setq projectile-svn-command
       "find . -type f | grep -v '/.svn/' | tr '\\n' '\\0'")
 
+;; For large files, projectile-find-file can be a little sluggish. Cache the result.
+(setq projectile-enable-caching t)
+
 (diminish-undo #'projectile-mode)
 (setq projectile-mode-line
       '(:eval
