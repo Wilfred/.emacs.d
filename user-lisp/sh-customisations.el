2f8cffce53d71e83945ea5973d454ea0d22b0387
Wilfred Hughes <whughes@ahl.com>
2017-05-24 13:41:38 +0100

    Backport my sh-mode fix for RPM spec files

--- user-lisp/sh-customisations.el
+++ user-lisp/sh-customisations.el
@@ -1,4 +1,11 @@
 (add-to-list 'auto-mode-alist '("\\.zsh$" . sh-mode))
 (add-to-list 'auto-mode-alist '("PKGBUILD$" . sh-mode))
 
+;; Backported from http://git.savannah.gnu.org/cgit/emacs.git/commit/?id=63d0a3c63f833faad7a04fb4bf384d55ae6ae8d1
+(setq sh-mode-syntax-table-input
+      `((sh . nil)
+        ;; Treat ' as punctuation rather than a string delimiter, as RPM
+        ;; files often contain prose with apostrophes.
+        (rpm . (,sh-mode-syntax-table ?\' "."))))
+
 (provide 'sh-customisations)
