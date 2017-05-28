6f03fff605817620b3205b8a96200a54526e7e5a
Wilfred Hughes <me@wilfred.me.uk>
2017-05-28 18:54:00 +0100

    Ensure we can start up without error

--- user-lisp/sh-customisations.el
+++ user-lisp/sh-customisations.el
@@ -2,6 +2,7 @@
 (add-to-list 'auto-mode-alist '("PKGBUILD$" . sh-mode))
 
 ;; Backported from http://git.savannah.gnu.org/cgit/emacs.git/commit/?id=63d0a3c63f833faad7a04fb4bf384d55ae6ae8d1
+(require 'sh-script)
 (setq sh-mode-syntax-table-input
       `((sh . nil)
         ;; Treat ' as punctuation rather than a string delimiter, as RPM
