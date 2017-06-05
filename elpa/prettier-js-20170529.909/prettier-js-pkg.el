e791db268fbea8e4591c59ee0d6e286e9fd04e1c
Wilfred Hughes <me@wilfred.me.uk>
2017-06-05 13:50:05 +0100

    Install prettier-js

---
+++ elpa/prettier-js-20170529.909/prettier-js-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "prettier-js" "20170529.909" "Minor mode to format JS code on file save" 'nil :commit "29a1b55b372ae88311157b411d81d9818cedd24f" :url "https://github.com/prettier/prettier/blob/master/editors/emacs/prettier-js.el" :keywords '("convenience" "wp" "edit" "js"))
