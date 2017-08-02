5fa8126d57368fda88172f4e2fb477bb9c64aacb
Wilfred Hughes <me@wilfred.me.uk>
2017-08-02 11:57:44 +0100

    Install go-rename

---
+++ elpa/go-rename-20161117.331/go-rename-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "go-rename" "20161117.331" "Integration of the 'gorename' tool into Emacs." '((go-mode "1.3.1")) :commit "99b06da201afb91e8db0c525c0f3c3590fd92fa6" :keywords '("tools"))
