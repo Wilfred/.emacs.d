b7e345ffbe602a0e66cfd547c7235debaa14f193
Wilfred Hughes <whughes@ahl.com>
2017-08-07 12:26:34 +0100

    Prefer flake8 to pyflakes

--- elpa/flycheck-pyflakes-20140630.1521/flycheck-pyflakes-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; flycheck-pyflakes-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("flycheck-pyflakes-pkg.el" "flycheck-pyflakes.el")
-;;;;;;  (21430 65037 231261 926000))
-
-;;;***
-
-(provide 'flycheck-pyflakes-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; flycheck-pyflakes-autoloads.el ends here
--- elpa/flycheck-pyflakes-20140630.1521/flycheck-pyflakes-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "flycheck-pyflakes" "20140630.1521" "Support pyflakes in flycheck" (quote ((flycheck "0.18"))))
