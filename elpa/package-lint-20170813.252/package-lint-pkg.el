a76ef5c6bcfd1d38d6f9a4c8d65a314fe33dcfdc
Wilfred Hughes <me@wilfred.me.uk>
2017-08-13 12:00:55 +0100

    Install package-lint

---
+++ elpa/package-lint-20170813.252/package-lint-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "package-lint" "20170813.252" "A linting library for elisp package authors" '((cl-lib "0.5") (emacs "24")) :commit "e61964db1793b1a36fe9020e383470acef6e112f" :url "https://github.com/purcell/package-lint" :keywords '("lisp"))
