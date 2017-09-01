c66dfde935d75cd1ff5e9bad71689c39f93caade
Wilfred Hughes <me@wilfred.me.uk>
2017-09-01 18:13:57 +0100

    Version bump suggest

--- elpa/suggest-20170806.1414/suggest-autoloads.el
+++ elpa/suggest-20170806.1414/suggest-autoloads.el
@@ -3,7 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "suggest" "suggest.el" (22903 1842 448907 378000))
+;;;### (autoloads nil "suggest" "suggest.el" (22953 38105 171449
+;;;;;;  644000))
 ;;; Generated autoloads from suggest.el
 
 (autoload 'suggest "suggest" "\
