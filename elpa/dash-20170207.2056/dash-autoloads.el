bf57227f7c2c095d02261d0bc544d9f0e302353b
Wilfred Hughes <me@wilfred.me.uk>
2017-09-04 11:30:58 +0100

    Version bump magit and its dependencies
    Magit has had some caching/performance improvements recently, which
    would be nice to have.
    
    See https://github.com/magit/magit/pull/3096.

--- elpa/dash-20170207.2056/dash-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("dash-pkg.el" "dash.el") (22697 45798
-;;;;;;  930803 98000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
--- elpa/magit-popup-20170214.347/magit-popup-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "magit-popup" "20170214.347" "Define prefix-infix-suffix command combos"
-  '((emacs "24.4")
-    (async "20161103.1036")
-    (dash "20170207.2056"))
-  :url "https://github.com/magit/magit" :keywords
-  '("bindings"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
