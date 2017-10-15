a7403dc8de3c101d21c8162066e397dd9d4a6ed7
Wilfred Hughes <me@wilfred.me.uk>
2017-10-15 23:04:39 +0100

    Move rust configuration into use-package block

--- user-lisp/rust-customisations.el
+++ user-lisp/rust-customisations.el
@@ -1,86 +1,84 @@
-(require 'f)
-
-;; Work around https://github.com/flycheck/flycheck-rust/issues/47
-(setq flycheck-rust-check-tests nil)
-
-(add-hook 'rust-mode-hook #'flycheck-mode)
-
-(add-to-list 'exec-path (f-expand "~/.cargo/bin"))
-
 (use-package racer
   :config
+  (require 'f)
   (setq racer-cmd (f-expand "~/.cargo/bin/racer")))
 
-(require 'company)
+(use-package rust-mode
+  :config
+  (add-hook 'rust-mode-hook #'flycheck-mode)
+  ;; Work around https://github.com/flycheck/flycheck-rust/issues/47
+  (setq flycheck-rust-check-tests nil)
 
-(defun wh/rust-company-settings ()
-  "Set up company settings for rust buffers."
-  (setq-local company-idle-delay 0.1))
+  (require 'f)
+  (add-to-list 'exec-path (f-expand "~/.cargo/bin"))
 
-(add-hook 'rust-mode-hook #'wh/rust-company-settings)
+  (defun wh/rust-company-settings ()
+    "Set up company settings for rust buffers."
+    (setq-local company-idle-delay 0.1))
 
-(require 'rust-mode)
-(define-key rust-mode-map (kbd "M-.") #'racer-find-definition)
+  (add-hook 'rust-mode-hook #'wh/rust-company-settings)
 
-(defun wh/rust-toggle-visibility ()
-  "Toggle the public visibility of the function at point."
-  (interactive)
-  (save-excursion
-    ;; If we're already at the beginning of the function definition,
-    ;; `beginning-of-defun' moves to the previous function, so move elsewhere.
-    (end-of-line)
+  (require 'rust-mode)
+  (define-key rust-mode-map (kbd "M-.") #'racer-find-definition)
 
-    (beginning-of-defun)
-    (if (looking-at "pub ")
-        (delete-char 4)
-      (insert "pub "))))
+  (defun wh/rust-toggle-visibility ()
+    "Toggle the public visibility of the function at point."
+    (interactive)
+    (save-excursion
+      ;; If we're already at the beginning of the function definition,
+      ;; `beginning-of-defun' moves to the previous function, so move elsewhere.
+      (end-of-line)
 
-(define-key rust-mode-map (kbd "C-c v") #'wh/rust-toggle-visibility)
+      (beginning-of-defun)
+      (if (looking-at "pub ")
+          (delete-char 4)
+        (insert "pub "))))
 
-(defun wh/rust-toggle-mutability ()
-  "Toggle the mutability of the variable at point."
-  (interactive)
-  (save-excursion
-    (racer-find-definition)
-    (back-to-indentation)
-    (forward-char 4)
-    (if (looking-at "mut ")
-        (delete-char 4)
-      (insert "mut "))))
+  (define-key rust-mode-map (kbd "C-c v") #'wh/rust-toggle-visibility)
 
-(define-key rust-mode-map (kbd "C-c m") #'wh/rust-toggle-mutability)
+  (defun wh/rust-toggle-mutability ()
+    "Toggle the mutability of the variable at point."
+    (interactive)
+    (save-excursion
+      (racer-find-definition)
+      (back-to-indentation)
+      (forward-char 4)
+      (if (looking-at "mut ")
+          (delete-char 4)
+        (insert "mut "))))
 
-(defun wh/rust-vec-as-slice ()
-  "Convert the vector expression at point to a slice.
+  (define-key rust-mode-map (kbd "C-c m") #'wh/rust-toggle-mutability)
+
+  (defun wh/rust-vec-as-slice ()
+    "Convert the vector expression at point to a slice.
 foo -> &foo[..]"
-  (interactive)
-  (insert "&")
-  (forward-symbol 1)
-  (insert "[..]"))
+    (interactive)
+    (insert "&")
+    (forward-symbol 1)
+    (insert "[..]"))
 
-(define-key rust-mode-map (kbd "C-c s") #'wh/rust-vec-as-slice)
+  (define-key rust-mode-map (kbd "C-c s") #'wh/rust-vec-as-slice)
 
-(require 'projectile)
-(require 'which-func)
+  (require 'projectile)
 
-(defun wh/rust-run-test ()
-  "Run the test at point."
-  (interactive)
-  (let ((defualt-directory (projectile-project-root)))
-    (compilation-start
-     (format "cargo test %s" (which-function)))))
+  (defun wh/rust-run-test ()
+    "Run the test at point."
+    (interactive)
+    (let ((default-directory (projectile-project-root)))
+      (compilation-start
+       (format "cargo test %s" (which-function))))
 
-(define-key rust-mode-map (kbd "C-c t") #'wh/rust-run-test)
+    (define-key rust-mode-map (kbd "C-c t") #'wh/rust-run-test)
 
-;; Use 'cargo check' as a build command, as it's much faster.
-(projectile-register-project-type 'rust-cargo '("Cargo.toml") "cargo check" "cargo test")
+    ;; Use 'cargo check' as a build command, as it's much faster.
+    (projectile-register-project-type 'rust-cargo '("Cargo.toml") "cargo check" "cargo test")
 
-(require 'multi-compile)
-(setq multi-compile-alist
-      '((rust-mode . (("rust-build" "cargo build" (projectile-project-root))
-                      ("rust-check" "cargo check" (projectile-project-root))
-                      ("rust-test" "cargo test" (projectile-project-root))))))
+    (require 'multi-compile)
+    (setq multi-compile-alist
+          '((rust-mode . (("rust-build" "cargo build" (projectile-project-root))
+                          ("rust-check" "cargo check" (projectile-project-root))
+                          ("rust-test" "cargo test" (projectile-project-root))))))
 
-(define-key rust-mode-map (kbd "C-c C-c") #'multi-compile-run)
+    (define-key rust-mode-map (kbd "C-c C-c") #'multi-compile-run)))
 
 (provide 'rust-customisations)
