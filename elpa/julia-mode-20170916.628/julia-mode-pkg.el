aa1f904f78f6e6531078268a3ae0ce4de7d26cfb
Wilfred Hughes <me@wilfred.me.uk>
2017-10-17 23:06:23 +0100

    Install julia-mode

---
+++ elpa/julia-mode-20170916.628/julia-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "julia-mode" "20170916.628" "Major mode for editing Julia source code" 'nil :commit "115d4dc8a07445301772da8376b232fa8c7168f4" :url "https://github.com/JuliaLang/julia" :keywords '("languages"))
