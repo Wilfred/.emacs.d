7c12ca8285e568670068000852334670915fe02e
Wilfred Hughes <me@wilfred.me.uk>
2017-10-17 23:06:31 +0100

    Install counsel-projectile

---
+++ elpa/counsel-projectile-20171001.641/counsel-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel-projectile" "20171001.641" "Ivy integration for Projectile" '((counsel "0.8.0") (projectile "0.14.0")) :commit "88a16cb75301ae95f21a71e8838a49590d011e73" :url "https://github.com/ericdanan/counsel-projectile" :keywords '("project" "convenience"))
