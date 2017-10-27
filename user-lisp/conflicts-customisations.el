a4c323aacfe096175f4381d91b33aba0e8a7dbaf
Wilfred Hughes <whughes@ahl.com>
2017-10-27 10:31:48 +0100

    Also bind M-RET for smerge-keep-current

--- user-lisp/conflicts-customisations.el
+++ user-lisp/conflicts-customisations.el
@@ -13,6 +13,7 @@
   :config
   ;; TODO: it would be nice if this jumped to the next conflict
   (define-key smerge-mode-map (kbd "<C-return>") #'smerge-keep-current)
+  (define-key smerge-mode-map (kbd "<M-return>") #'smerge-keep-current)
   (define-key smerge-mode-map (kbd "C-c <C-return>") #'smerge-keep-all)
   (define-key smerge-mode-map (kbd "<f8>") #'smerge-prev)
   (define-key smerge-mode-map (kbd "<f9>") #'smerge-next))
