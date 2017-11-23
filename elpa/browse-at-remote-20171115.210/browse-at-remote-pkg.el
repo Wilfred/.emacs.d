0d5bdd45d71ce8501fe9cb300e40231d7f61b8ec
Wilfred Hughes <me@wilfred.me.uk>
2017-11-23 12:03:41 +0000

    Install browse-at-remote

---
+++ elpa/browse-at-remote-20171115.210/browse-at-remote-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "browse-at-remote" "20171115.210" "Open github/gitlab/bitbucket/stash page from Emacs" '((f "0.17.2") (s "1.9.0") (cl-lib "0.5")) :commit "31dcf77d7c89a12f230e2b2332585db2c44530ef" :keywords '("github" "gitlab" "bitbucket" "convenience"))
