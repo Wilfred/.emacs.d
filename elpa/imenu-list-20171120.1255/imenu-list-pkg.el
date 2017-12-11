aa63a33736ea8ac65f08eab10e73ff8dc1955237
Wilfred Hughes <me@wilfred.me.uk>
2017-12-11 23:29:58 +0000

    Install imenu-ilist

---
+++ elpa/imenu-list-20171120.1255/imenu-list-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "imenu-list" "20171120.1255" "Show imenu entries in a separate buffer" '((cl-lib "0.5")) :commit "0d3d445ce60a77a6c7bcbdbf1ae62cc20d2b5575" :url "https://github.com/bmag/imenu-list")
