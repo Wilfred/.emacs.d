58675bb9078d30e129cbccf4ac8cb614de935423
Wilfred Hughes <me@wilfred.me.uk>
2018-01-27 01:00:13 +0000

    Update pythonic
    Fixes crash (which only occurs on my linux boxes) due to trying to
    call nonexistent function pythonic-remote-docker-p.

--- elpa/pythonic-20171219.810/pythonic-autoloads.el
+++ elpa/pythonic-20171219.810/pythonic-autoloads.el
@@ -1,10 +1,10 @@
 ;;; pythonic-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "pythonic" "pythonic.el" (22243 58624 583112
-;;;;;;  189000))
+;;;### (autoloads nil "pythonic" "pythonic.el" (23147 52959 657090
+;;;;;;  422000))
 ;;; Generated autoloads from pythonic.el
 
 (autoload 'pythonic-activate "pythonic" "\
