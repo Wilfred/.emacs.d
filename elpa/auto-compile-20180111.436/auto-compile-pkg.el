8ed0ccc5b9f3b521f35a5d95ee83edc8a71acf08
Wilfred Hughes <me@wilfred.me.uk>
2018-03-02 22:47:41 +0000

    Install auto-compile

---
+++ elpa/auto-compile-20180111.436/auto-compile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "auto-compile" "20180111.436" "automatically compile Emacs Lisp libraries" '((emacs "24.3") (packed "2.0.0")) :commit "8d117868a0a091389d528428136e60f951e9c550" :url "https://github.com/emacscollective/auto-compile" :keywords '("compile" "convenience" "lisp"))
