0cd4233e180b3e6fb724f7d76e960d1879f2f2f1
Wilfred Hughes <me@wilfred.me.uk>
2018-03-06 23:20:32 +0000

    Install elisp-def

---
+++ elpa/elisp-def-20180224.1517/elisp-def-autoloads.el
@@ -0,0 +1,29 @@
+;;; elisp-def-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "elisp-def" "elisp-def.el" (23185 33337 847555
+;;;;;;  512000))
+;;; Generated autoloads from elisp-def.el
+
+(autoload 'elisp-def "elisp-def" "\
+Go to the definition of the symbol at point.
+
+\(fn)" t nil)
+
+(autoload 'elisp-def-mode "elisp-def" "\
+Minor mode for finding definitions with `elisp-def'.
+
+\\{elisp-def-mode-map}
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; elisp-def-autoloads.el ends here
---
+++ elpa/elisp-def-20180224.1517/elisp-def-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "elisp-def" "20180224.1517" "macro-aware go-to-definition for elisp" '((dash "2.12.0") (f "0.19.0") (s "1.11.0") (emacs "24.3")) :authors '(("Wilfred Hughes" . "me@wilfred.me.uk")) :maintainer '("Wilfred Hughes" . "me@wilfred.me.uk"))
