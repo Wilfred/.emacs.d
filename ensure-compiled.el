b42433917db1ea284beaf40d1a248556fa99d8c1
Wilfred Hughes <Wilfred.Hughes@man.com>
2018-03-21 11:30:15 +0000

    Allow ensure-compiled to be run interactively

--- ensure-compiled.el
+++ ensure-compiled.el
@@ -15,12 +15,11 @@
   "Does the directory at PATH contain any .elc files?"
   (--any-p (f-ext? it "elc") (f-files path)))
 
-(defun ensure-packages-compiled ()
+(defun wh/ensure-packages-compiled ()
   "If any packages installed with package.el aren't compiled yet, compile them."
+  (interactive)
   (--each (f-directories package-user-dir)
     (unless (was-compiled-p it)
       (byte-recompile-directory it 0))))
-
-(ensure-packages-compiled)
 
 ;; todo: clean up orphaned .elc files
