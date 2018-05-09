0fc7fe6aa49b2b7126b14f328ce226ea9f28d89e
Wilfred Hughes <me@wilfred.me.uk>
2018-05-09 21:00:33 +0100

    Install a major-mode for .env files

---
+++ elpa/dotenv-mode-20180207.1114/dotenv-mode-autoloads.el
@@ -0,0 +1,24 @@
+;;; dotenv-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "dotenv-mode" "dotenv-mode.el" (23283 21314
+;;;;;;  145265 812000))
+;;; Generated autoloads from dotenv-mode.el
+
+(autoload 'dotenv-mode "dotenv-mode" "\
+Major mode for `.env' files.
+
+\(fn)" t nil)
+
+(mapc (lambda (s) (add-to-list 'auto-mode-alist `(,s . dotenv-mode))) '("\\.env\\'" "\\.env\\.example\\'"))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dotenv-mode-autoloads.el ends here
