6f4783e71ab29f3b3d1a0e94777dfa4475d0f872
Wilfred Hughes <me@wilfred.me.uk>
2018-06-07 10:34:47 +0100

    Version bump magit

---
+++ elpa/git-commit-20180602.1756/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20180602.1756" "Edit Git commit messages" '((emacs "24.4") (dash "20180413") (with-editor "20180414")) :commit "59429b50ccbfb68f4463bf68e025bf5176999684" :keywords '("git" "tools" "vc") :url "https://github.com/magit/magit")
