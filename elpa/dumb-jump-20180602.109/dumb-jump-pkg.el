c550a8fcc091d190841db0c1e1ac084c51054237
Wilfred Hughes <me@wilfred.me.uk>
2018-06-08 15:11:41 +0100

    Install dumb-jump

---
+++ elpa/dumb-jump-20180602.109/dumb-jump-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dumb-jump" "20180602.109" "jump to definition for multiple languages without configuration." '((emacs "24.3") (f "0.20.0") (s "1.11.0") (dash "2.9.0") (popup "0.5.3")) :commit "c59d7cc0c238f055cdfd76b0b6cb10d82e365294" :keywords '("programming"))
