02393a87fababbdfbbd4ba21f99794f7c5e9d049
Wilfred Hughes <me@wilfred.me.uk>
2018-06-24 12:38:10 +0100

    Install tuareg (and dependency) for editing ocaml files

---
+++ elpa/caml-20171209.1232/caml-pkg.el
@@ -0,0 +1,6 @@
+(define-package "caml" "20171209.1232" "OCaml code editing commands for Emacs" 'nil :keywords
+  '("ocaml")
+  :url "https://github.com/ocaml/ocaml/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
