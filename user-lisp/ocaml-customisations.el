9ac97beed499f7a9e4fee9c1dd2cb90b2cf75e47
Wilfred Hughes <me@wilfred.me.uk>
2018-06-27 14:21:22 +0100

    Basic ocaml configuration

---
+++ user-lisp/ocaml-customisations.el
@@ -0,0 +1,5 @@
+(use-package tuareg
+  :config
+  ;; Don't move past the expression after evaluating. I usually want
+  ;; to repeatedly eval the same expression.
+  (setq tuareg-skip-after-eval-phrase nil))
