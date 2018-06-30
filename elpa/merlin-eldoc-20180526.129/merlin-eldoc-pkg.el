af08afe177528106dda03ef36a876d44c8464ad6
Wilfred Hughes <me@wilfred.me.uk>
2018-06-30 10:52:11 +0100

    Install merlin and merlin-eldoc
    merlin-eldoc has been patched with:
    https://github.com/Khady/merlin-eldoc/pull/26

---
+++ elpa/merlin-eldoc-20180526.129/merlin-eldoc-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "merlin-eldoc" "20180526.129" "eldoc for OCaml and Reason" '((emacs "24.4") (merlin "3.0")) :commit "096bc5524674bbde5bf0edac1599f5a43b6eddf0" :keywords '("merlin" "ocaml" "languages" "eldoc") :url "https://github.com/khady/merlin-eldoc")
