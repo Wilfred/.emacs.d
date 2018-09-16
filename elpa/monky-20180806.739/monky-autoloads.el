e1eefb82f8353d3a39d9c5bca0edc42708889d5a
Wilfred Hughes <me@wilfred.me.uk>
2018-09-16 18:54:41 +0100

    Install monky

---
+++ elpa/monky-20180806.739/monky-autoloads.el
@@ -0,0 +1,31 @@
+;;; monky-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "monky" "monky.el" (0 0 0 0))
+;;; Generated autoloads from monky.el
+
+(autoload 'monky-status "monky" "\
+Show the status of Hg repository.
+
+\(fn &optional DIRECTORY)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "monky" '("monky-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("monky-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; monky-autoloads.el ends here
---
+++ elpa/monky-20180806.739/monky-pkg.el
@@ -0,0 +1,4 @@
+(define-package "monky" "20180806.739" "Control Hg from Emacs." 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
