45e0fe18b98de96d7806a85c97eab908e853e36d
Wilfred Hughes <me@wilfred.me.uk>
2018-12-19 23:27:59 +0000

    Configure flycheck with rust

--- user-lisp/rust-customisations.el
+++ user-lisp/rust-customisations.el
@@ -6,6 +6,10 @@
 (use-package rust-mode
   :config
   (add-hook 'rust-mode-hook #'flycheck-mode)
+
+  (with-eval-after-load 'rust-mode
+    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
   ;; Work around https://github.com/flycheck/flycheck-rust/issues/47
   (setq flycheck-rust-check-tests nil)
 
