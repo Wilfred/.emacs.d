f21ebe0050b58155ba04b643ecede4b354816cfe
Wilfred Hughes <me@wilfred.me.uk>
2019-03-03 21:42:39 +0000

    Adding an additional quote

--- user-lisp/startup-customisations.el
+++ user-lisp/startup-customisations.el
@@ -1,26 +1,27 @@
 ; no startup screen
 (setq-default inhibit-startup-screen t)
 
-(defvar programming-quotes
-      '("First, solve the problem. Then, write the code. -- John Johnson"
-        "The computing scientist’s main challenge is not to get confused by the complexities of his own making. -- E. W. Dijkstra"
-        "There are two ways of constructing a software design: One way is to make it so simple that there are obviously no deficiencies and the other way is to make it so complicated that there are no obvious deficiencies. -- C. A. R. Hoare"
-        "Controlling complexity is the essence of computer programming. -- Brian Kernigan"
-        "If you’re willing to restrict the flexibility of your approach, you can almost always do something better. -- John Carmack"
-        "Measuring programming progress by lines of code is like measuring aircraft building progress by weight. -- Bill Gates"
-        "The best code is no code at all."
-        "There is not now, nor has there ever been, nor will there ever be, any programming language in which it is the least bit difficult to write bad code."
-        "Code never lies, comments sometimes do. -- Ron Jeffries"
-        "Simplicity carried to the extreme becomes elegance. -- Jon Franklin"
-        "The unavoidable price of reliability is simplicity. -- C. A. R. Hoare"
-        "Good code is short, simple, and symmetrical – the challenge is figuring out how to get there. -- Sean Parent"
-        "True glory consists in doing what deserves to be written; in writing what deserves to be read. -- Pliny the Elder"
-        "The whole point of getting things done is knowing what to leave undone. -- Oswald Chambers"))
+(defvar wh/programming-quotes
+  '("First, solve the problem. Then, write the code. -- John Johnson"
+    "The computing scientist’s main challenge is not to get confused by the complexities of his own making. -- E. W. Dijkstra"
+    "There are two ways of constructing a software design: One way is to make it so simple that there are obviously no deficiencies and the other way is to make it so complicated that there are no obvious deficiencies. -- C. A. R. Hoare"
+    "Controlling complexity is the essence of computer programming. -- Brian Kernigan"
+    "If you’re willing to restrict the flexibility of your approach, you can almost always do something better. -- John Carmack"
+    "Measuring programming progress by lines of code is like measuring aircraft building progress by weight. -- Bill Gates"
+    "The best code is no code at all."
+    "There is not now, nor has there ever been, nor will there ever be, any programming language in which it is the least bit difficult to write bad code."
+    "Code never lies, comments sometimes do. -- Ron Jeffries"
+    "Simplicity carried to the extreme becomes elegance. -- Jon Franklin"
+    "The unavoidable price of reliability is simplicity. -- C. A. R. Hoare"
+    "Good code is short, simple, and symmetrical – the challenge is figuring out how to get there. -- Sean Parent"
+    "True glory consists in doing what deserves to be written; in writing what deserves to be read. -- Pliny the Elder"
+    "The whole point of getting things done is knowing what to leave undone. -- Oswald Chambers"
+    "No one can become a [...] revolutionary by trying to imitate revolutionariness. You can only get there by pursuing the correct answer in all things, whether the correct answer is revolutionary or not. -- E Yudkowsky"))
 
 (require 'random-utils)
 
 ;; populate the initial scratch buffer with a random quote.
-(setq initial-scratch-message (random-choice programming-quotes))
+(setq initial-scratch-message (random-choice wh/programming-quotes))
 (setq initial-major-mode #'fundamental-mode)
 
 ;; Use hard line wrapping on the quote in the scratch buffer.
