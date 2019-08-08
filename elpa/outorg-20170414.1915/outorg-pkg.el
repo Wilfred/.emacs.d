2b97827a921bf59c44dda7d6e1de04d4e722aae3
Wilfred Hughes <me@wilfred.me.uk>
2019-08-08 23:30:38 +0100

    Install outshine

---
+++ elpa/outorg-20170414.1915/outorg-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "outorg" "20170414.1915" "Org-style comment editing" '((emacs "24.4")) :commit "78b0695121fb974bc4e971eb4ef7f8afd6d89d64" :maintainer '("Adam Porter") :url "https://github.com/alphapapa/outorg")
---
+++ elpa/outshine-20180625.1959/outshine-pkg.el
@@ -0,0 +1,9 @@
+(define-package "outshine" "20180625.1959" "outline with outshine outshines outline"
+  '((outorg "2.0")
+    (cl-lib "0.5"))
+  :maintainer
+  '("Adam Porter" . "adam@alphapapa.net")
+  :url "http://github.com/alphapapa/outshine")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
