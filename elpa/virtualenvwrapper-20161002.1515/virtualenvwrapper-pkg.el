31f0b23541e675aae35f28e9ba84db4bce8177a8
Wilfred Hughes <me@wilfred.me.uk>
2020-08-15 17:10:57 -0700

    Remove the unused virtualenvwrapper package

--- elpa/virtualenvwrapper-20161002.1515/virtualenvwrapper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "virtualenvwrapper" "20161002.1515" "a featureful virtualenv tool for Emacs" '((dash "1.5.0") (s "1.6.1")) :url "http://github.com/porterjamesj/virtualenvwrapper.el" :keywords '("python" "virtualenv" "virtualenvwrapper"))
