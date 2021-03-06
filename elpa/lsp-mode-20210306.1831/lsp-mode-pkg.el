7de0793557de70ae19792c5cedc2cc0aebdb4004
Wilfred Hughes <me@wilfred.me.uk>
2021-03-06 13:06:45 -0800

    version bump lsp-mode

---
+++ elpa/lsp-mode-20210306.1831/lsp-mode-pkg.el
@@ -0,0 +1,18 @@
+(define-package "lsp-mode" "20210306.1831" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.18.0")
+    (f "0.20.0")
+    (ht "2.3")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "b57a9bb53e27ae185c785c70592ff8b2ec80f7a4" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
