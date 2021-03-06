75effd9909c9de8e2cd61b71411c524fbaf6114c
Wilfred Hughes <me@wilfred.me.uk>
2021-03-06 12:17:52 -0800

    Version bump utility libraries

---
+++ elpa/dash-20210228.2221/dash-pkg.el
@@ -0,0 +1,12 @@
+(define-package "dash" "20210228.2221" "A modern list library for Emacs"
+  '((emacs "24"))
+  :commit "334a0b31cab6b42e4465ffdfc6f07c00cd227562" :authors
+  '(("Magnar Sveen" . "magnars@gmail.com"))
+  :maintainer
+  '("Magnar Sveen" . "magnars@gmail.com")
+  :keywords
+  '("extensions" "lisp")
+  :url "https://github.com/magnars/dash.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- elpa/ht-20161015.1945/ht-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; ht-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("ht.el") (22697 45737 411576 939000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ht-autoloads.el ends here
