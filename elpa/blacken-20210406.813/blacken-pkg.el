ff7cb2fd9744e3fe24c9142077d70f3b02c28cf4
Wilfred Hughes <me@wilfred.me.uk>
2021-04-18 12:21:18 -0700

    Install blacken

---
+++ elpa/blacken-20210406.813/blacken-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from blacken.el  -*- no-byte-compile: t -*-
+(define-package "blacken" "20210406.813" "Reformat python buffers using the \"black\" formatter" '((emacs "25.2")) :commit "880cf502198753643a3e2ccd4131ee6973be2e8a" :authors '(("Artem Malyshev" . "proofit404@gmail.com")) :maintainer '("Artem Malyshev" . "proofit404@gmail.com") :url "https://github.com/proofit404/blacken")
