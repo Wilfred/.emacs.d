6b50489bfbdb4fb53511af51a07cbb55efe37460
Wilfred Hughes <me@wilfred.me.uk>
2021-04-24 16:40:34 -0700

    Initial LSP configuration

---
+++ user-lisp/lsp-customisations.el
@@ -0,0 +1,6 @@
+
+(setq lsp-modeline-code-actions-segments '(count name))
+
+(setq lsp-rust-clippy-preference "on")
+
+(set-face-attribute 'lsp-lsp-flycheck-warning-unnecessary-face nil :foreground nil)
