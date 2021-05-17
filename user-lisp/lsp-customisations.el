9bf1ccaac617fecd509b28e126c84b31f0eba8ab
Wilfred Hughes <me@wilfred.me.uk>
2021-05-17 00:13:56 -0700

    Fix M-n and M-p when LSP is active

--- user-lisp/lsp-customisations.el
+++ user-lisp/lsp-customisations.el
@@ -4,3 +4,7 @@
 (setq lsp-rust-clippy-preference "on")
 
 (set-face-attribute 'lsp-lsp-flycheck-warning-unnecessary-face nil :foreground nil)
+
+;; Don't override my navigation bindings.
+(define-key lsp-signature-mode-map (kbd "M-n") nil)
+(define-key lsp-signature-mode-map (kbd "M-p") nil)
