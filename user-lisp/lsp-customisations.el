c1e463f6c91258603afc5d3c6b74054e33790b34
Wilfred Hughes <me@wilfred.me.uk>
2021-06-13 09:18:32 -0700

    Wrap lsp config in use-package

--- user-lisp/lsp-customisations.el
+++ user-lisp/lsp-customisations.el
@@ -1,14 +1,19 @@
-;; For some reason icons aren't currently rendering, so don't show them.
-(setq lsp-modeline-code-actions-segments '(count name))
 
-(setq lsp-rust-clippy-preference "on")
+(use-package lsp-mode
+  :config
+  ;; For some reason icons aren't currently rendering, so don't show them.
+  (setq lsp-modeline-code-actions-segments '(count name))
 
-(set-face-attribute 'lsp-lsp-flycheck-warning-unnecessary-face nil :foreground nil)
+  (setq lsp-rust-clippy-preference "on")
 
-;; Don't override my navigation bindings.
-(define-key lsp-signature-mode-map (kbd "M-n") nil)
-(define-key lsp-signature-mode-map (kbd "M-p") nil)
+  (set-face-attribute 'lsp-lsp-flycheck-warning-unnecessary-face nil :foreground nil)
 
-(define-key lsp-mode-map (kbd "C-c f") #'lsp-format-buffer)
+  ;; Don't override my navigation bindings.
+  (define-key lsp-signature-mode-map (kbd "M-n") nil)
+  (define-key lsp-signature-mode-map (kbd "M-p") nil)
+
+  (define-key lsp-mode-map (kbd "C-c f") #'lsp-format-buffer)
+
+  (add-hook 'rust-mode-hook #'lsp-mode))
 
 (provide 'lsp-customisations)
