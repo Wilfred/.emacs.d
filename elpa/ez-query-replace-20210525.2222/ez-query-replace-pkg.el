72e92ff01eb7c33c537b2a2a89b1b33b08ea764c
Wilfred Hughes <me@wilfred.me.uk>
2021-07-31 16:23:37 -0700

    Version bump ez-query-replace

--- elpa/ez-query-replace-20210525.2222/ez-query-replace-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from ez-query-replace.el  -*- no-byte-compile: t -*-
-(define-package "ez-query-replace" "20210525.2222" "a smarter context-sensitive query-replace that can be reapplied" '((dash "1.2.0") (s "1.11.0")) :commit "3202cf4644ed3b6549284c3816b90bb230970a5b" :authors '(("Wilfred Hughes" . "me@wilfred.me.uk")) :maintainer '("Wilfred Hughes" . "me@wilfred.me.uk"))
---
+++ elpa/ez-query-replace-20210724.2247/ez-query-replace-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from ez-query-replace.el  -*- no-byte-compile: t -*-
+(define-package "ez-query-replace" "20210724.2247" "a smarter context-sensitive query-replace that can be reapplied" '((dash "1.2.0") (s "1.11.0")) :commit "2b68472f4007a73908c3b242e83ac5a7587967ff" :authors '(("Wilfred Hughes" . "me@wilfred.me.uk")) :maintainer '("Wilfred Hughes" . "me@wilfred.me.uk"))
