dad670cd9651db234b773df56d76b8715bd659fa
Wilfred Hughes <me@wilfred.me.uk>
2021-08-04 00:15:19 -0700

    Version bump zoutline (used by lispy)

--- elpa/zoutline-20160915.503/zoutline-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; zoutline-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("zoutline.el") (22697 45713 925205 514000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; zoutline-autoloads.el ends here
