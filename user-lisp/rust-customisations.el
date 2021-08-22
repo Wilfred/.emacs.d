ac1cf904a8c9bc09915247434238e2e219788f3b
Wilfred Hughes <me@wilfred.me.uk>
2021-08-22 11:25:43 -0700

    Don't require point exactly on dbg! when unwrapping

--- user-lisp/rust-customisations.el
+++ user-lisp/rust-customisations.el
@@ -46,10 +46,14 @@ foo -> &foo[..]"
   (define-key rust-mode-map (kbd "C-c d") #'wh/rust-wrap-dbg))
 
 (defun wh/rust-unwrap-dbg ()
-  "Remove dbg!(foo) to just foo at point."
+  "Remove dbg!(foo) to just foo on the line at point."
   (interactive)
-  (sp-splice-sexp)
-  (backward-delete-char 4))
+
+  (save-excursion
+    (beginning-of-line)
+    (search-forward "dbg!(")
+    (sp-splice-sexp)
+    (backward-delete-char 4)))
 
 (require 'lsp-customisations)
 
