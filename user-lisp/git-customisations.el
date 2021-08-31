fe74c8efd676adb82386475ee34ec8b5784df571
Wilfred Hughes <me@wilfred.me.uk>
2021-08-31 11:47:06 -0700

    Add keybindings for git-gutter next/prev

--- user-lisp/git-customisations.el
+++ user-lisp/git-customisations.el
@@ -82,4 +82,9 @@
   :config
   (define-key with-editor-mode-map (kbd "<C-f12>") #'wh/commit-and-push))
 
+(use-package git-gutter
+  :config
+  (global-set-key (kbd "C-c n") #'git-gutter:next-hunk)
+  (global-set-key (kbd "C-c p") #'git-gutter:previous-hunk))
+
 (provide 'git-customisations)
