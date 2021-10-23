c52863302ebe37a8bb9f41d449de0018cc4014b6
Wilfred Hughes <me@wilfred.me.uk>
2021-10-23 11:58:36 -0700

    Version bump go-mode

--- elpa/go-mode-20210509.2353/go-mode-autoloads.el
+++ elpa/go-mode-20210509.2353/go-mode-autoloads.el
@@ -1,10 +1,12 @@
 ;;; go-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "go-mode" "go-mode.el" (22697 45750 251415
-;;;;;;  488000))
+;;;### (autoloads nil "go-mode" "go-mode.el" (0 0 0 0))
 ;;; Generated autoloads from go-mode.el
 
 (autoload 'go-mode "go-mode" "\
@@ -79,9 +81,7 @@ Add this to .emacs to run gofmt on the current buffer when saving:
 \(add-hook 'before-save-hook 'gofmt-before-save).
 
 Note that this will cause ‘go-mode’ to get loaded the first time
-you save any file, kind of defeating the point of autoloading.
-
-\(fn)" t nil)
+you save any file, kind of defeating the point of autoloading." t nil)
 
 (autoload 'godoc "go-mode" "\
 Show Go documentation for QUERY, much like \\<go-mode-map>\\[man].
@@ -94,11 +94,21 @@ Tries to look for a URL at point.
 
 \(fn URL)" t nil)
 
+(autoload 'go-dot-mod-mode "go-mode" "\
+A major mode for editing go.mod files.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("go\\.mod\\'" . go-dot-mod-mode))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "go-mode" '("go-" "god" "gofmt")))
+
 ;;;***
 
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; go-mode-autoloads.el ends here
