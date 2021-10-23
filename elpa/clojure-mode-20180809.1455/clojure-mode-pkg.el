4e4228848e06914d8026bdff41d0c4f93d127b94
Wilfred Hughes <me@wilfred.me.uk>
2021-10-23 11:58:22 -0700

    Version bump clojure-mode

--- elpa/clojure-mode-20180809.1455/clojure-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "clojure-mode" "20180809.1455" "Major mode for Clojure code" '((emacs "25.1")) :commit "c5239f3c8b993122a83bfb618cc3cc6cb6eb3001" :keywords '("languages" "clojure" "clojurescript" "lisp") :url "http://github.com/clojure-emacs/clojure-mode")
