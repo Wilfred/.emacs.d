04d4bb4c8f4f63934f030cdca19539a3731b1155
Wilfred Hughes <me@wilfred.me.uk>
2021-11-30 23:17:55 -0800

    Update to latest tide-mode

---
+++ elpa/tide-20210930.356/tide-pkg.el
@@ -0,0 +1,17 @@
+(define-package "tide" "20210930.356" "Typescript Interactive Development Environment"
+  '((emacs "25.1")
+    (dash "2.10.0")
+    (s "1.11.0")
+    (flycheck "27")
+    (typescript-mode "0.1")
+    (cl-lib "0.5"))
+  :commit "28137ed904deb143dba8f8f67660966e11921c6d" :authors
+  '(("Anantha kumaran" . "ananthakumaran@gmail.com"))
+  :maintainer
+  '("Anantha kumaran" . "ananthakumaran@gmail.com")
+  :keywords
+  '("typescript")
+  :url "http://github.com/ananthakumaran/tide")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
