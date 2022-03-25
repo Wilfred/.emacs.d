faf4a8deda326a397015c6db03a73b34ba86fa33
Wilfred Hughes <me@wilfred.me.uk>
2022-03-25 16:46:21 -0700

    Update merlin package

--- elpa/merlin-20220318.2012/merlin-pkg.el
+++ elpa/merlin-20220318.2012/merlin-pkg.el
@@ -1,4 +1,6 @@
-(define-package "merlin" "20210129.1443" "Mode for Merlin, an assistant for OCaml." 'nil :commit "ff88721dd63b1423bd281efe9d4e29f4d66d30c9" :authors
+(define-package "merlin" "20220318.2012" "Mode for Merlin, an assistant for OCaml"
+  '((emacs "25.1"))
+  :commit "fb5cfc2a5091ffa77894629538efb0ec953fb6e7" :authors
   '(("Frédéric Bour <frederic.bour(_)lakaban.net>"))
   :maintainer
   '("Frédéric Bour <frederic.bour(_)lakaban.net>")
