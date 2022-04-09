c9f781ec0760754deb379f46c6f08cdc4fb74ffe
Wilfred Hughes <me@wilfred.me.uk>
2022-04-09 11:39:29 -0700

    Update rainbow-delimiters

--- elpa/rainbow-delimiters-20210515.1254/rainbow-delimiters.el
+++ elpa/rainbow-delimiters-20210515.1254/rainbow-delimiters.el
@@ -2,13 +2,14 @@
 
 ;; Copyright (C)
 ;;   2010-2013 Jeremy Rayman
-;;   2013-2016 Fanael Linithien
+;;   2013-2021 Fanael Linithien
 ;; Author: Jeremy Rayman <opensource@jeremyrayman.com>
 ;;         Fanael Linithien <fanael4@gmail.com>
 ;; Maintainer: Fanael Linithien <fanael4@gmail.com>
 ;; Created: 2010-09-02
-;; Version: 2.1.3
-;; Package-Version: 20160430.2358
+;; Version: 2.1.5
+;; Package-Version: 20210515.1254
+;; Package-Commit: a32b39bdfe6c61c322c37226d66e1b6d4f107ed0
 ;; Keywords: faces, convenience, lisp, tools
 ;; Homepage: https://github.com/Fanael/rainbow-delimiters
 
@@ -105,14 +106,24 @@ The function should not move the point or mark or change the match data."
   :type 'function
   :group 'rainbow-delimiters)
 
+(defface rainbow-delimiters-base-face
+  '((default (:inherit unspecified)))
+  "Face inherited by all other rainbow-delimiter faces."
+  :group 'rainbow-delimiters-faces)
+
+(defface rainbow-delimiters-base-error-face
+  '((default (:inherit rainbow-delimiters-base-face))
+    (t (:foreground "#88090B")))
+  "Face inherited by all other rainbow-delimiter error faces."
+  :group 'rainbow-delimiters-faces)
+
 (defface rainbow-delimiters-unmatched-face
-  '((((background light)) (:foreground "#88090B"))
-    (((background dark)) (:foreground "#88090B")))
+  '((default (:inherit rainbow-delimiters-base-error-face)))
   "Face to highlight unmatched closing delimiters in."
   :group 'rainbow-delimiters-faces)
 
 (defface rainbow-delimiters-mismatched-face
-  '((t :inherit rainbow-delimiters-unmatched-face))
+  '((default (:inherit rainbow-delimiters-unmatched-face)))
   "Face to highlight mismatched closing delimiters in."
   :group 'rainbow-delimiters-faces)
 
@@ -125,7 +136,8 @@ The function should not move the point or mark or change the match data."
                         "#b0b0b3" "#90a890" "#a2b6da" "#9cb6ad"]))
       (dotimes (i 9)
         (push `(defface ,(intern (format "rainbow-delimiters-depth-%d-face" (1+ i)))
-                 '((((class color) (background light)) :foreground ,(aref light-colors i))
+                 '((default (:inherit rainbow-delimiters-base-face))
+                   (((class color) (background light)) :foreground ,(aref light-colors i))
                    (((class color) (background dark)) :foreground ,(aref dark-colors i)))
                  ,(format "Nested delimiter face, depth %d." (1+ i))
                  :group 'rainbow-delimiters-faces)
@@ -221,6 +233,10 @@ Returns t if char at loc meets one of the following conditions:
   "Highlight delimiters in region between point and END.
 
 Used by font-lock for dynamic highlighting."
+  (when (bound-and-true-p mmm-current-submode)
+    ;; `mmm-mode' is weird and apparently needs this hack, because otherwise we
+    ;; may end up thinking matched parentheses are mismatched.
+    (widen))
   (let* ((last-ppss-pos (point))
          (ppss (syntax-ppss)))
     (while (> end (progn (skip-syntax-forward "^()" end)
@@ -256,7 +272,9 @@ Used by font-lock for dynamic highlighting."
 ;;;###autoload
 (define-minor-mode rainbow-delimiters-mode
   "Highlight nested parentheses, brackets, and braces according to their depth."
-  nil "" nil ; No modeline lighter - it's already obvious when the mode is on.
+  :init-value nil
+  :lighter "" ; No modeline lighter - it's already obvious when the mode is on.
+  :keymap nil
   (font-lock-remove-keywords nil rainbow-delimiters--font-lock-keywords)
   (when rainbow-delimiters-mode
     (font-lock-add-keywords nil rainbow-delimiters--font-lock-keywords 'append)
