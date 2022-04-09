19725b7b52d3b314115652eb3b1a658b9cbb95e1
Wilfred Hughes <me@wilfred.me.uk>
2022-04-09 11:33:00 -0700

    Delete ocamlformat copy

--- elpa/ocamlformat-20210617.1726/ocamlformat-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ocamlformat" "20210617.1726" "Utility functions to format ocaml code" '((emacs "24.3")) :commit "62d702a1b2cdc05040234ed920fcf1bd6203e06b" :keywords '("languages" "ocaml") :url "https://github.com/ocaml-ppx/ocamlformat")
