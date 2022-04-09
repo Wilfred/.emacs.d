6e03c91f9fb3572dac136c6452f14b007e7d8ba6
Wilfred Hughes <me@wilfred.me.uk>
2022-04-09 11:37:39 -0700

    Prefer org-mode as shipped with Emacs

--- elpa/org-bullets-20140918.1137/org-bullets-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "org-bullets" "20140918.1137" "Show bullets in org-mode as UTF-8 characters" 'nil :url "https://github.com/sabof/org-bullets")
--- init.el
+++ init.el
@@ -48,8 +48,7 @@
 (require 'package)
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
-        ("melpa" . "https://melpa.org/packages/")
-        ("org" . "http://orgmode.org/elpa/")))
+        ("melpa" . "https://melpa.org/packages/")))
 
 (package-initialize)
 
