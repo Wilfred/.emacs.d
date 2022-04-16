bd5001783150751fa5e2c7bd42b20080c74f110a
Wilfred Hughes <me@wilfred.me.uk>
2022-04-16 12:41:31 -0700

    Version bump yascroll

--- elpa/yascroll-20220212.1742/yascroll-autoloads.el
+++ elpa/yascroll-20220212.1742/yascroll-autoloads.el
@@ -1,4 +1,4 @@
-;;; yascroll-autoloads.el --- automatically extracted autoloads
+;;; yascroll-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
 ;;
 ;;; Code:
 
@@ -10,19 +10,31 @@
 ;;; Generated autoloads from yascroll.el
 
 (autoload 'yascroll:show-scroll-bar "yascroll" "\
-Show scroll bar in BUFFER.
-
-\(fn)" t nil)
+Default key to show all scroll bars." t nil)
 
 (autoload 'yascroll:hide-scroll-bar "yascroll" "\
-Hide scroll bar of BUFFER.
-
-\(fn)" t nil)
+Hide scroll bar of BUFFER." t nil)
 
 (autoload 'yascroll-bar-mode "yascroll" "\
 Yet Another Scroll Bar Mode.
 
+This is a minor mode.  If called interactively, toggle the
+`Yascroll-Bar mode' mode.  If the prefix argument is positive,
+enable the mode, and if it is zero or negative, disable the mode.
+
+If called from Lisp, toggle the mode if ARG is `toggle'.  Enable
+the mode if ARG is nil, omitted, or is a positive number.
+Disable the mode if ARG is a negative number.
+
+To check whether the minor mode is enabled in the current buffer,
+evaluate `yascroll-bar-mode'.
+
+The mode's hook is called both when the mode is enabled and when
+it is disabled.
+
 \(fn &optional ARG)" t nil)
+
+(put 'global-yascroll-bar-mode 'globalized-minor-mode t)
 
 (defvar global-yascroll-bar-mode nil "\
 Non-nil if Global Yascroll-Bar mode is enabled.
@@ -37,16 +49,20 @@ or call the function `global-yascroll-bar-mode'.")
 (autoload 'global-yascroll-bar-mode "yascroll" "\
 Toggle Yascroll-Bar mode in all buffers.
 With prefix ARG, enable Global Yascroll-Bar mode if ARG is positive;
-otherwise, disable it.  If called from Lisp, enable the mode if
-ARG is omitted or nil.
+otherwise, disable it.
 
-Yascroll-Bar mode is enabled in all buffers where
-`yascroll:turn-on' would do it.
+If called from Lisp, toggle the mode if ARG is `toggle'.
+Enable the mode if ARG is nil, omitted, or is a positive number.
+Disable the mode if ARG is a negative number.
+
+Yascroll-Bar mode is enabled in all buffers where `yascroll:turn-on'
+would do it.
+
 See `yascroll-bar-mode' for more information on Yascroll-Bar mode.
 
 \(fn &optional ARG)" t nil)
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "yascroll" '("yascroll:")))
+(register-definition-prefixes "yascroll" '("yascroll:"))
 
 ;;;***
 
---
+++ elpa/yascroll-20220212.1742/yascroll-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from yascroll.el  -*- no-byte-compile: t -*-
+(define-package "yascroll" "20220212.1742" "Yet Another Scroll Bar Mode" '((emacs "26.1")) :commit "a5ae81e44c8707949fad521ba532bc92568f79c2" :authors '(("Tomohiro Matsuyama" . "m2ym.pub@gmail.com")) :maintainer '("Shen, Jen-Chieh" . "jcs090218@gmail.com") :keywords '("convenience") :url "https://github.com/emacsorphanage/yascroll")
