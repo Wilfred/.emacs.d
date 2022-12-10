ace19e46b22428da42bf4171c7c2347cc4b0882d
Wilfred Hughes <me@wilfred.me.uk>
2022-12-10 11:13:44 -0800

    Use prettier with HTML

--- user-lisp/html-customisations.el
+++ user-lisp/html-customisations.el
@@ -3,4 +3,6 @@
   ;; Don't bind C-c C-n, I use it for renaming files.
   (define-key html-mode-map (kbd "C-c C-n") nil))
 
+(add-hook 'mhtml-mode-hook #'prettier-js-mode)
+
 (provide 'html-customisations)
