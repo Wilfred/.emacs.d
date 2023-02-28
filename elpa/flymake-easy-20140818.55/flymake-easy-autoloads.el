a2caab2a103342c0f5b922414d7506c21545b054
Wilfred Hughes <me@wilfred.me.uk>
2023-02-27 20:22:56 -0800

    Remove old, unused flymake packages

--- elpa/flymake-easy-20140818.55/flymake-easy-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; flymake-easy-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("flymake-easy.el") (21613 8186 703296
-;;;;;;  604000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flymake-easy-autoloads.el ends here
--- elpa/flymake-jshint-20140319.1500/flymake-jshint-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; flymake-jshint-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (flymake-jshint-load) "flymake-jshint" "flymake-jshint.el"
-;;;;;;  (21423 59503 589068 929000))
-;;; Generated autoloads from flymake-jshint.el
-
-(autoload 'flymake-jshint-load "flymake-jshint" "\
-Configure flymake mode to check the current buffer's JavaScript syntax.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("flymake-jshint-pkg.el") (21423 59503
-;;;;;;  647842 360000))
-
-;;;***
-
-(provide 'flymake-jshint-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; flymake-jshint-autoloads.el ends here
