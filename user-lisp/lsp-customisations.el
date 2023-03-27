bc6f1a0c042371259714cfa65c68fdfa3d32d851
Wilfred Hughes <me@wilfred.me.uk>
2023-03-26 22:07:40 -0700

    Add basic eglot configuration

--- user-lisp/lsp-customisations.el
+++ user-lisp/lsp-customisations.el
@@ -47,4 +47,22 @@
 
   (add-hook 'rust-mode-hook #'lsp))
 
+;; eglot
+;; 
+;; (load "package") ; workaround on Emacs 28
+
+(use-package 'eglot
+  :config
+  (add-hook 'eglot--managed-mode-hook (lambda () (flymake-mode -1)))
+  (global-flycheck-eglot-mode)
+
+  (define-key eglot-mode-map (kbd "C-c f") #'eglot-format-buffer)
+
+  (define-key eglot-mode-map (kbd "C-c r") #'eglot-rename)
+  ;; Mnemonic: show references in Xref.
+  (define-key eglot-mode-map (kbd "C-c x") #'xref-find-references)
+
+  ;; Mnemonic: action.
+  (define-key eglot-mode-map (kbd "C-c a") #'eglot-code-actions))
+
 (provide 'lsp-customisations)
