16239574f34ddd09c1e485b6a5843db666a343f1
Wilfred Hughes <me@wilfred.me.uk>
2023-04-09 16:34:50 -0700

    Update cider

---
+++ elpa/parseclj-20220422.936/parseclj-pkg.el
@@ -0,0 +1,11 @@
+(define-package "parseclj" "20220422.936" "Clojure/EDN parser"
+  '((emacs "25"))
+  :commit "4d0e780e00f1828b00c43099e6eebc6582998f72" :authors
+  '(("Arne Brasseur" . "arne@arnebrasseur.net"))
+  :maintainer
+  '("Arne Brasseur" . "arne@arnebrasseur.net")
+  :keywords
+  '("lisp" "clojure" "edn" "parser"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
