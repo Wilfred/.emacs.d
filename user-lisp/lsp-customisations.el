c0447916445bdadddc124c3fca76234ac15fe794
Wilfred Hughes <me@wilfred.me.uk>
2023-04-19 20:54:34 -0700

    Only show signature in minibuffer with eglot, not docs

--- user-lisp/lsp-customisations.el
+++ user-lisp/lsp-customisations.el
@@ -17,4 +17,18 @@
   ;; Mnemonic: action.
   (define-key eglot-mode-map (kbd "C-c a") #'eglot-code-actions))
 
+;; Doc comments are verbose, so only show the signature in the minibuffer.
+;;
+;; https://www.reddit.com/r/emacs/comments/11faie2/how_can_i_make_eglot_shut_up_in_the_minibuffer/jg0io6p/
+(defun wh/eglot-managed-mode-initialize ()
+  (setq-local
+   eldoc-documentation-functions
+   (list
+    #'eglot-signature-eldoc-function
+    ;; #'eglot-hover-eldoc-function
+    ;; #'flymake-eldoc-function
+    )))
+
+(add-hook 'eglot-managed-mode-hook #'wh/eglot-managed-mode-initialize)
+
 (provide 'lsp-customisations)
