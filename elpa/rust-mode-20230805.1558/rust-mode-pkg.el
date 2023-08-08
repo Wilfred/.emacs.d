410fada981c6d1b70eeffec6542d9d7704e9be8a
Wilfred Hughes <me@wilfred.me.uk>
2023-08-08 08:45:09 -0700

    Update rust-mode

--- elpa/rust-mode-20230805.1558/rust-mode-pkg.el
+++ elpa/rust-mode-20230805.1558/rust-mode-pkg.el
@@ -1,6 +1,8 @@
-(define-package "rust-mode" "20230112.27" "A major-mode for editing Rust source code"
+(define-package "rust-mode" "20230805.1558" "A major-mode for editing Rust source code"
   '((emacs "25.1"))
-  :commit "c12348e40531fc7e7325368621a0e5f8031d99b2" :authors
+  :commit "fac7d284d24080d0886c47d20bbddbf60ec51511" :authors
+  '(("Mozilla"))
+  :maintainers
   '(("Mozilla"))
   :maintainer
   '("Mozilla")
--- elpa/rust-mode-20230805.1558/rust-utils.el
+++ elpa/rust-mode-20230805.1558/rust-utils.el
@@ -36,46 +36,77 @@ visit the new file."
 
 ;;; dbg! macro
 
-(defun rust-insert-dbg ()
-  "Insert the dbg! macro."
-  (cond ((region-active-p)
-         (when (< (mark) (point))
-           (exchange-point-and-mark))
-         (let ((old-point (point)))
-           (insert-parentheses)
-           (goto-char old-point)))
-        (t
-         (when (rust-in-str)
-           (up-list -1 t t))
-         (insert "(")
-         (forward-sexp)
-         (insert ")")
-         (backward-sexp)))
-  (insert "dbg!"))
+(defun rust-insert-dbg-sexp ()
+  "Insert the dbg! macro around a sexp if possible, insert at current position
+if not. Move cursor to the end of macro."
+  (when (rust-in-str)
+    (up-list -1 t t))
+  (setq safe-to-forward t)
+  (save-excursion
+    (condition-case nil
+        (forward-sexp)
+      (error (setq safe-to-forward nil)
+             nil)))
+  (cond
+   ((not safe-to-forward)
+    (rust-insert-dbg-alone))
+   (t
+    (insert "(")
+    (forward-sexp)
+    (insert ")")
+    (backward-sexp)
+    (insert "dbg!")
+    (forward-sexp))))
+
+(defun rust-insert-dbg-region ()
+  "Insert the dbg! macro around a region. Move cursor to the end of macro."
+  (when (< (mark) (point))
+    (exchange-point-and-mark))
+  (let ((old-point (point)))
+    (insert-parentheses)
+    (goto-char old-point))
+  (insert "dbg!")
+  (forward-sexp))
+
+(defun rust-insert-dbg-alone ()
+  "Insert the dbg! macro alone. Move cursor in between the brackets."
+  (insert "dbg!()")
+  (backward-char))
 
 ;;;###autoload
 (defun rust-dbg-wrap-or-unwrap ()
   "Either remove or add the dbg! macro."
   (interactive)
-  (save-excursion
-    (if (region-active-p)
-        (rust-insert-dbg)
+  
+  (cond
+   
+   ;; region
+   ((region-active-p)
+    (rust-insert-dbg-region))
 
-      (let ((beginning-of-symbol (ignore-errors (beginning-of-thing 'symbol))))
-        (when beginning-of-symbol
-          (goto-char beginning-of-symbol)))
+   ;; alone
+   ((or (looking-at-p " *$") (looking-at-p " *//.*"))
+    (rust-insert-dbg-alone))
 
-      (let ((dbg-point (save-excursion
-                         (or (and (looking-at-p "dbg!") (+ 4 (point)))
-                             (ignore-errors
-                               (while (not (rust-looking-back-str "dbg!"))
-                                 (backward-up-list))
-                               (point))))))
-        (cond (dbg-point
-               (goto-char dbg-point)
-               (delete-char -4)
-               (delete-pair))
-              (t (rust-insert-dbg)))))))
+   ;; symbol
+   (t
+    (let ((beginning-of-symbol (ignore-errors (beginning-of-thing 'symbol))))
+      (when beginning-of-symbol
+        (goto-char beginning-of-symbol)))
+
+    (let ((dbg-point (save-excursion
+                       (or (and (looking-at-p "dbg!") (+ 4 (point)))
+                           (ignore-errors
+                             (while (not (rust-looking-back-str "dbg!"))
+                               (backward-up-list))
+                             (point))))))
+      (cond (dbg-point
+             (goto-char dbg-point)
+             (delete-char -4)
+             (delete-pair))
+            (t (rust-insert-dbg-sexp)))))
+   )
+)
 
 (defun rust-toggle-mutability ()
   "Toggles the mutability of the variable defined on the current line"
