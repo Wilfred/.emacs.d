1f481d8f15c8b108a5b7c6d88dfdb374c65a26b9
Wilfred Hughes <me@wilfred.me.uk>
2023-08-08 08:21:00 -0700

    Remove unused lsp-mode dependencies

--- elpa/cquery-20190118.542/cquery-autoloads.el
+++
@@ -1,72 +0,0 @@
-;;; cquery-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "cquery" "cquery.el" (0 0 0 0))
-;;; Generated autoloads from cquery.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "cquery" '("cquery-")))
-
-;;;***
-
-;;;### (autoloads nil "cquery-call-hierarchy" "cquery-call-hierarchy.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from cquery-call-hierarchy.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "cquery-call-hierarchy" '("cquery-call-hierarchy")))
-
-;;;***
-
-;;;### (autoloads nil "cquery-code-lens" "cquery-code-lens.el" (0
-;;;;;;  0 0 0))
-;;; Generated autoloads from cquery-code-lens.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "cquery-code-lens" '("cquery-")))
-
-;;;***
-
-;;;### (autoloads nil "cquery-common" "cquery-common.el" (0 0 0 0))
-;;; Generated autoloads from cquery-common.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "cquery-common" '("cquery--")))
-
-;;;***
-
-;;;### (autoloads nil "cquery-inheritance-hierarchy" "cquery-inheritance-hierarchy.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from cquery-inheritance-hierarchy.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "cquery-inheritance-hierarchy" '("cquery-inheritance-hierarchy")))
-
-;;;***
-
-;;;### (autoloads nil "cquery-semantic-highlighting" "cquery-semantic-highlighting.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from cquery-semantic-highlighting.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "cquery-semantic-highlighting" '("cquery-")))
-
-;;;***
-
-;;;### (autoloads nil "cquery-tree" "cquery-tree.el" (0 0 0 0))
-;;; Generated autoloads from cquery-tree.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "cquery-tree" '("cquery-tree-")))
-
-;;;***
-
-;;;### (autoloads nil nil ("cquery-pkg.el") (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; cquery-autoloads.el ends here
