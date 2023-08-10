8cb0556db1bd739b0e3ad6f4a7bc0d11ccee71f7
Wilfred Hughes <me@wilfred.me.uk>
2023-08-10 08:52:22 -0700

    Update git-gutter to fix startup on Emacs 29

---
+++ elpa/git-gutter-20220922.256/git-gutter-pkg.el
@@ -0,0 +1,12 @@
+(define-package "git-gutter" "20220922.256" "Port of Sublime Text plugin GitGutter"
+  '((emacs "25.1"))
+  :commit "ec28e85d237065cb3c28db4b66d129da6d309f9c" :authors
+  '(("Syohei YOSHIDA" . "syohex@gmail.com"))
+  :maintainers
+  '(("Neil Okamoto" . "neil.okamoto+melpa@gmail.com"))
+  :maintainer
+  '("Neil Okamoto" . "neil.okamoto+melpa@gmail.com")
+  :url "https://github.com/emacsorphanage/git-gutter")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
