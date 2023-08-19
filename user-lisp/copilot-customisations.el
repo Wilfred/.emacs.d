009525283abcc16564b2005cca405dbd1c45909e
Wilfred Hughes <me@wilfred.me.uk>
2023-08-19 15:51:13 -0700

    Explicit usage of copilot only

--- user-lisp/copilot-customisations.el
+++ user-lisp/copilot-customisations.el
@@ -2,4 +2,10 @@
 (global-set-key (kbd "C-<return>") #'copilot-accept-completion)
 (global-set-key (kbd "S-<return>") #'copilot-accept-completion-by-line)
 
+;; Don't trigger copilot on idle.
+(setq copilot-idle-delay 999)
+
+;; Instead, use it explicitly.
+(global-set-key (kbd "<backtab>") #'copilot-complete)
+
 ;; (add-hook 'prog-mode-hook 'copilot-mode)
