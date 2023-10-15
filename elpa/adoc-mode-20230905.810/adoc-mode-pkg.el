c125d52090fb2c9fbe30595e3ddc771bb3b2dcbf
Wilfred Hughes <me@wilfred.me.uk>
2023-10-15 16:40:35 -0700

    Install adoc-mode

---
+++ elpa/adoc-mode-20230905.810/adoc-mode-pkg.el
@@ -0,0 +1,14 @@
+(define-package "adoc-mode" "20230905.810" "a major-mode for editing AsciiDoc files"
+  '((emacs "26"))
+  :commit "d2af113cea92e371f8265230b7f840a285a781a8" :authors
+  '(("Florian Kaufmann" . "sensorflo@gmail.com"))
+  :maintainers
+  '(("Bozhidar Batsov" . "bozhidar@batsov.dev"))
+  :maintainer
+  '("Bozhidar Batsov" . "bozhidar@batsov.dev")
+  :keywords
+  '("docs" "wp")
+  :url "https://github.com/bbatsov/adoc-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
