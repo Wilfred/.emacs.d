a1de241ae3d3f353a2b21894e049906d6ebcd38e
Wilfred Hughes <me@wilfred.me.uk>
2023-10-15 16:39:50 -0700

    Configure clippy in Rust with eglot

--- user-lisp/lsp-customisations.el
+++ user-lisp/lsp-customisations.el
@@ -17,4 +17,9 @@
   ;; Mnemonic: action.
   (define-key eglot-mode-map (kbd "C-c a") #'eglot-code-actions))
 
+;; Use clippy for the check command in rust-analyzer, so we get yellow squiggles.
+(add-to-list 'eglot-server-programs
+             '((rust-ts-mode rust-mode) .
+               ("rust-analyzer" :initializationOptions (:check (:command "clippy")))))
+
 (provide 'lsp-customisations)
