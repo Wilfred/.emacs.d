61ca9bb9aee71c76f203e39c269a650220b789ed
Wilfred Hughes <me@wilfred.me.uk>
2023-11-16 08:20:45 -0800

    Update company

---
+++ elpa/company-20231115.2254/company-pkg.el
@@ -0,0 +1,14 @@
+(define-package "company" "20231115.2254" "Modular text completion framework"
+  '((emacs "25.1"))
+  :commit "e22230561ef559b032b4a2d79bef686d494b8ea6" :authors
+  '(("Nikolaj Schumacher"))
+  :maintainers
+  '(("Dmitry Gutov" . "dmitry@gutov.dev"))
+  :maintainer
+  '("Dmitry Gutov" . "dmitry@gutov.dev")
+  :keywords
+  '("abbrev" "convenience" "matching")
+  :url "http://company-mode.github.io/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
