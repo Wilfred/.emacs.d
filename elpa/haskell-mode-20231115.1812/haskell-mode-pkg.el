1c8ca2f81526971fb90c014bfe932f472ade450d
Wilfred Hughes <me@wilfred.me.uk>
2023-12-28 19:05:16 -0800

    Version bump haskell-mode

---
+++ elpa/haskell-mode-20231115.1812/haskell-mode-pkg.el
@@ -0,0 +1,8 @@
+(define-package "haskell-mode" "20231115.1812" "A Haskell editing mode"
+  '((emacs "25.1"))
+  :commit "8d0f44bfe2a9ab6b0969c9bafb75089f315ff5ae" :keywords
+  '("haskell" "cabal" "ghc" "repl" "languages")
+  :url "https://github.com/haskell/haskell-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
