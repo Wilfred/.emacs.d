679b3ec8f0c9ffa230daedfa96b4aeaa29053357
Wilfred Hughes <me@wilfred.me.uk>
2023-12-28 19:05:42 -0800

    Install highlight-indent-guides

---
+++ elpa/highlight-indent-guides-20200820.2328/highlight-indent-guides-pkg.el
@@ -0,0 +1,12 @@
+(define-package "highlight-indent-guides" "20200820.2328" "Minor mode to highlight indentation"
+  '((emacs "24.1"))
+  :commit "cf352c85cd15dd18aa096ba9d9ab9b7ab493e8f6" :authors
+  '(("DarthFennec" . "darthfennec@derpymail.org"))
+  :maintainers
+  '(("DarthFennec" . "darthfennec@derpymail.org"))
+  :maintainer
+  '("DarthFennec" . "darthfennec@derpymail.org")
+  :url "https://github.com/DarthFennec/highlight-indent-guides")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
