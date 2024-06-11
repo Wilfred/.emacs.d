0aea803e9d94bba98da4fef0fb6a2eb00cf2ebca
Wilfred Hughes <me@wilfred.me.uk>
2024-06-11 00:07:44 -0700

    Document rust-analyzer binary path

--- user-lisp/lsp-customisations.el
+++ user-lisp/lsp-customisations.el
@@ -22,6 +22,9 @@
 ;; Use clippy for the check command in rust-analyzer, so we get yellow squiggles.
 (add-to-list 'eglot-server-programs
              '((rust-ts-mode rust-mode) .
+               ;; Prefer the system installed rust-analyzer over the
+               ;; ~/.cargo/bin/rust-analyzer provided by rustup. On
+               ;; Arch Linux, the system package is much newer.
                ("/usr/bin/rust-analyzer" :initializationOptions (:check (:command "clippy")))))
 
 (provide 'lsp-customisations)
