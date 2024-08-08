89eb96e9f03a7a1652e041c0922c561b8a8c7c23
Wilfred Hughes <wilfred@meta.com>
2024-08-08 13:33:19 -0700

    Install clipetty

---
+++ elpa/clipetty-20200327.2241/clipetty-pkg.el
@@ -0,0 +1,14 @@
+(define-package "clipetty" "20200327.2241" "Send every kill from a TTY frame to the system clipboard"
+  '((emacs "25.1"))
+  :commit "7ee3f9c52f70f80820a8c66fb6f796d6e01dd92d" :authors
+  '(("Mike Hamrick" . "mikeh@muppetlabs.com"))
+  :maintainers
+  '(("Mike Hamrick" . "mikeh@muppetlabs.com"))
+  :maintainer
+  '("Mike Hamrick" . "mikeh@muppetlabs.com")
+  :keywords
+  '("terminals" "convenience")
+  :url "https://github.com/spudlyo/clipetty")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
