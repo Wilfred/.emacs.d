cd07731557392b782f62de016b2a1352a9254352
Wilfred Hughes <me@wilfred.me.uk>
2024-08-12 22:07:53 -0700

    Intall goto-chg

---
+++ elpa/goto-chg-20240407.1110/goto-chg-pkg.el
@@ -0,0 +1,14 @@
+(define-package "goto-chg" "20240407.1110" "Go to last change"
+  '((emacs "24.1"))
+  :commit "72f556524b88e9d30dc7fc5b0dc32078c166fda7" :authors
+  '(("David Andersson" . "l.david.anderssonsverige.nu"))
+  :maintainers
+  '(("Vasilij Schneidermann" . "mail@vasilij.de"))
+  :maintainer
+  '("Vasilij Schneidermann" . "mail@vasilij.de")
+  :keywords
+  '("convenience" "matching")
+  :url "https://github.com/emacs-evil/goto-chg")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
