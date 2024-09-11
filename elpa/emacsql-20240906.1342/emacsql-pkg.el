5b042d6e07a858bfc4e49cddb2ea6720568d8671
Wilfred Hughes <me@wilfred.me.uk>
2024-09-10 23:00:49 -0700

    Install org-roam and its emacsql dependency

---
+++ elpa/emacsql-20240906.1342/emacsql-pkg.el
@@ -0,0 +1,12 @@
+(define-package "emacsql" "20240906.1342" "High-level SQL database front-end"
+  '((emacs "25.1"))
+  :commit "fb05d0f72729a4b4452a3b1168a9b7b35a851a53" :authors
+  '(("Christopher Wellons" . "wellons@nullprogram.com"))
+  :maintainers
+  '(("Jonas Bernoulli" . "emacs.emacsql@jonas.bernoulli.dev"))
+  :maintainer
+  '("Jonas Bernoulli" . "emacs.emacsql@jonas.bernoulli.dev")
+  :url "https://github.com/magit/emacsql")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
