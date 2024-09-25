e65393cbad843104254cd471de001e018d966c10
Wilfred Hughes <me@wilfred.me.uk>
2024-09-25 00:04:22 -0400

    Remove unused package

--- elpa/el-x-20140111.1401/el-x-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; el-x-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "dflet" "dflet.el" (21613 8205 47551 783000))
-;;; Generated autoloads from dflet.el
-
-(autoload 'dflet "dflet")
-
-(autoload 'adflet "dflet" "\
-Anaphoric version of `dflet'. Binds `this-fn' to the original
-definition of the function.
-
-\(fn BINDINGS &rest BODY)" nil t)
-
-;;;***
-
-;;;### (autoloads nil nil ("el-x-pkg.el" "el-x.el" "subr-compat.el")
-;;;;;;  (21613 8205 193581 778000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; el-x-autoloads.el ends here
--- elpa/el-x-20140111.1401/el-x-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "el-x" "20140111.1401" "main entry point for el-x package" 'nil :keywords
-  '("lisp"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
