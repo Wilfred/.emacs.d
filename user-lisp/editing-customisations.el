5f3c16d73f551590fbe983164a6ce085fd2b4688
Wilfred Hughes <me@wilfred.me.uk>
2024-10-29 08:55:09 -0700

    Tweak buffer name

--- user-lisp/editing-customisations.el
+++ user-lisp/editing-customisations.el
@@ -429,7 +429,7 @@ keep it visible when editing other similar code."
                   (setq end-pos (point))
                   (buffer-substring start-pos end-pos)))))
          (words (s-split " " s))
-         (buf (get-buffer-create (format "*copy: %s*" (nth 1 words))))
+         (buf (get-buffer-create (format "*copy: %s*" (s-trim (nth 1 words)))))
          (inhibit-read-only t))
     (with-current-buffer buf
       (delete-region (point-min) (point-max))
