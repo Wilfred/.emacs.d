afe8ffff150c207f5356dbbc6071d8745adb00a0
Wilfred Hughes <me@wilfred.me.uk>
2024-10-29 08:56:26 -0700

    Include source buffer in copy buffer's name

--- user-lisp/editing-customisations.el
+++ user-lisp/editing-customisations.el
@@ -429,7 +429,8 @@ keep it visible when editing other similar code."
                   (setq end-pos (point))
                   (buffer-substring start-pos end-pos)))))
          (words (s-split " " s))
-         (buf (get-buffer-create (format "*copy: %s*" (s-trim (nth 1 words)))))
+         (buf (get-buffer-create
+               (format "*%s: %s*" (buffer-name) (s-trim (nth 1 words)))))
          (inhibit-read-only t))
     (with-current-buffer buf
       (delete-region (point-min) (point-max))
