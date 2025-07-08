667efe1ebb14c376cbc4e791f1480f16ed7a9a7f
Wilfred Hughes <me@wilfred.me.uk>
2025-07-08 23:38:32 +0100

    Add dependency missing from elm-mode

---
+++ elpa/reformatter-20240906.1405/reformatter-pkg.el
@@ -0,0 +1,14 @@
+(define-package "reformatter" "20240906.1405" "Define commands which run reformatters on the current buffer"
+  '((emacs "24.3"))
+  :commit "155b235f6c76f2f5790aa53798c7a7e36ce5aded" :authors
+  '(("Steve Purcell" . "steve@sanityinc.com"))
+  :maintainers
+  '(("Steve Purcell" . "steve@sanityinc.com"))
+  :maintainer
+  '("Steve Purcell" . "steve@sanityinc.com")
+  :keywords
+  '("convenience" "tools")
+  :url "https://github.com/purcell/emacs-reformatter")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
