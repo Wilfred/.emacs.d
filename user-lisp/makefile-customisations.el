e5a4cc10c62687d0950b4a08f3a29091f5753ca1
Wilfred Hughes <me@wilfred.me.uk>
2026-02-04 16:27:01 +0000

    Fix copypasta

--- user-lisp/makefile-customisations.el
+++ user-lisp/makefile-customisations.el
@@ -1,6 +1,6 @@
 (use-package make-mode
   :config
   (define-key makefile-mode-map (kbd "M-n") #'highlight-symbol-next)
-  (define-key makefile-mode-map (kbd "M-p") #'highlight-symbol-next))
+  (define-key makefile-mode-map (kbd "M-p") #'highlight-symbol-prev))
 
 (provide 'makefile-customisations)
