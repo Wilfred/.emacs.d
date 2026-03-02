c37f18780ed59ddf0ab3a38120def82f3f3b3ba7
Wilfred Hughes <me@wilfred.me.uk>
2026-03-02 11:30:26 +0000

    Add a helper for toggling meta key on macOS

--- user-lisp/os-x-fixes.el
+++ user-lisp/os-x-fixes.el
@@ -5,4 +5,16 @@
 ;; set Meta-3 to insert a # character
 (global-set-key "\263" #'wh/insert-hash)
 
+
+(defun wh/toggle-meta-key ()
+  "Toggle whether the option or command key is treated as meta."
+  (interactive)
+  (if (eq mac-command-modifier 'meta)
+      (setq mac-command-modifier 'super
+            mac-option-modifier 'meta)
+    (setq mac-command-modifier 'meta
+          mac-option-modifier 'super))
+  (message "Meta key is now: %s"
+           (if (eq mac-command-modifier 'meta) "command" "option")))
+
 (provide 'os-x-fixes)
