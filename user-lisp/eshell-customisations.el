8898786ba7a01a9288348adecff52e9b550a7464
Wilfred Hughes <me@wilfred.me.uk>
2026-09-08 22:16:37 +0100

    Modernize Eshell buffer naming advice

--- user-lisp/eshell-customisations.el
+++ user-lisp/eshell-customisations.el
@@ -4,14 +4,15 @@
 ;; Auto-complete in eshell should stop at the first ambiguity.
 (setq eshell-cmpl-cycle-completions nil)
 
-(defadvice eshell (around eshell-append-cwd (&optional arg) activate)
+(define-advice eshell (:around (old-function &rest args) eshell-append-cwd)
   "New eshell buffers should be named according to their working directory."
   (interactive "P")
-  (if (and arg (not (numberp arg)))
-      ;; Non-numeric prefix arg given, change the eshell buffer name.
-      (let* ((eshell-buffer-name (format "*eshell*<%s>" default-directory)))
-        ad-do-it)
-    ;; Otherwise, continue as normal
-    ad-do-it))
+  (let ((arg (car args)))
+    (if (and arg (not (numberp arg)))
+        ;; Non-numeric prefix arg given, change the eshell buffer name.
+        (let ((eshell-buffer-name (format "*eshell*<%s>" default-directory)))
+          (apply old-function args))
+      ;; Otherwise, continue as normal
+      (apply old-function args))))
 
 (provide 'eshell-customisations)
