5133cd48fc09169542529c0440c172a1484cecd1
Wilfred Hughes <me@wilfred.me.uk>
2026-09-08 22:34:18 +0100

    Declare the Xref auto-jump option

--- user-lisp/xref-customisations.el
+++ user-lisp/xref-customisations.el
@@ -1,4 +1,6 @@
 ;;; -*- lexical-binding: nil; -*-
+(defvar xref-auto-jump-to-first-xref)
+
 ;; If there's only one result, jump to it directly rather than showing
 ;; the summary buffer.
 (setq xref-auto-jump-to-first-xref 'show)
