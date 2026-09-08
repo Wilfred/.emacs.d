9bc23f641f0f101362c3b763adc3ca80872b5b55
Wilfred Hughes <me@wilfred.me.uk>
2026-09-08 22:12:29 +0100

    Use the current clipboard option name

--- user-lisp/kill-ring-customisations.el
+++ user-lisp/kill-ring-customisations.el
@@ -1,6 +1,6 @@
 ;;; -*- lexical-binding: nil; -*-
 ;; top of kill ring should also be in X clipboard
-(setq x-select-enable-clipboard t)
+(setq select-enable-clipboard t)
 
 ;; If the current value is the same as the head of the kill ring,
 ;; don't push the duplicate.
