08b13b511f045ff7558f7724f60a37aed324882d
Wilfred Hughes <me@wilfred.me.uk>
2026-09-08 22:34:17 +0100

    Declare the Flycheck highlighting option

--- user-lisp/flycheck-customisations.el
+++ user-lisp/flycheck-customisations.el
@@ -1,5 +1,7 @@
 ;;; -*- lexical-binding: nil; -*-
 
+(defvar flycheck-highlighting-style)
+
 ;; By default flycheck stops highlighting if there are more than four
 ;; lines of errors, which is super confusing. Increase that limit. 
 (setq flycheck-highlighting-style
