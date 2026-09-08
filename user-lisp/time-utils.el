506de02b7d295473d684969594baa0801de196a6
Wilfred Hughes <me@wilfred.me.uk>
2026-09-08 22:08:47 +0100

    Use arithmetic shifts for time conversion

--- user-lisp/time-utils.el
+++ user-lisp/time-utils.el
@@ -1,12 +1,12 @@
 ;;; -*- lexical-binding: nil; -*-
 (defun time-list-to-float (most-sig-bits least-sig-bits microseconds)
   "Return a float of seconds since 1st of January 1970."
-  (+ (lsh most-sig-bits 16) least-sig-bits
+  (+ (ash most-sig-bits 16) least-sig-bits
      (/ microseconds 1000000.0)))
 
 (defun time-float-to-list (float)
   "Inverse of `time-list-to-float'."
-  (let ((high (lsh (floor float) -16))
+  (let ((high (ash (floor float) -16))
         (low (floor (mod float (expt 2 16))))
         (microseconds (floor (* (- float (floor float)) 1000000))))
     (list high low microseconds)))
