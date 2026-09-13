e89831cc5a3dbea90be75c0d0e65a253ec42e0ae
Wilfred Hughes <me@wilfred.me.uk>
2026-09-13 22:50:41 +0100

    Update dockerfile-mode

---
+++ elpa/dockerfile-mode-20251221.1644/dockerfile-mode-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "dockerfile-mode" "20251221.1644"
+  "Major mode for editing Docker's Dockerfiles."
+  '((emacs "24"))
+  :url "https://github.com/spotify/dockerfile-mode"
+  :commit "97733ce074b1252c1270fd5e8a53d178b66668ed"
+  :revdesc "97733ce074b1"
+  :keywords '("docker" "languages" "processes" "tools"))
